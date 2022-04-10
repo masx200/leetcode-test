f90069401e424a9584ccb50775ffc4b3f8e0fb6f
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-10 17:48:00 +0800

    添加了infinity支持,删除缓存,删除lazyMultiplyPositive

--- powx-n/index.ts
+++ powx-n/index.ts
@@ -2,54 +2,70 @@ export default function myPow(x: number, n: number): number {
     if (Number.isNaN(x) || Number.isNaN(n)) {
         throw Error("nan:" + x + "," + n);
     }
-    if (!Number.isFinite(x) || !Number.isFinite(n)) {
-        throw Error("InFinite:" + x + "," + n);
+    // console.log(x, n);
+    if (x === Infinity) {
+        return n > 0 ? Infinity : n < 0 ? 0 : 1;
     }
-    setTimeout(() => {
-        cacheStore.clear();
-    }, 0);
-    const cached = cacheStore.get(`${x},${n}`);
-    if (cached) {
-        return cached;
+    if (x === -Infinity) {
+        return (n % 2 === 0 ? 1 : -1) * myPow(-x, n);
     }
-    // console.log(x,n)
-    const result = n === 1
-        ? x
-        : x < 0
-        ? (n % 2 === 0 ? 1 : -1) * myPow(-x, n)
-        : x === 1
-        ? 1
-        : x === 0
-        ? 0
-        : n === 0
-        ? 1
-        : n < 0
-        ? myPow(1 / x, -n)
-        : n % 2
-        ? lazyMultiplyPositive(
-            () => x,
-            () => myPow(x, n - 1),
-        )
-        : lazyMultiplyPositive(
+    if (n === Infinity) {
+        return x === 0 ? 0 : Infinity;
+    }
+    if (n === -Infinity) {
+        return x === 0 ? Infinity : 0;
+    }
+    // setTimeout(() => {
+    //     cacheStore.clear();
+    // }, 0);
+    // const cached = cacheStore.get(`${x},${n}`);
+    // if (cached) {
+    //     console.log("cached");
+    //     console.log(x, n);
+    //     return cached;
+    // }
+
+    const result =
+        n === 1
+            ? x
+            : x < 0
+            ? (n % 2 === 0 ? 1 : -1) * myPow(-x, n)
+            : x === 1
+            ? 1
+            : x === 0
+            ? 0
+            : n === 0
+            ? 1
+            : n < 0
+            ? myPow(1 / x, -n)
+            : n % 2
+            ? x * myPow(x, n - 1)
+            : /* lazyMultiplyPositive(
+                  () => x,
+                  () => myPow(x, n - 1)
+              ) */
+              myPow(x * x, Math.floor(n / 2));
+    /* lazyMultiplyPositive(
             () => myPow(x, Math.floor(n / 2)),
             () => myPow(x, n - Math.floor(n / 2)),
-        );
-    cacheStore.set(`${x},${n}`, result);
+        ); */
+
+    // cacheStore.set(`${x},${n}`, result);
     return result;
 }
+//缓存命中率过低
+// const cacheStore = new Map<`${number},${number}`, number>();
 
-const cacheStore = new Map<`${number},${number}`, number>();
-
-function lazyMultiplyPositive(a: () => number, b: () => number): number {
-    if (Math.random() < 0.5) {
-        [b, a] = [a, b];
-    }
-    const l = a();
-    if (l === 0) {
-        return 0;
-    } else if (l === Infinity) {
-        return Infinity;
-    } else {
-        return l * b();
-    }
-}
+// function lazyMultiplyPositive(a: () => number, b: () => number): number {
+//     if (Math.random() < 0.5) {
+//         [b, a] = [a, b];
+//     }
+//     const l = a();
+//     if (l === 0) {
+//         return 0;
+//     } else if (l === Infinity) {
+//         return Infinity;
+//     } else {
+//         return l * b();
+//     }
+// }
--- powx-n/test.ts
+++ powx-n/test.ts
@@ -14,12 +14,27 @@ Deno.test("powx-n", () => {
             output: 64.0,
         },
         { input: [2, -99999], output: 0.0 },
+        { input: [2, 99999], output: Infinity },
         { input: [6553688888888, 6553688888888], output: Infinity },
         { input: [6553688888888, -6553688888888], output: 0.0 },
         { input: [1 / 6553688888888, 6553688888888], output: 0.0 },
         { input: [1 / 6553688888888, -6553688888888], output: Infinity },
         { input: [-6553688888888, -6553688888888], output: 0.0 },
         { input: [-6553688888888, 6553688888888], output: Infinity },
+        { input: [Infinity, 2], output: Infinity },
+        { input: [Infinity, -2], output: 0 },
+        { input: [Infinity, 0], output: 1 },
+        { input: [-Infinity, 0], output: 1 },
+        { input: [-Infinity, 3], output: -Infinity },
+        { input: [-Infinity, 30], output: Infinity },
+        { input: [-Infinity, -30], output: 0 },
+
+        { input: [0, Infinity], output: 0 },
+        { input: [10, Infinity], output: Infinity },
+        { input: [-10, Infinity], output: Infinity },
+        { input: [-10, -Infinity], output: 0 },
+        { input: [10, -Infinity], output: 0 },
+        { input: [0, -Infinity], output: Infinity },
     ];
     examples.forEach(({ input, output }) => {
         assertAlmostEquals(output, pow_x_n(...input));
