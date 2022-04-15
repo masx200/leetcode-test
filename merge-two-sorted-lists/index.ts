6c5b76ba1e79e9371c2f32b37dafcf134dd98b7a
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-15 17:15:20 +0800

    测试

--- merge-two-sorted-lists/index.ts
+++ merge-two-sorted-lists/index.ts
@@ -58,3 +58,13 @@ export default function mergeTwoLists(
     // }
     // return result;
 }
+// export default function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+//     if(!list1){return list2}
+//     if(!list2){return list1}
+
+//     const first=Math.min(list1.val,list2.val)
+//     const next=list1.val<list2.val?mergeTwoLists(list1.next,list2):mergeTwoLists(list2.next,list1)
+//     const result=new ListNode(first,next)
+
+//     return result
+//     };
