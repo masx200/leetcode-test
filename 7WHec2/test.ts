99063670890b8815e76e611e88d90b2586637806
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-19 14:22:24 +0800

    测试

---
+++ 7WHec2/test.ts
@@ -0,0 +1,25 @@
+import { assertEquals } from "../deps.ts";
+import { ArrayToListNode, ListNodeToArray } from "../mod.ts";
+import sortList from "./index.ts";
+
+Deno.test("sort-list", () => {
+    const inputs = [
+        [4, 2, 1, 3],
+        [-1, 5, 3, 4, 0],
+        [],
+        Array.from({ length: 50 }).map((_v, i) => i),
+    ];
+    const outputs = [
+        [1, 2, 3, 4],
+        [-1, 0, 3, 4, 5],
+        [],
+        Array.from({ length: 50 }).map((_v, i) => i),
+    ];
+    const examples: {
+        input: number[];
+        output: number[];
+    }[] = inputs.map((v, i) => ({ input: v, output: outputs[i] }));
+    examples.forEach(({ input, output }) => {
+        assertEquals(output, ListNodeToArray(sortList(ArrayToListNode(input))));
+    });
+});
