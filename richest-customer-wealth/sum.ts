233144b555fb31bfe9579f3cd1eb790b93016e39
masx200 <masx200@qq.com>
2022-04-22 11:30:18 +0000

    update richest-customer-wealth/sum.ts.

--- richest-customer-wealth/sum.ts
+++ richest-customer-wealth/sum.ts
@@ -1,3 +1,8 @@
 export function sum(a: Array<number>) {
-    return a.reduce((p, v) => p + v, 0);
+    // return a.reduce((p, v) => p + v, 0);
+      let r=0
+    for(const v of a){
+        r+=v
+    }
+    return r
 }
