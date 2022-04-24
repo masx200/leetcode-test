aa898b8f8ff7a4bf1fbf1c258c0bbd9525658ebb
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-24 11:36:38 +0800

    Update PrefixTreeTraverse.ts

--- utils/PrefixTreeTraverse.ts
+++ utils/PrefixTreeTraverse.ts
@@ -1,16 +1,49 @@
 import { PrefixTree } from "../implement-trie-prefix-tree/PrefixTree.ts";
-
+//小心调用栈溢出
+// export function PrefixTreeTraverse(root: PrefixTree): Array<string> {
+//     const r: Array<string> = [];
+//     traverse(root, (s) => r.push(s));
+//     return r;
+// }
+// function traverse(root: PrefixTree, output: (s: string) => void) {
+//     root.children.forEach(function (child, key) {
+//         // console.log(key);
+//         if (child.isEnd) {
+//             output(key);
+//         }
+//         return traverse(child, (s: string) => output(key + s));
+//     });
+// }
 export function PrefixTreeTraverse(root: PrefixTree): Array<string> {
-    const r: Array<string> = [];
-    traverse(root, (s) => r.push(s));
-    return r;
-}
-function traverse(root: PrefixTree, output: (s: string) => void) {
-    root.children.forEach(function (child, key) {
-        // console.log(key);
-        if (child.isEnd) {
-            output(key);
+    if (root.children.size === 0) {
+        return [];
+    }
+    const res: Array<string> = [];
+    let prefix_and_entries: {
+        prefix: string;
+        key: string;
+        child: PrefixTree;
+    }[] = [...root.children.entries()].map((e) => ({
+        prefix: "",
+        key: e[0],
+        child: e[1],
+    }));
+    while (prefix_and_entries.length > 0) {
+        const temp: typeof prefix_and_entries = [];
+        for (const entry of prefix_and_entries) {
+            const { prefix, key, child } = entry;
+            if (child.isEnd) {
+                res.push(prefix + key);
+            }
+            temp.push(
+                ...[...child.children.entries()].map((e) => ({
+                    prefix: prefix + key,
+                    key: e[0],
+                    child: e[1],
+                }))
+            );
         }
-        return traverse(child, (s: string) => output(key + s));
-    });
+        prefix_and_entries = temp;
+    }
+    return res;
 }
