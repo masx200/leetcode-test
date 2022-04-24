b2df2523af49e50258e7897df0e73770ebb716fa
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-24 22:05:12 +0800

    Update PriorityQueue.ts

--- kth-largest-element-in-a-stream/PriorityQueue.ts
+++ kth-largest-element-in-a-stream/PriorityQueue.ts
@@ -105,8 +105,8 @@ export function PriorityQueue<T = any>(
     function clear() {
         data.length = 0;
     }
-    function at(index: number): T | undefined {
-        return data.at(index);
-    }
-    return { clear, length, comparator, offer, head, tail, pop, shift, at };
+    //function at(index: number): T | undefined {
+    //    return data.at(index);
+   // }
+    return { clear, length, comparator, offer, head, tail, pop, shift/*, at*/ };
 }
