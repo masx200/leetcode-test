82e1acd0d60af074fcfd517b5006d3e5f9f6a5aa
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-26 23:40:40 +0800

    Update check.ts

--- check.ts
+++ check.ts
@@ -3,30 +3,41 @@ import { walk } from "https://deno.land/std@0.136.0/fs/mod.ts";
 // Async
 async function printFilesNames() {
     console.log("type check start!");
-    for await (
-        const entry of walk(".", {
-            includeFiles: true,
-            includeDirs: false,
-            exts: ["ts"],
-            skip: [/node_modules/],
-        })
-    ) {
+    const stack: string[] = [];
+    for await (const entry of walk(".", {
+        includeFiles: true,
+        includeDirs: false,
+        exts: ["ts"],
+        skip: [/node_modules/],
+    })) {
         console.log(entry.path);
-        const process = Deno.run({
-            cmd: ["deno", "check", entry.path],
-            stderr: "piped",
-            stdout: "piped",
-        });
-        /*        const [status/* , stdout, stderr */
-        //] =  */
-        await Promise.all([
-            process.status(),
-            // process.output(),
-            // process.stderrOutput(),
-        ]);
-        process.close();
-        // console.log({ status, stdout, stderr });
+        if (stack.length < 10) {
+            stack.push(entry.path);
+        } else {
+            await runDenoCheck([...stack, entry.path]);
+            stack.length = 0;
+            // console.log({ status, stdout, stderr });
+        }
+    }
+    if (stack.length) {
+        await runDenoCheck(stack);
     }
 }
 
 printFilesNames().then(() => console.log("type check Done!"));
+
+async function runDenoCheck(stack: string[]) {
+    const process = Deno.run({
+        cmd: ["deno", "check", ...stack],
+        stderr: "piped",
+        stdout: "piped",
+    });
+    /*        const [status/* , stdout, stderr */
+    //] =  */
+    await Promise.all([
+        process.status(),
+        // process.output(),
+        // process.stderrOutput(),
+    ]);
+    process.close();
+}
