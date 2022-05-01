277bdf852d429af34707e1ed6abb3714f3268612
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-01 13:45:29 +0800

    Update index.ts

--- combinations/index.ts
+++ combinations/index.ts
@@ -1,44 +1,37 @@
 export default function combine(n: number, k: number): number[][] {
-    return Array.from(
-        combinations(
-            Array(n)
-                .fill(0)
-                .map((_v, i) => i + 1),
-            k,
-        ),
-    );
+    const nums = Array(n)
+        .fill(0)
+        .map((_v, i) => i + 1);
+    const ans: number[][] = [];
+
+    dfs(0, n - 1, k, [], (indexs: number[]) => {
+        ans.push(indexs.map((v) => nums[v]));
+    });
+    return ans;
 }
-function* combinations<T>(iterable: Iterable<T>, r: number): Generator<T[]> {
-    if (!Number.isInteger(r) || r < 0) {
-        throw RangeError("r must be a non-negative integer");
-    }
-    const pool = [...iterable];
-    const n = pool.length;
-    if (r > n) {
+const dfs = (
+    cur: number,
+    n: number,
+    k: number,
+    temp: number[],
+    output: (nums: number[]) => void
+) => {
+    if (temp.length > k) {
         return;
     }
-    const indices = new Uint32Array(r).map((_, index) => index);
-    yield pool.slice(0, r);
-    while (true) {
-        let i: number;
-        loop: {
-            for (i = r - 1; i >= 0; i--) {
-                if (indices[i] !== i + n - r) {
-                    break loop;
-                }
-            }
-            return;
-        }
-        const result: T[] = Array(r);
-        for (let j = 0; j < i; j++) {
-            result[j] = pool[indices[j]];
-        }
-        let index = (indices[i] += 1);
-        result[i] = pool[index];
-        for (let j = i + 1; j < r; j++) {
-            indices[j] = index += 1;
-            result[j] = pool[index];
-        }
-        yield result;
+    if (cur > n + 1) {
+        return;
     }
-}
+    // 记录合法的答案
+    if (temp.length == k) {
+        output(temp);
+        return;
+    }
+
+    // 考虑不选择当前位置
+    dfs(cur + 1, n, k, temp, output);
+    // 考虑选择当前位置
+    if (temp.length + 1 <= k) {
+        dfs(cur + 1, n, k, [...temp, cur], output);
+    }
+};
