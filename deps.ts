5771c639a9c2764d45897276fdcada3d9edff205
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-06 16:08:20 +0800

    update .gitignore

--- deps.ts
+++ deps.ts
@@ -4,7 +4,7 @@ export {
     assertEquals,
     assertStrictEquals,
     equal,
-} from "https://deno.land/std@0.137.0/testing/asserts.ts";
+} from "https://deno.land/std@0.138.0/testing/asserts.ts";
 export { default as random } from "https://cdn.skypack.dev/lodash@4.17.21/random?dts";
 
 export { default as uniqBy } from "https://cdn.skypack.dev/lodash@4.17.21/uniqBy?dts";
