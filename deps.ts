6005a7fba2ab675803f00cc74a6834f87d562749
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-19 11:26:58 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -4,13 +4,13 @@ export {
     assertEquals,
     assertStrictEquals,
     equal,
-} from "https://deno.land/std@0.139.0/testing/asserts.ts";
+} from "https://deno.land/std@0.140.0/testing/asserts.ts";
 export { default as random } from "https://cdn.skypack.dev/lodash@4.17.21/random?dts";
 
 export { default as uniqBy } from "https://cdn.skypack.dev/lodash@4.17.21/uniqBy?dts";
 
 export { default as intersection } from "https://cdn.skypack.dev/lodash@4.17.21/intersection?dts";
 
-export { walk } from "https://deno.land/std@0.139.0/fs/mod.ts";
-export { parse } from "https://deno.land/std@0.139.0/flags/mod.ts";
+export { walk } from "https://deno.land/std@0.140.0/fs/mod.ts";
+export { parse } from "https://deno.land/std@0.140.0/flags/mod.ts";
 export { combinations } from "https://deno.land/x/combinatorics@1.1.2/mod.ts";
