87b4d5b4f9d0a7523c3473df684bcdbb222adcc2
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-20 22:55:15 +0800

    Update index.ts

--- 4ueAj6/index.ts
+++ 4ueAj6/index.ts
@@ -15,4 +15,37 @@ import { ListNode as Node } from "../reverse-linked-list/ListNode.ts";
 export default function insert(
     head: Node | null,
     insertVal: number,
-): Node | null {}
+): Node | null {
+    if (!head) {
+        const node = new Node(insertVal);
+        node.next = node;
+        return node;
+    }
+    const node = new Node(insertVal);
+
+    let cur = head;
+    while (cur.next !== head) {
+        const nextval = cur.next?.val;
+        if (typeof nextval == "undefined") {
+            throw Error("none next node");
+        }
+        if (cur.val <= insertVal && nextval >= insertVal) {
+            break;
+        }
+        if (cur.val > nextval) {
+            if (nextval >= insertVal) {
+                break;
+            } else if (cur.val <= insertVal) {
+                break;
+            }
+        }
+        if (cur.next) {
+            cur = cur.next;
+        } else {
+            throw Error("none next node");
+        }
+    }
+    node.next = cur.next;
+    cur.next = node;
+    return head;
+}
