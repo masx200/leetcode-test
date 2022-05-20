36a78067c5cbfacffcfb376f392fa6b9d8a937a6
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-20 11:40:19 +0800

    正则表达式

--- biao-shi-shu-zhi-de-zi-fu-chuan-lcof/index.ts
+++ biao-shi-shu-zhi-de-zi-fu-chuan-lcof/index.ts
@@ -1,7 +1,11 @@
 export default function isNumber(s: string): boolean {
     s = s.trim();
-    if (s.length === 0) return false;
-    if (["+Infinity", "Infinity", "-Infinity"].includes(s)) return false;
-    const num = Number(s);
-    return !Number.isNaN(num);
+    return /^(\+|\-)?(\d+(\.\d*)?|\.\d+)((e|E)(\+|\-)?\d+)?$/.test(s);
 }
+// export default function isNumber(s: string): boolean {
+//     s = s.trim();
+//     if (s.length === 0) return false;
+//     if (["+Infinity", "Infinity", "-Infinity"].includes(s)) return false;
+//     const num = Number(s);
+//     return !Number.isNaN(num);
+// }
--- valid-number/index.ts
+++ valid-number/index.ts
@@ -1,5 +1,9 @@
 export default function isNumber(s: string): boolean {
-    if (["+Infinity", "Infinity", "-Infinity"].includes(s)) return false;
-    const num = Number(s);
-    return !Number.isNaN(num);
+    //s=s.trim()
+    return /^(\+|\-)?(\d+(\.\d*)?|\.\d+)((e|E)(\+|\-)?\d+)?$/.test(s);
 }
+// export default function isNumber(s: string): boolean {
+//     if (["+Infinity", "Infinity", "-Infinity"].includes(s)) return false;
+//     const num = Number(s);
+//     return !Number.isNaN(num);
+// }
