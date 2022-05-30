3f0b92c341a54291bd154e888b7c9be25fac2bd1
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-30 18:13:59 +0800

    Update index.ts

--- longest-palindromic-substring/index.ts
+++ longest-palindromic-substring/index.ts
@@ -3,25 +3,21 @@ export default function longestPalindrome(s: string): string {
     const length = s.length;
     let max_length = 1;
     let start = 0;
-    const isPalindromic = (() => {
-        const cache = new Array<boolean>((length * (length + 1)) / 2);
-        // console.log(cache)
-        return function get(i: number, j: number): boolean {
-            const key = i >= j ? (i * (i + 1)) / 2 + j : (j * (j + 1)) / 2 + i;
-            const cached = cache[key];
-            if (typeof cached !== "undefined") {
-                return cached;
-            }
-            const result =
-                i === j
-                    ? true
-                    : i + 1 === j
-                    ? s[i] === s[j]
-                    : s[i] === s[j] && get(i + 1, j - 1);
-            cache[key] = result;
-            return result;
-        };
-    })();
+    const cache = new Array<boolean>(length * (length + 1) / 2);
+    function isPalindromic(i: number, j: number): boolean {
+        const key = i >= j ? (i * (i + 1) / 2) + j : (j * (j + 1) / 2) + i;
+        const cached = cache[key];
+        if (typeof cached !== "undefined") {
+            return cached;
+        }
+        const result = i === j
+            ? true
+            : i + 1 === j
+            ? s[i] === s[j]
+            : s[i] === s[j] && isPalindromic(i + 1, j - 1);
+        cache[key] = result;
+        return result;
+    }
 
     for (let i = 0; i < s.length; i++) {
         for (let j = i; j < s.length; j++) {
