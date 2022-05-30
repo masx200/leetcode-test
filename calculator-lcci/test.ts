3813b7c5303e923ba8818d78b3a39c7e4a15fdcd
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-30 22:07:38 +0800

    Create test.ts

---
+++ calculator-lcci/test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "../deps.ts";
+import calculate from "./index.ts";
+Deno.test("testcase-calculator", () => {
+    assertEquals(calculate("3+2*2"), 7);
+    assertEquals(calculate("3/2"), 1);
+    assertEquals(calculate(" 3+5 / 2 "), 5);
+});
