26fa017e25e60010ee45a83db3ce896bf8f0bc70
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-03 22:47:34 +0800

    Update index.ts

--- design-an-ordered-stream/index.ts
+++ design-an-ordered-stream/index.ts
@@ -1,24 +1,24 @@
-export default  class OrderedStream {
-    #store: string[];
+export default class OrderedStream {
+    #store: Map<number, string>;
     #ptr = 1;
-    #n:number
+    #n: number;
     constructor(n: number) {
-        this.#store = Array(0);
-        this.#n=n
+        this.#store = new Map();
+        this.#n = n;
     }
 
     insert(idKey: number, value: string): string[] {
-        if(idKey>this.#n||idKey<1){
-            throw Error('range error')
+        if (idKey > this.#n || idKey < 1) {
+            throw Error("range error");
         }
-        this.#store[idKey] = value;
-        if (this.#ptr === idKey && Reflect.has(this.#store, idKey)) {
+        this.#store.set(idKey, value);
+        if (this.#ptr === idKey && (this.#store.has(idKey))) {
             const res: string[] = [];
-            let i = this.#ptr
-            for (; Reflect.has(this.#store, i); i++) {
-                res.push(this.#store[i]);
+            let i = this.#ptr;
+            for (; this.#store.has(i); i++) {
+                res.push(this.#store.get(i) || "");
             }
-            this.#ptr = i ;
+            this.#ptr = i;
             return res;
         }
         return [];
