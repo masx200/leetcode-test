00204d340692ae8a94604d92cfb8e3f9733d21d9
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-17 11:48:49 +0800

    Create index.ts

---
+++ duplicate-zeros/index.ts
@@ -0,0 +1,17 @@
+export default function duplicateZeros(arr: number[]): void {
+    const ans: Array<number> = []
+    const length = arr.length
+    for (const num of arr) {
+        if (ans.length >= length) { break }
+        if (num === 0) {
+            ans.push(0, 0)
+        } else {
+            ans.push(num)
+        }
+    }
+    ans.forEach((v, i) => {
+        if (i >= length) { return }
+        arr[i] = v
+
+    })
+};
