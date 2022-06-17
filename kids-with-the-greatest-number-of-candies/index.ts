d7e272b59f19982462892e057392f0b5d926445c
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-17 15:01:12 +0800

    https://leetcode.cn/problems/kids-with-the-greatest-number-of-candies/

---
+++ kids-with-the-greatest-number-of-candies/index.ts
@@ -0,0 +1,7 @@
+export default function kidsWithCandies(
+    candies: number[],
+    extraCandies: number,
+): boolean[] {
+    const max = Math.max(...candies);
+    return candies.map((candy) => candy + extraCandies >= max);
+}
