2aa927a60e2b65efb5b1e5fe6a2abd07d05bd9aa
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-22 11:10:09 +0800

    Update index.ts

--- prison-cells-after-n-days/index.ts
+++ prison-cells-after-n-days/index.ts
@@ -1,2 +1,31 @@
 export default function prisonAfterNDays(cells: number[], n: number): number[] {
+    if (n === 0) return cells;
+    const sets: Set<number> = new Set<number>(),
+        results: number[][] = [];
+    let key: number,
+        times: number = n; //, i: number = 0;
+    while (times-- > 0) {
+        cells = transform(cells);
+        key = state_unique_key(cells);
+        if (sets.has(key)) {
+            // console.log(results)
+            return results[times % results.length];
+        }
+        sets.add(key);
+        results.push(cells);
+    }
+    return cells;
+}
+
+function transform(cells: number[]) {
+    return cells.map((_value, index) => {
+        if (index === 0 || index === cells.length - 1) {
+            return 0;
+        }
+
+        return Number(cells[index + 1] === cells[index - 1]);
+    });
+}
+function state_unique_key(cells: number[]): number {
+    return Number("0b" + cells.join(""));
 }
