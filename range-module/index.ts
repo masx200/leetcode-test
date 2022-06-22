b10ce2a9f57ab3d2c22a3796c055471804871145
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-22 16:13:21 +0800

    Update index.ts

--- range-module/index.ts
+++ range-module/index.ts
@@ -1,9 +1,29 @@
+import { searchSegmentTree } from "../mod.ts";
+import { SegmentTree } from "../my-calendar-iii/SegmentTree.ts";
+
 export default class RangeModule {
-    constructor() {}
+    #root: SegmentTree = SegmentTree(
+        Number.MIN_SAFE_INTEGER,
+        Number.MAX_SAFE_INTEGER,
+        0
+    );
 
-    addRange(left: number, right: number): void {}
+    addRange(left: number, right: number): void {
+        const nodes = searchSegmentTree(left, right - 1, this.#root);
+        for (const node of nodes) {
+            node.value = 1;
+        }
+    }
 
-    queryRange(left: number, right: number): boolean {}
+    queryRange(left: number, right: number): boolean {
+        const nodes = searchSegmentTree(left, right - 1, this.#root);
+        return nodes.every((node) => node.value === 1);
+    }
 
-    removeRange(left: number, right: number): void {}
+    removeRange(left: number, right: number): void {
+        const nodes = searchSegmentTree(left, right - 1, this.#root);
+        for (const node of nodes) {
+            node.value = 0;
+        }
+    }
 }
