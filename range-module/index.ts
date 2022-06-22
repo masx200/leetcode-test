bb9df3524a16bdf11819b846b9c4f06a27c42749
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-22 17:08:02 +0800

    Update index.ts

--- range-module/index.ts
+++ range-module/index.ts
@@ -5,7 +5,7 @@ export default class RangeModule {
     #root: SegmentTree = SegmentTree(
         Number.MIN_SAFE_INTEGER,
         Number.MAX_SAFE_INTEGER,
-        0,
+        0
     );
 
     addRange(left: number, right: number): void {
@@ -16,14 +16,30 @@ export default class RangeModule {
     }
 
     queryRange(left: number, right: number): boolean {
-        const nodes = searchSegmentLeaf(left, right - 1, this.#root);
-        return nodes.every((node) => node.value === 1);
+        // const nodes = searchSegmentLeaf(left, right - 1, this.#root);
+        // return nodes.every((node) => node.value === 1);
+        return isRangeTracked(left, right, this.#root);
     }
 
     removeRange(left: number, right: number): void {
         const nodes = searchSegmentLeaf(left, right - 1, this.#root);
         for (const node of nodes) {
             node.value = 0;
+        }
+    }
+}
+function isRangeTracked(
+    left: number,
+    right: number,
+    node: SegmentTree
+): boolean {
+    if (node.children.length) {
+        return node.children.every((node) => isRangeTracked(left, right, node));
+    } else {
+        if (left <= node.start && right >= node.end) {
+            return node.value === 1;
+        } else {
+            return true;
         }
     }
 }
