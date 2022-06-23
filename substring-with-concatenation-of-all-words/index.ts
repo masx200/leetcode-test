215261a7d7c585c40d41b645e86c414985bbeb61
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-23 10:28:50 +0800

    Update index.ts

--- substring-with-concatenation-of-all-words/index.ts
+++ substring-with-concatenation-of-all-words/index.ts
@@ -1,4 +1,3 @@
-import { equal } from "../deps.ts";
 import { counter } from "./counter.ts";
 
 export default function findSubstring(s: string, words: string[]): number[] {
@@ -8,15 +7,40 @@ export default function findSubstring(s: string, words: string[]): number[] {
     return Array(s.length - step * words.length + 1)
         .fill(0)
         .map((_, i) => i)
-        .filter((i) =>
-            equal(
-                cnts,
-                counter(
-                    Array(words.length)
-                        .fill(0)
-                        .map((_, i) => i)
-                        .map((j) => s.slice(i + j * step, i + (j + 1) * step)),
-                ),
-            )
+        .filter(
+            (i) => {
+                const curcnts = new Map<string, number>();
+                const values = Array(words.length)
+                    .fill(0)
+                    .map((_, i) => i)
+                    .map((j) => s.slice(i + j * step, i + (j + 1) * step));
+
+                for (const word of values) {
+                    if (!cnts.has(word)) {
+                        return false;
+                    }
+                    curcnts.set(word, (curcnts.get(word) ?? 0) + 1);
+
+                    const count1 = curcnts.get(word);
+                    const count2 = cnts.get(word);
+                    if (
+                        typeof count2 === "number" &&
+                        count1 &&
+                        count1 > count2
+                    ) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            // equal(
+            //     cnts,
+            //     counter(
+            //         Array(words.length)
+            //             .fill(0)
+            //             .map((_, i) => i)
+            //             .map((j) => s.slice(i + j * step, i + (j + 1) * step)),
+            //     ),
+            // )
         );
 }
