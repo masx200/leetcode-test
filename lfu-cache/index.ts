81970ddd8439a37d5e18e4c70b94fb3a4acb7465
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-26 13:39:54 +0800

    Update index.ts

--- lfu-cache/index.ts
+++ lfu-cache/index.ts
@@ -1,26 +1,60 @@
 import { DoublyLinkedList } from "../design-linked-list/DoublyLinkedList.ts";
+class MyLinkedList<T = number> {
+    readonly #freq_list_head = DoublyLinkedList<T>();
+    readonly #freq_list_tail = DoublyLinkedList<T>();
+    constructor() {
+        this.#freq_list_head.next = this.#freq_list_tail;
+        this.#freq_list_tail.prev = this.#freq_list_head;
+    }
+    first_node() {
+        return this.#freq_list_head.next === this.#freq_list_tail
+            ? null
+            : this.#freq_list_head.next;
+    }
+    insert_First(node: DoublyLinkedList<T>): void {
+        this.insert_After(this.#freq_list_head, node);
+    }
+    remove_node(node: DoublyLinkedList<T>): void {
+        const prev = node.prev;
+        const next = node.next;
+        if (prev && next) {
+            prev.next = next;
 
+            next.prev = prev;
+        } else {
+            throw Error("next prev node not in list");
+        }
+    }
+    insert_After(
+        prev_node: DoublyLinkedList<T>,
+        new_node: DoublyLinkedList<T>,
+    ): void {
+        const next = prev_node.next;
+        const prev = prev_node;
+        if (prev && next) {
+            prev.next = new_node;
+            new_node.prev = prev;
+            new_node.next = next;
+            next.prev = new_node;
+        } else {
+            throw Error("next prev_node not in list");
+        }
+    }
+}
 class LFUCache {
     readonly #key_to_freq: Map<number, number> = new Map();
     readonly #key_to_value: Map<number, number> = new Map();
     readonly #freq_to_keys = new Map<number, Set<number>>();
     readonly #freq_to_node = new Map<number, DoublyLinkedList<number>>();
     readonly #capacity: number;
-    readonly #freq_list_head = DoublyLinkedList<number>();
-    readonly #freq_list_tail = DoublyLinkedList<number>();
+    readonly #linked_list = new MyLinkedList<number>();
     constructor(capacity: number) {
         if (capacity < 1) throw Error("capacity invalid");
         this.#capacity = capacity;
-        this.#freq_list_head.next = this.#freq_list_tail;
-        this.#freq_list_tail.prev = this.#freq_list_head;
     }
-    #first_node() {
-        return this.#freq_list_head.next === this.#freq_list_tail
-            ? null
-            : this.#freq_list_head.next;
-    }
+
     #least_frequently_recently_used_key = (): number | undefined => {
-        const least_freq_node = this.#first_node();
+        const least_freq_node = this.#linked_list.first_node();
         const least_frequent = least_freq_node && least_freq_node.val;
 
         if (typeof least_frequent === "number") {
@@ -46,7 +80,7 @@ class LFUCache {
         if (freq_set && freq_set.size === 0) {
             const node = this.#freq_to_node.get(freq);
             this.#freq_to_node.delete(freq);
-            node && this.#remove_node(node);
+            node && this.#linked_list.remove_node(node);
         }
     }
 
@@ -63,7 +97,7 @@ class LFUCache {
             const node = DoublyLinkedList(freq);
             this.#freq_to_keys.set(freq, new Set());
             if (freq === 1) {
-                this.#insert_First(node);
+                this.#linked_list.insert_First(node);
 
                 this.#freq_to_node.set(freq, node);
             } else {
@@ -72,7 +106,7 @@ class LFUCache {
                 if (!prev) {
                     throw new Error("accident prev is undefined");
                 }
-                this.#insert_After(prev, node);
+                this.#linked_list.insert_After(prev, node);
 
                 this.#freq_to_node.set(freq, node);
             }
@@ -83,35 +117,6 @@ class LFUCache {
         this.#remove_key_empty_freq_node(freq - 1, key);
     }
 
-    #insert_First(node: DoublyLinkedList<number>): void {
-        this.#insert_After(this.#freq_list_head, node);
-    }
-    #remove_node(node: DoublyLinkedList<number>): void {
-        const prev = node.prev;
-        const next = node.next;
-        if (prev && next) {
-            prev.next = next;
-
-            next.prev = prev;
-        } else {
-            throw Error("next prev node not in list");
-        }
-    }
-    #insert_After(
-        prev_node: DoublyLinkedList<number>,
-        new_node: DoublyLinkedList<number>
-    ): void {
-        const next = prev_node.next;
-        const prev = prev_node;
-        if (prev && next) {
-            prev.next = new_node;
-            new_node.prev = prev;
-            new_node.next = next;
-            next.prev = new_node;
-        } else {
-            throw Error("next prev_node not in list");
-        }
-    }
     get(key: number): number {
         const value = this.#key_to_value.get(key);
         // debugger;
@@ -128,8 +133,8 @@ class LFUCache {
         // debugger;
         if (!this.#has(key)) {
             if (this.#size === this.#capacity) {
-                const to_be_removed_key =
-                    this.#least_frequently_recently_used_key();
+                const to_be_removed_key = this
+                    .#least_frequently_recently_used_key();
                 if (typeof to_be_removed_key === "number") {
                     this.#delete(to_be_removed_key);
                 }
