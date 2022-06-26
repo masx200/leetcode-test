9a217097ea8e01ede460b14e496d6639ab650ff3
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-26 11:46:38 +0800

    Update index.ts

--- lfu-cache/index.ts
+++ lfu-cache/index.ts
@@ -12,7 +12,19 @@ class LFUCache {
         this.#freq_list_head.next = this.#freq_list_tail;
         this.#freq_list_tail.prev = this.#freq_list_head;
     }
-    #least_frequently_recently_used_key = (): number | undefined => {};
+    #least_frequently_recently_used_key = (): number | undefined => {
+        const least_freq_node = this.#freq_list_head.next;
+        const least_frequent = least_freq_node && least_freq_node.val;
+
+        if (typeof least_frequent === "number") {
+            const least_freq_keys = this.#freq_to_keys.get(least_frequent);
+            if (least_freq_keys && least_freq_keys.size) {
+                for (const key of least_freq_keys) {
+                    return key;
+                }
+            }
+        }
+    };
     #delete(key: number): void {
         const freq = this.#key_to_freq.get(key);
         this.#key_to_freq.delete(key);
@@ -35,11 +47,30 @@ class LFUCache {
     get(key: number): number {}
 
     put(key: number, value: number): void {}
+    #insertFirst(node: DoublyLinkedList<number>): void {
+        this.#insert_node_after(this.#freq_list_head, node);
+    }
+    #remove_node(node: DoublyLinkedList<number>): void {
+        const prev = node.prev;
+        const next = node.next;
+        if (prev && next) {
+            prev.next = next;
 
-    #remove_node(node: DoublyLinkedList<number>): void {}
+            next.prev = prev;
+        }
+    }
     #insert_node_after(
+        prev_node: DoublyLinkedList<number>,
         new_node: DoublyLinkedList<number>,
-        prev_node: DoublyLinkedList<number>
-    ): void {}
+    ): void {
+        const next = prev_node.next;
+        const prev = prev_node;
+        if (prev && next) {
+            prev.next = new_node;
+            new_node.prev = prev;
+            new_node.next = next;
+            next.prev = new_node;
+        }
+    }
 }
 export default LFUCache;
