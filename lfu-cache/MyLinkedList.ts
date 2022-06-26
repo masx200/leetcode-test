cbd626ff799262d840324e2eed539f72f2df9abc
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-26 14:16:02 +0800

    Update MyLinkedList.ts

--- lfu-cache/MyLinkedList.ts
+++ lfu-cache/MyLinkedList.ts
@@ -7,7 +7,18 @@ export class MyLinkedList<T = number> {
         this.#freq_list_head.next = this.#freq_list_tail;
         this.#freq_list_tail.prev = this.#freq_list_head;
     }
-
+    isEmpty() {
+        return this.#freq_list_head.next === this.#freq_list_tail;
+    }
+    clear() {
+        this.#freq_list_head.next = this.#freq_list_tail;
+        this.#freq_list_tail.prev = this.#freq_list_head;
+    }
+    last_node() {
+        return this.#freq_list_tail.prev === this.#freq_list_head
+            ? null
+            : this.#freq_list_tail.prev;
+    }
     first_node() {
         return this.#freq_list_head.next === this.#freq_list_tail
             ? null
@@ -29,7 +40,7 @@ export class MyLinkedList<T = number> {
     }
     insert_After(
         prev_node: DoublyLinkedList<T>,
-        new_node: DoublyLinkedList<T>,
+        new_node: DoublyLinkedList<T>
     ): void {
         const next = prev_node.next;
         const prev = prev_node;
