4709955d8caa190ca7a826f169856f925b03c69e
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-27 23:26:29 +0800

    Create index.ts

---
+++ longest-uncommon-subsequence-ii/index.ts
@@ -0,0 +1,39 @@
+export default
+function findLUSlength(strs: string[]): number {
+
+return Math.max(-1,...strs.filter((str1,i)=>{
+    
+     const check=strs.every((str,j)=>{
+            
+            if(i!==j&&isSubseq(str1,str)){
+                
+                return false
+            }
+            
+            
+            
+            
+            return true
+            
+            
+            
+        })
+        
+        return check
+    
+}).map(str=>str.length))
+
+   
+    
+}
+
+const isSubseq = (s:string, t:string) => {
+    let ptS = 0, ptT = 0;
+    while (ptS < s.length && ptT < t.length) {
+        if (s[ptS] === t[ptT]) {
+            ++ptS;
+        }
+        ++ptT;
+    }
+    return ptS === s.length;
+}
