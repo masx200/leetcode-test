9024f086bd0d0fa36a7182e0d6ec05a4a4f34ff8
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-29 10:13:45 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -5,17 +5,17 @@ export {
     assertFalse,
     assertStrictEquals,
     equal,
-} from "https://deno.land/std@0.144.0/testing/asserts.ts";
+} from "https://deno.land/std@0.145.0/testing/asserts.ts";
 export { default as random } from "https://cdn.skypack.dev/lodash@4.17.21/random?dts";
 
 export { default as uniqBy } from "https://cdn.skypack.dev/lodash@4.17.21/uniqBy?dts";
 
 export { default as intersection } from "https://cdn.skypack.dev/lodash@4.17.21/intersection?dts";
 
-export { walk } from "https://deno.land/std@0.144.0/fs/mod.ts";
-export { parse } from "https://deno.land/std@0.144.0/flags/mod.ts";
+export { walk } from "https://deno.land/std@0.145.0/fs/mod.ts";
+export { parse } from "https://deno.land/std@0.145.0/flags/mod.ts";
 export { combinations } from "https://deno.land/x/combinatorics@1.1.2/mod.ts";
-export { isIP } from "https://deno.land/std@0.144.0/node/internal/net.ts";
+export { isIP } from "https://deno.land/std@0.145.0/node/internal/net.ts";
 export {
     Heap,
 } from "https://cdn.skypack.dev/@datastructures-js/heap@4.1.0/?dts";
