ac99c159cb411b5dd031920bbcc3c1b185cad32a
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-01 23:01:36 +0800

    Update index.ts

--- 1-bit-and-2-bit-characters/index.ts
+++ 1-bit-and-2-bit-characters/index.ts
@@ -1,6 +1,4 @@
 export default function isOneBitCharacter(bits: number[]): boolean {
-    return bits
-        .join("")
-        .replaceAll(/(10)|(11)|0/g, (s) => (s.length === 2 ? "B" : "A"))
-        .endsWith("A");
+    
+return /^(10|11|0)*0$/g.test(bits.join(""))
 }
