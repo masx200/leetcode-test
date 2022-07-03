7a7829e55e7d6fc206aadf56c61ed8b22b878a3e
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-03 16:20:06 +0800

    Update test.ts

--- p0NxJO/test.ts
+++ p0NxJO/test.ts
@@ -9,6 +9,6 @@ Deno.test("magicTower", () => {
     assertEquals(-1, magicTower([-200, -300, 400, 0]));
     assertEquals(
         1,
-        magicTower([100, 100, 100, -250, -60, -140, -50, -50, 100, 150])
+        magicTower([100, 100, 100, -250, -60, -140, -50, -50, 100, 150]),
     );
 });
