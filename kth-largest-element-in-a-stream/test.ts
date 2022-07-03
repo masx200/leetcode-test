961e83ac70b8563135c4d2a04487e72d7f6ec2cb
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-03 16:12:02 +0800

    Update test.ts

--- kth-largest-element-in-a-stream/test.ts
+++ kth-largest-element-in-a-stream/test.ts
@@ -1,6 +1,15 @@
 import { assert, assertEquals, assertFalse } from "../deps.ts";
+import KthLargest from "./index.ts";
 import { PriorityQueue } from "./PriorityQueue.ts";
 
+Deno.test("kth-largest-element-in-a-stream", () => {
+    const kthLargest = KthLargest(3, [4, 5, 8, 2]);
+    assertEquals(4, kthLargest.add(3)); // return 4
+    assertEquals(5, kthLargest.add(5)); // return 5
+    assertEquals(5, kthLargest.add(10)); // return 5
+    assertEquals(8, kthLargest.add(9)); // return 8
+    assertEquals(8, kthLargest.add(4)); // return 8
+});
 Deno.test("PriorityQueue", () => {
     const data = [
         40,
