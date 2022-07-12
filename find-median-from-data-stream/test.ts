c14d795116666b5f2318052e54e884e35d7430cb
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-12 14:17:12 +0800

    MedianFinder2

--- find-median-from-data-stream/test.ts
+++ find-median-from-data-stream/test.ts
@@ -1,8 +1,18 @@
 import { assertEquals } from "../deps.ts";
-import MedianFinder from "./index.ts";
+import MedianFinder1 from "./index.ts";
+import MedianFinder2 from "./MedianFinder.ts";
 
-Deno.test("find-median-from-data-stream", () => {
-    const medianFinder = new MedianFinder();
+Deno.test("find-median-from-data-stream-1", () => {
+    const medianFinder = new MedianFinder1();
+
+    medianFinder.addNum(1);
+    medianFinder.addNum(2);
+    assertEquals(medianFinder.findMedian(), 1.5);
+    medianFinder.addNum(3);
+    assertEquals(medianFinder.findMedian(), 2);
+});
+Deno.test("find-median-from-data-stream-2", () => {
+    const medianFinder = new MedianFinder2();
 
     medianFinder.addNum(1);
     medianFinder.addNum(2);
