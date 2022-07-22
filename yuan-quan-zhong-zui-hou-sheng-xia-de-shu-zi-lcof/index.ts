67e9cf0436e793314ef801b6cf0958ea546eedea
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-22 11:30:06 +0800

    格式化

--- yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/index.ts
+++ yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/index.ts
@@ -1,6 +1,5 @@
-export default
-function lastRemaining(n: number, m: number): number {
-let f = 0;
+export default function lastRemaining(n: number, m: number): number {
+    let f = 0;
     for (let i = 2; i <= n; i++) {
         f = (f + m) % i;
     }
