70d242419b1f412df608543399480a5f7b5c028e
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-27 17:32:18 +0800

    Create test.ts

---
+++ design-skiplist/test.ts
@@ -0,0 +1,15 @@
+import Skiplist from "./index.ts";
+import { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
+
+Deno.test("design-skiplist", () => {
+    const skiplist: Skiplist = Skiplist();
+    skiplist.add(1);
+    skiplist.add(2);
+    skiplist.add(3);
+    assertEquals(false, skiplist.search(0)); // 返回 false
+    skiplist.add(4);
+    assertEquals(true, skiplist.search(1)); // 返回 true
+    assertEquals(false, skiplist.erase(0)); // 返回 false，0 不在跳表中
+    assertEquals(true, skiplist.erase(1)); // 返回 true
+    assertEquals(false, skiplist.search(1)); // 返回 false，1 已被擦除
+});
