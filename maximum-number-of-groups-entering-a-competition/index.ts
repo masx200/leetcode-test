8df984673d2ed1013011ba83b65d5a917dfb4906
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-31 20:09:48 +0800

    Create index.ts

---
+++ maximum-number-of-groups-entering-a-competition/index.ts
@@ -0,0 +1,8 @@
+export default
+function maximumGroups(grades: number[]): number {
+const t = 2 * grades.length
+const g =Math.floor( Math.sqrt(t));
+
+        return g * (g + 1) > t ? g - 1 : g;
+
+}
