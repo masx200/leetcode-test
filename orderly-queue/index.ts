7985b72c483f413fe22c6bef33ca1bcdd17cbd22
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-03 10:22:46 +0800

    Update index.ts

--- orderly-queue/index.ts
+++ orderly-queue/index.ts
@@ -1,14 +1,11 @@
 function orderlyQueue(s: string, k: number): string {
-    
-    if(s.length==1||!k)return s
-if(k===1){
-    
-    
-    
-    return Array.from(s).map((_,i)=>s.slice(-i)+s.slice(0,s.length-i)).reduce((a,v)=>a.localeCompare(v)<0?a:v)
-}
+    if (s.length == 1 || !k) return s;
+    if (k === 1) {
+        return Array.from(s).map((_, i) =>
+            s.slice(-i) + s.slice(0, s.length - i)
+        ).reduce((a, v) => a.localeCompare(v) < 0 ? a : v);
+    }
 
-
-return Array.from(s).sort().join("")
+    return Array.from(s).sort().join("");
 }
-export default orderlyQueue
+export default orderlyQueue;
