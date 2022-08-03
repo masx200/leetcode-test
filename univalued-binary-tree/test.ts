d43b6172ac08bab3b14de3d1eb3704852948da7b
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-03 16:29:18 +0800

    Create test.ts

---
+++ univalued-binary-tree/test.ts
@@ -0,0 +1,32 @@
+import { assertEquals } from "https://deno.land/std@0.150.0/testing/asserts.ts";
+import isUnivalTree from "./index.ts";
+Deno.test("univalued-binary-tree", () => {
+    assertEquals(
+        false,
+        isUnivalTree({
+            val: 2,
+            left: {
+                val: 2,
+                left: { val: 5, left: null, right: null },
+                right: { val: 2, left: null, right: null },
+            },
+            right: { val: 2, left: null, right: null },
+        }),
+    );
+    assertEquals(
+        true,
+        isUnivalTree({
+            val: 1,
+            left: {
+                val: 1,
+                left: { val: 1, left: null, right: null },
+                right: { val: 1, left: null, right: null },
+            },
+            right: {
+                val: 1,
+                left: null,
+                right: { val: 1, left: null, right: null },
+            },
+        }),
+    );
+});
