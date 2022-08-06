9db7b5413c370d8ffee62b1e04dd80d9e833e135
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-06 15:35:59 +0800

    Create test.ts

---
+++ string-matching-in-an-array/test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "https://deno.land/std@0.150.0/testing/asserts.ts";
+import stringMatching from "./index.ts";
+
+Deno.test("string-matching-in-an-array", () => {
+    const inputs = [["mass", "as", "hero", "superhero"], [
+        "leetcode",
+        "et",
+        "code",
+    ], ["blue", "green", "bu"]];
+    const outputs = [["as", "hero"], ["et", "code"], []];
+    assertEquals(inputs.map(stringMatching), outputs);
+});
