dbe27e95020a379ef7f4923a09b30d63cdf88813
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-07 16:07:03 +0800

    Create test.ts

---
+++ combination-sum/test.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from "https://deno.land/std@0.151.0/testing/asserts.ts";
+import combinationSum from "./index.ts";
+
+Deno.test("combination-sum", function () {
+    const inputs: Array<Parameters<typeof combinationSum>> = [
+        [[2, 3, 6, 7], 7],
+        [[2, 3, 5], 8],
+        [[2], 1],
+    ];
+    const outputs = [
+        [[2, 2, 3], [7]],
+        [
+            [2, 2, 2, 2],
+            [2, 3, 3],
+            [3, 5],
+        ],
+        [],
+    ];
+    assertEquals(
+        inputs.map(([c, t]) => combinationSum(c, t)),
+        outputs
+    );
+});
