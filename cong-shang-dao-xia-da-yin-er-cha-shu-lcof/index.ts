43bf50f5da68b9e7bb5b9a9829dfb58eb00ee51b
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-09 13:15:20 +0800

    https://leetcode.cn/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof/

---
+++ cong-shang-dao-xia-da-yin-er-cha-shu-lcof/index.ts
@@ -0,0 +1,14 @@
+import { TreeNode } from "../binary-tree-inorder-traversal/TreeNode.ts";
+import { level } from "../binary-tree-level-order-traversal-ii/level.ts";
+
+function levelOrder(root: TreeNode | null): number[] {
+    if (!root) {
+        return [];
+    }
+    const current: TreeNode[] = [root];
+    const result: number[] = [];
+
+    level(current, (r) => result.push(...r));
+    return result;
+}
+export default levelOrder;
