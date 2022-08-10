a71f03da01ba01c415c00321730a0fbada5e6ef3
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-10 16:49:52 +0800

    测试

--- convert-bst-to-greater-tree/index.ts
+++ convert-bst-to-greater-tree/index.ts
@@ -1,44 +1,20 @@
 import { TreeNode } from "../binary-tree-inorder-traversal/TreeNode.ts";
 
 export default function convertBST(root: TreeNode | null): TreeNode | null {
-    if (root != null) {
-        let result = bstToGst(root, { value: 0 });
-
-        while (typeof result === "function") {
-            // console.log(result.toString())
-            result = result();
+    let pre = 0;
+    let cur: TreeNode | null | undefined = root;
+    const stack: (TreeNode | null | undefined)[] = [];
+    while (cur || stack.length !== 0) {
+        while (cur) {
+            stack.push(cur);
+            cur = cur.right;
+        }
+        cur = stack.pop();
+        if (cur) {
+            cur.val += pre;
+            pre = cur.val;
+            cur = cur?.left;
         }
     }
-
     return root;
 }
-type CallBack = undefined | (() => CallBack);
-function bstToGst(
-    root: TreeNode | null,
-    sum: { value: number },
-    callback?: CallBack,
-): CallBack {
-    if (root != null) {
-        return () => {
-            return bstToGst(root.right, sum, () => {
-                sum.value += root.val;
-                root.val = sum.value;
-
-                return bstToGst(root.left, sum, callback);
-            });
-        };
-    }
-    return callback;
-}
-// let count = 1
-// const root = new TreeNode(1, new TreeNode(1), new TreeNode(1))
-// let temp = root
-// while (count < 70000) {
-//   temp.right = new TreeNode(1, new TreeNode(1), new TreeNode(1))
-//   temp = temp.right
-//   count++
-// }
-
-//   console.log(root)
-//   convertBST(root)
-//   console.log(root)
---
+++ convert-bst-to-greater-tree/test.ts
@@ -0,0 +1,53 @@
+import { assertEquals } from 'https://deno.land/std@0.151.0/testing/asserts.ts';
+import convertBST from "./index.ts";
+Deno.test("convert-bst-to-greater-tree", () => {
+    const inputs = [
+        {
+            val: 4,
+            left: {
+                val: 1,
+                left: { val: 0, left: null, right: null },
+                right: {
+                    val: 2,
+                    left: null,
+                    right: { val: 3, left: null, right: null },
+                },
+            },
+            right: {
+                val: 6,
+                left: { val: 5, left: null, right: null },
+                right: {
+                    val: 7,
+                    left: null,
+                    right: { val: 8, left: null, right: null },
+                },
+            },
+        },
+        { val: 0, left: null, right: { val: 1, left: null, right: null } },
+    ];
+    const outputs = [
+        {
+            val: 30,
+            left: {
+                val: 36,
+                left: { val: 36, left: null, right: null },
+                right: {
+                    val: 35,
+                    left: null,
+                    right: { val: 33, left: null, right: null },
+                },
+            },
+            right: {
+                val: 21,
+                left: { val: 26, left: null, right: null },
+                right: {
+                    val: 15,
+                    left: null,
+                    right: { val: 8, left: null, right: null },
+                },
+            },
+        },
+        { val: 1, left: null, right: { val: 1, left: null, right: null } },
+    ];
+    assertEquals(inputs.map((input, ) => convertBST(input)), outputs);
+});
