a5ff35be42ab4db50010bbf96d492c42cd51963c
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-11 13:50:09 +0800

    Create test.ts

---
+++ design-front-middle-back-queue/test.ts
@@ -0,0 +1,16 @@
+import FrontMiddleBackQueue from "./index.ts";
+import { assertEquals } from 'https://deno.land/std@0.151.0/testing/asserts.ts';
+Deno.test("design-front-middle-back-queue", () => {
+    const res: Array<number> = [];
+    const q: FrontMiddleBackQueue = new FrontMiddleBackQueue();
+    q.pushFront(1); // [1]
+    q.pushBack(2); // [1, 2]
+    q.pushMiddle(3); // [1, 3, 2]
+    q.pushMiddle(4); // [1, 4, 3, 2]
+    res.push(q.popFront()); // 返回 1 -> [4, 3, 2]
+    res.push(q.popMiddle()); // 返回 3 -> [4, 2]
+    res.push(q.popMiddle()); // 返回 4 -> [2]
+    res.push(q.popBack()); // 返回 2 -> []
+    res.push(q.popFront()); // 返回 -1 -> [] （队列为空）
+    assertEquals(res, [1, 3, 4, 2, -1]);
+});
