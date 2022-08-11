2f1a73f84dd58990f82290d8cdb8d7f7638426a1
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-11 11:57:54 +0800

    Update index.ts

--- er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/index.ts
+++ er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/index.ts
@@ -1,13 +1,3 @@
-// class TreeNode {
-//     val: number;
-//     left: TreeNode | null;
-//     right: TreeNode | null;
-//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-//         this.val = val === undefined ? 0 : val;
-//         this.left = left === undefined ? null : left;
-//         this.right = right === undefined ? null : right;
-//     }
-
 import { inorder } from "../binary-tree-inorder-traversal/inorder.ts";
 import { TreeNode } from "../binary-tree-inorder-traversal/TreeNode.ts";
 
@@ -32,9 +22,3 @@ export default function treeToDoublyList(
     current.right = tree;
     return tree;
 }
-// function inorder(root: TreeNode | null, output: (a: number) => void) {
-//     if (!root) return;
-//     inorder(root.left, output);
-//     output(root.val);
-//     inorder(root.right, output);
-// }
