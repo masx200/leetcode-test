dcca06dcfc8b16a6fe1895a62032e3e6cf867c79
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-12 16:48:24 +0800

    Update index.ts

--- hanota-lcci/index.ts
+++ hanota-lcci/index.ts
@@ -1,5 +1,7 @@
+// deno-lint-ignore-file
+// deno-lint-ignore ban-unused-ignore
 // deno-lint-ignore no-unused-vars
-export default function hanota(A: number[], B: number[], C: number[]): void {
+export default function hanota(A: number[], _B: number[], C: number[]): void {
     A.forEach((n) => C.push(n));
     A.length = 0;
 }
