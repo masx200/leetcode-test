53f74298421e81fe8fa03862ead52fa2b9e3b830
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-12 16:14:55 +0800

    测试

---
+++ iterator-for-combination/test.ts
@@ -0,0 +1,14 @@
+import CombinationIterator from "./index.ts";
+import { assertEquals } from "https://deno.land/std@0.151.0/testing/asserts.ts";
+
+Deno.test("iterator-for-combination", () => {
+    const res = [];
+    const iterator: CombinationIterator = new CombinationIterator("abc", 2); // 创建迭代器 iterator
+    res.push(iterator.next()); // 返回 "ab"
+    res.push(iterator.hasNext()); // 返回 true
+    res.push(iterator.next()); // 返回 "ac"
+    res.push(iterator.hasNext()); // 返回 true
+    res.push(iterator.next()); // 返回 "bc"
+    res.push(iterator.hasNext()); // 返回 false
+    assertEquals(res, ["ab", true, "ac", true, "bc", false]);
+});
---
+++ largest-triangle-area/test.ts
@@ -0,0 +1,21 @@
+import { assertEquals } from "https://deno.land/std@0.151.0/testing/asserts.ts";
+import largestTriangleArea from "./index.ts";
+Deno.test("largest-triangle-area", () => {
+    assertEquals(
+        [
+            [
+                [0, 0],
+                [0, 1],
+                [1, 0],
+                [0, 2],
+                [2, 0],
+            ],
+            [
+                [1, 0],
+                [0, 0],
+                [0, 1],
+            ],
+        ].map(largestTriangleArea),
+        [2.0, 0.5],
+    );
+});
