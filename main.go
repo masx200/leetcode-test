9e8e95d99af40c87a5001e376582e820727359d0
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-14 20:41:34 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"io/fs"
+	"os"
+	"os/exec"
+	"path"
+)
+
+func main() {
+
+	var matches, err = (fs.Glob(os.DirFS("./"), "./*/go.mod"))
+	if err != nil {
+		return
+	}
+
+	for _, m := range matches {
+		cmd := exec.Command("go", "test", "-v")
+		// fmt.Println(path.Dir(m), (cmd.Dir))
+		cmd.Dir = "./" + path.Dir(m)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		var err = cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
