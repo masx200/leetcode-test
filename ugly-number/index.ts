0e8c66da1d58975052a4075f3fa4e4641d7b4f30
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-14 21:24:14 +0800

    https://leetcode.cn/problems/ugly-number/

---
+++ ugly-number/index.ts
@@ -0,0 +1,13 @@
+function isUgly(n: number): boolean {
+    if (n <= 0) {
+        return false;
+    }
+
+    for (const factor of [2, 3, 5]) {
+        while (n % factor === 0) {
+            n /= factor;
+        }
+    }
+    return n == 1;
+}
+export default isUgly;
