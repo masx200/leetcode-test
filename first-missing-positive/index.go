0d1e030e32e0d5be5fe779ed85cd1867f91e344d
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-16 19:29:44 +0800

    优化

--- first-missing-positive/index.go
+++ first-missing-positive/index.go
@@ -3,6 +3,15 @@ package first_missing_positive
 import "sort"
 
 func firstMissingPositive(nums []int) int {
+
+	var temp = []int{}
+
+	for _, v := range nums {
+		if v > 0 && v <= len(nums) {
+			temp = append(temp, v)
+		}
+	}
+	nums = temp
 	sort.Ints(nums)
 	re := 1
 	for i := 0; i < len(nums); i++ {
