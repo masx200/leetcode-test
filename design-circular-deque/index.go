5ba62eda35c0abd96caf69b912ea9a940a2fe890
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-16 21:48:12 +0800

    Create index.go

---
+++ design-circular-deque/index.go
@@ -0,0 +1,64 @@
+package design_circular_deque
+
+type MyCircularDeque struct {
+	capacity int
+	deque    []int
+}
+
+func Constructor(k int) MyCircularDeque {
+	return MyCircularDeque{capacity: k}
+}
+
+func (d *MyCircularDeque) InsertFront(value int) bool {
+	if d.IsFull() {
+		return false
+	}
+	d.deque = append([]int{value}, d.deque...)
+	return true
+}
+
+func (d *MyCircularDeque) InsertLast(value int) bool {
+	if d.IsFull() {
+		return false
+	}
+	d.deque = append(d.deque, value)
+	return true
+}
+
+func (d *MyCircularDeque) DeleteFront() bool {
+	if d.IsEmpty() {
+		return false
+	}
+	d.deque = d.deque[1:]
+	return true
+}
+
+func (d *MyCircularDeque) DeleteLast() bool {
+	if d.IsEmpty() {
+		return false
+	}
+	d.deque = d.deque[:len(d.deque)-1]
+	return true
+}
+
+func (d *MyCircularDeque) GetFront() int {
+	if d.IsEmpty() {
+		return -1
+	}
+	return d.deque[0]
+}
+
+func (d *MyCircularDeque) GetRear() int {
+	if d.IsEmpty() {
+		return -1
+	}
+	return d.deque[len(d.deque)-1]
+}
+
+func (d *MyCircularDeque) IsEmpty() bool {
+	return len(d.deque) == 0
+}
+
+func (d *MyCircularDeque) IsFull() bool {
+	return len(d.deque) >= d.capacity
+}
