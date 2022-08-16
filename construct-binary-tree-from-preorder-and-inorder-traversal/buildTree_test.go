9666a0965a5e1ec5b5429ede94903d4c1e75f8f7
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-16 12:38:54 +0800

    Update buildTree_test.go

--- construct-binary-tree-from-preorder-and-inorder-traversal/buildTree_test.go
+++ construct-binary-tree-from-preorder-and-inorder-traversal/buildTree_test.go
@@ -9,4 +9,7 @@ import (
 func TestBuildTree(t *testing.T) {
 
 	assert.DeepEqual(t, &TreeNode{Val: -1}, buildTree([]int{-1}, []int{-1}))
+
+	assert.DeepEqual(t, &TreeNode{Val: 3, Left: &TreeNode{Val: 9},
+		Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}}}, buildTree([]int{3, 9, 20, 15, 7}, []int{9, 3, 15, 20, 7}))
 }
