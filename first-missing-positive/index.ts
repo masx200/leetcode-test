2e927a6e515b122a5b59f6b18f17d04aac430215
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-16 19:24:45 +0800

    格式化

--- first-missing-positive/index.ts
+++ first-missing-positive/index.ts
@@ -1,12 +1,11 @@
 function firstMissingPositive(nums: number[]): number {
-nums=nums.filter(a=>a>0&&a<=nums.length).sort((a,b)=>a-b)
-let re=1
-for(const v of nums){
-    if(v===re){
-        re++
-    
+    nums = nums.filter((a) => a > 0 && a <= nums.length).sort((a, b) => a - b);
+    let re = 1;
+    for (const v of nums) {
+        if (v === re) {
+            re++;
+        }
     }
-}
-return re
+    return re;
 }
 export default firstMissingPositive;
