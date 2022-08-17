ff4bd4a150821e83c93006d06ecb0f38580e9fee
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-17 23:17:05 +0800

    Update check.ts

--- check.ts
+++ check.ts
@@ -44,7 +44,7 @@ async function parallel_check(
 }
 export function split_by_count<T>(files: T[], limit: number) {
     return Object.values(
-        group(files, (_s: any, i: number) => i % (files.length / limit))
+        group(files, (_s: any, i: number) => i % Math.floor(files.length / limit))
     ) as T[][];
 }
 
