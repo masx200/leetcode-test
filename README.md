fea0d41817850d655a3915c0299a5951c6b1bb1b
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-17 09:05:50 +0800

    https://leetcode.cn/problems/deepest-leaves-sum/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/deepest-leaves-sum/
+
 https://leetcode.cn/problems/decode-ways
 
 https://leetcode.cn/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof/
---
+++ deepest-leaves-sum/index.ts
@@ -0,0 +1,20 @@
+import { TreeNode } from "../binary-tree-inorder-traversal/TreeNode.ts";
+
+export default function deepestLeavesSum(root: TreeNode | null): number {
+    if (!root) return 0;
+    let cur = [root];
+
+    while (cur.length) {
+        const temp: typeof cur = [];
+
+        cur.map((n) => [n.left, n.right].filter(Boolean)).forEach((a) =>
+            temp.push(...(a as TreeNode[]))
+        );
+
+        if (!temp.length) {
+            return cur.reduce((a, v) => a + v.val, 0);
+        }
+        cur = temp;
+    }
+    return 0;
+}
