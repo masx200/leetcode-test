034275888788b09886b98536ed445c87a5e27cf6
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-17 22:06:24 +0800

    Update check.ts

--- check.ts
+++ check.ts
@@ -6,10 +6,10 @@ import { WalkEntry } from "https://deno.land/std@0.152.0/fs/_util.ts";
 function searchFilesNames({
     skip,
 }: // limiter,
-    {
-        skip?: RegExp | RegExp[];
-        // limiter: AsyncCurrentLimiter;
-    }) {
+{
+    skip?: RegExp | RegExp[];
+    // limiter: AsyncCurrentLimiter;
+}) {
     console.log("type check start!");
 
     const entry_iter = walk(".", {
@@ -27,19 +27,17 @@ if (import.meta.main) {
 
 async function parallel_check(
     entry_iter: AsyncIterableIterator<WalkEntry>,
-    limiter: AsyncCurrentLimiter,
+    limiter: AsyncCurrentLimiter
 ) {
     const stack: string[] = [];
 
     const ps: Array<Promise<void>> = [];
     for await (const entry of entry_iter) {
-        console.log(entry.path);
         if (stack.length < 15) {
             stack.push(entry.path);
         } else {
             ps.push(limiter.run(() => runDenoCheck([...stack, entry.path])));
             stack.length = 0;
-            // console.log({ status, stdout, stderr });
         }
     }
     if (stack.length) {
@@ -52,11 +50,12 @@ async function start() {
     const limiter = new AsyncLimiterClass(navigator.hardwareConcurrency);
     const args = parse(Deno.args);
     console.log(args);
-    const skip = typeof args.skip === "string"
-        ? new RegExp(String(args.skip))
-        : Array.isArray(args.skip)
-        ? args.skip.map((s) => new RegExp(s))
-        : undefined;
+    const skip =
+        typeof args.skip === "string"
+            ? new RegExp(String(args.skip))
+            : Array.isArray(args.skip)
+            ? args.skip.map((s) => new RegExp(s))
+            : undefined;
     const entry_iter = searchFilesNames({ skip });
     await parallel_check(entry_iter, limiter);
     console.log("type check Done!");
@@ -64,6 +63,7 @@ async function start() {
 
 async function runDenoCheck(stack: string[]) {
     const cmd = ["deno", "check", "--unstable", ...stack];
+    console.log(cmd);
     const process = Deno.run({
         cmd: cmd,
         stderr: "piped",
@@ -77,7 +77,7 @@ async function runDenoCheck(stack: string[]) {
     const decoder = new TextDecoder();
     const out = decoder.decode(stdout);
     const err = decoder.decode(stderr);
-    console.log(cmd, status, out, err);
+    console.log(status, out, err);
     process.close();
     if (!status.success) {
         throw new Error("type check failed:" + out + err);
