320efd7f6e13894812b89f2e47c030c56ed6668a
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-18 16:41:12 +0800

    Update index.go

--- implement-trie-prefix-tree/index.go
+++ implement-trie-prefix-tree/index.go
@@ -1,26 +1,23 @@
 package implement_trie_prefix_tree
-
-type PrefixTree struct {
-	isEnd    bool
-	children map[byte]*PrefixTree
-	rest     string
-}
 type Trie struct {
-	root *PrefixTree
+
 }
+
 
 func Constructor() Trie {
 
-	return Trie{root: &PrefixTree{children: make(map[byte]*PrefixTree)}}
 }
 
-func (this *Trie) Insert(word string) {
+
+func (this *Trie) Insert(word string)  {
 
 }
+
 
 func (this *Trie) Search(word string) bool {
 
 }
+
 
 func (this *Trie) StartsWith(prefix string) bool {
 
