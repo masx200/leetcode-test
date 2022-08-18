5bf34f8067344fb42ba5774165ad10156af196d0
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-18 10:10:54 +0800

    Create split_by_count.ts

---
+++ utils/split_by_count.ts
@@ -0,0 +1,10 @@
+import { group } from "../deps.ts";
+
+export function split_by_count<T>(files: T[], limit: number) {
+    return Object.values(
+        group(
+            files,
+            (_s: any, i: number) => i % Math.floor(files.length / limit)
+        )
+    ) as T[][];
+}
