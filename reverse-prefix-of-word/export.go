e421ddb330adb18a747245370e7151152dcc78f6
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-18 12:33:53 +0800

    reversePrefix

---
+++ reverse-prefix-of-word/export.go
@@ -0,0 +1,5 @@
+package index
+
+func ReversePrefix(word string, ch byte) string {
+	return	reversePrefix(word,ch)
+}
---
+++ reverse-prefix-of-word/index.go
@@ -0,0 +1,13 @@
+package index
+
+import "strings"
+import "github.com/webx-top/com"
+
+func reversePrefix(word string, ch byte) string {
+	index := strings.IndexByte(word, ch)
+	if index < 0 {
+		return word
+	}
+
+	return com.Reverse(word[0:index+1]) + string(word[index+1:])
+}
