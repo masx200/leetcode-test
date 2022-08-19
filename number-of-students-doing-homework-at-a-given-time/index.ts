b9562a0f2a054a5803f2f781b5c2e11d6e0ed64a
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-19 17:32:55 +0800

    Update index.ts

--- number-of-students-doing-homework-at-a-given-time/index.ts
+++ number-of-students-doing-homework-at-a-given-time/index.ts
@@ -1,10 +1,9 @@
-export default function busyStudent(startTime: number[], endTime: number[], queryTime: number): number {
-
-
-
-return startTime.map((s,i)=>Number(queryTime<=endTime[i]&&queryTime>=s)).reduce((a,v)=>a+v,0)
-
-
-
-
+export default function busyStudent(
+    startTime: number[],
+    endTime: number[],
+    queryTime: number,
+): number {
+    return startTime.map((s, i) =>
+        Number(queryTime <= endTime[i] && queryTime >= s)
+    ).reduce((a, v) => a + v, 0);
 }
