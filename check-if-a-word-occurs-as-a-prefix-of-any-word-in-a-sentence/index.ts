e0f903b29481e9c053e147f49d0cfae64c93fe41
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-20 20:32:29 +0800

    https://leetcode.cn/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/

---
+++ check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/index.ts
@@ -0,0 +1,10 @@
+export default function isPrefixOfWord(
+    sentence: string,
+    searchWord: string,
+): number {
+    const index = sentence.split(" ").findIndex((a) =>
+        a.startsWith(searchWord)
+    );
+
+    return index < 0 ? index : index + 1;
+}
