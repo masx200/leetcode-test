e3a41188540475cdbe4e8f8b55a216f1a4178502
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-20 22:25:20 +0800

    测试

--- check-completeness-of-a-binary-tree/test.ts
+++ check-completeness-of-a-binary-tree/test.ts
@@ -3,6 +3,7 @@ import isCompleteTree from "./index.ts";
 import { TreeNodeLeetCodeParse as deserialize } from "../utils/TreeNodeLeetCodeParse.ts";
 Deno.test("check-completeness-of-a-binary-tree", () => {
     const inputs = [
+        "[1,2,3,4,5,6,7]",
         "[1,2,3,4,5,6]",
         "[1,2,3,4,5,null,7]",
         "[1,2,3,5,null,7,8]",
@@ -10,12 +11,12 @@ Deno.test("check-completeness-of-a-binary-tree", () => {
         "[1,2,3,4,5,6]",
         "[1,2,3,4,5,6,7,8,9,10,11,12,13,null,null,15]",
     ];
-    const outputs = [true, false, false, false, true, false];
+    const outputs = [true, true, false, false, false, true, false];
     assertEquals(
         inputs.map((input) => {
             const root = deserialize(input);
             return isCompleteTree(root);
         }),
-        outputs
+        outputs,
     );
 });
