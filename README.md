f1448871a58aba6b90dd7dae6f15641c0f3815b5
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-24 09:25:03 +0800

    https://leetcode.cn/problems/make-two-arrays-equal-by-reversing-sub-arrays/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/make-two-arrays-equal-by-reversing-sub-arrays/
+
 https://leetcode.cn/problems/transform-to-chessboard/
 
 https://leetcode.cn/problems/parse-lisp-expression/
---
+++ make-two-arrays-equal-by-reversing-sub-arrays/index.ts
@@ -0,0 +1,12 @@
+import { counter } from "../mod.ts";
+
+export default function canBeEqual(target: number[], arr: number[]): boolean {
+    return MapisEqual(counter(target), counter(arr));
+}
+
+function MapisEqual(a: Map<number, number>, b: Map<number, number>) {
+    return a === b || a.size === b.size &&
+            Array.from(a.keys()).every((k) =>
+                b.has(k) && a.get(k) === b.get(k)
+            );
+}
