6ef415d2bd16ee4f0e984e4b19d3675a8ad92adb
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-29 14:29:23 +0800

    测试

--- design-a-food-rating-system/test.ts
+++ design-a-food-rating-system/test.ts
@@ -1,3 +1,4 @@
+import { runScript } from "leetcode-class";
 import { assertEquals } from "../deps.ts";
 import FoodRatings from "./index.ts";
 
@@ -7,15 +8,39 @@ Deno.test("design-a-food-rating-system", () => {
         ["korean", "japanese", "japanese", "greek", "japanese", "korean"],
         [9, 12, 8, 15, 14, 7],
     );
-    assertEquals("kimchi", foodRatings.highestRated("korean")) // 返回 "kimchi"
+    assertEquals("kimchi", foodRatings.highestRated("korean")); // 返回 "kimchi"
     // "kimchi" 是分数最高的韩式料理，评分为 9 。
     assertEquals("ramen", foodRatings.highestRated("japanese")); // 返回 "ramen"
     // "ramen" 是分数最高的日式料理，评分为 14 。
     foodRatings.changeRating("sushi", 16); // "sushi" 现在评分变更为 16 。
-    assertEquals("sushi" ,foodRatings.highestRated("japanese")); // 返回 "sushi"
+    assertEquals("sushi", foodRatings.highestRated("japanese")); // 返回 "sushi"
     // "sushi" 是分数最高的日式料理，评分为 16 。
     foodRatings.changeRating("ramen", 16); // "ramen" 现在评分变更为 16 。
-    assertEquals("ramen",  foodRatings.highestRated("japanese")); // 返回 "ramen"
+    assertEquals("ramen", foodRatings.highestRated("japanese")); // 返回 "ramen"
     // "sushi" 和 "ramen" 的评分都是 16 。
     // 但是，"ramen" 的字典序比 "sushi" 更小。
+});
+Deno.test("design-a-food-rating-system", () => {
+    const c = [
+        "FoodRatings",
+        "changeRating",
+        "changeRating",
+        "changeRating",
+        "highestRated",
+    ];
+    const i = [
+        [["shjnfxk", "dmqitcmuc", "jeegkoabpu", "ammxyb", "vjxqrvfnxv"], [
+            "yrfziuszqu",
+            "yrfziuszqu",
+            "yrfziuszqu",
+            "yrfziuszqu",
+            "yrfziuszqu",
+        ], [12, 19, 13, 12, 1]],
+        ["ammxyb", 17],
+        ["dmqitcmuc", 9],
+        ["vjxqrvfnxv", 4],
+        ["yrfziuszqu"],
+    ];
+    const o = [null, null, null, null, "ammxyb"];
+    assertEquals(runScript(c, i, [FoodRatings]), o);
 });
