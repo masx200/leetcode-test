9b5fb552fc5855f2a34d8a888730bf3cf830fef7
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-29 10:21:57 +0800

    Update index.ts

--- last-stone-weight/index.ts
+++ last-stone-weight/index.ts
@@ -1,14 +1,14 @@
 import { PriorityQueue } from "../kth-largest-element-in-a-stream/PriorityQueue.ts";
 
 export default function lastStoneWeight(stones: number[]): number {
-    const maxheap = PriorityQueue<number>((a, b) => a - b);
+    const maxheap = PriorityQueue<number>((a, b) =>- a + b);
     stones.forEach((stone) => maxheap.offer(stone));
     while (maxheap.length() >= 2) {
-        const y = maxheap.pop();
-        const x = maxheap.pop();
+        const y = maxheap.shift();
+        const x = maxheap.shift();
         if (typeof y === "number" && typeof x === "number" && x !== y) {
             maxheap.offer(y - x);
         }
     }
-    return maxheap.tail() ?? 0;
+    return maxheap.head() ?? 0;
 }
