9284c769e03a13e6989368255aaf5b5f9a6274b1
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-30 10:23:04 +0800

    Create test.ts

---
+++ design-parking-system/test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "asserts";
+import { runScript } from "leetcode-class";
+import ParkingSystem from "./index.ts";
+Deno.test("design-parking-system", () => {
+    assertEquals(
+        runScript(
+            ["ParkingSystem", "addCar", "addCar", "addCar", "addCar"],
+            [[1, 1, 0], [1], [2], [3], [1]],
+
+            [ParkingSystem]
+        ),
+        [null, true, true, false, false]
+    );
+});
