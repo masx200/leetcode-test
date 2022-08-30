2c7bb877dd92d298daacc8e1630b8b5e8ab28653
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-30 10:12:52 +0800

    Create test.ts

---
+++ design-hashset/test.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from "asserts";
+import { runScript } from "leetcode-class";
+import MyHashSet from "./index.ts";
+Deno.test("design-hashset", () => {
+    assertEquals(
+        runScript(
+            [
+                "MyHashSet",
+                "add",
+                "add",
+                "contains",
+                "contains",
+                "add",
+                "contains",
+                "remove",
+                "contains",
+            ],
+            [[], [1], [2], [1], [3], [2], [2], [2], [2]],
+            [MyHashSet]
+        ),
+        [null, null, null, true, false, null, true, null, false]
+    );
+});
