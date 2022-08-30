2b1b8de458a45f828bc1a4555d765e5e5f64da41
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-30 09:55:31 +0800

    Create test.ts

---
+++ design-browser-history/test.ts
@@ -0,0 +1,49 @@
+Deno.test("design-browser-history", () => {
+    assertEquals(
+        runScript(
+            [
+                "BrowserHistory",
+                "visit",
+                "visit",
+                "visit",
+                "back",
+                "back",
+                "forward",
+                "visit",
+                "forward",
+                "back",
+                "back",
+            ],
+            [
+                ["leetcode.com"],
+                ["google.com"],
+                ["facebook.com"],
+                ["youtube.com"],
+                [1],
+                [1],
+                [1],
+                ["linkedin.com"],
+                [2],
+                [2],
+                [7],
+            ],
+            [BrowserHistory]
+        ),
+        [
+            null,
+            null,
+            null,
+            null,
+            "facebook.com",
+            "google.com",
+            "facebook.com",
+            null,
+            "linkedin.com",
+            "google.com",
+            "leetcode.com",
+        ]
+    );
+});
+import { assertEquals } from "asserts";
+import { runScript } from "leetcode-class";
+import BrowserHistory from "./index.ts";
