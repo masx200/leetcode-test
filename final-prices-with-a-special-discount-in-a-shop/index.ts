81cca75a5b8e7936355a9df9ebda2935c508a602
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-01 07:57:42 +0800

    格式化

--- final-prices-with-a-special-discount-in-a-shop/index.ts
+++ final-prices-with-a-special-discount-in-a-shop/index.ts
@@ -1,17 +1,14 @@
 export default function finalPrices(prices: number[]): number[] {
-
     const n = prices.length;
-    const ans : number[] = Array.from(prices)
-    const stack: number[]  = [];
+    const ans: number[] = Array.from(prices);
+    const stack: number[] = [];
     for (let i = n - 1; i >= 0; i--) {
         while (stack.length && stack[stack.length - 1] > prices[i]) {
             stack.pop();
         }
-        
-        if(stack.length){ans[i]-= stack[stack.length - 1];
-        }
+
+        if (stack.length) ans[i] -= stack[stack.length - 1];
         stack.push(prices[i]);
     }
     return ans;
-
 }
--- intersection-of-two-arrays/index.ts
+++ intersection-of-two-arrays/index.ts
@@ -1,4 +1,7 @@
-export default function intersection(nums1: number[], nums2: number[]): number[] {
-const set=new Set(nums2)
-return Array.from(new Set(nums1.filter(a=>set.has(a))))
+export default function intersection(
+    nums1: number[],
+    nums2: number[],
+): number[] {
+    const set = new Set(nums2);
+    return Array.from(new Set(nums1.filter((a) => set.has(a))));
 }
