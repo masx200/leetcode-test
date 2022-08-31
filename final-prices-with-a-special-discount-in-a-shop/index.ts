4b82f669a2e0d32297884c363ba42ac1dca62a74
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-31 23:51:23 +0800

    Create index.ts

---
+++ final-prices-with-a-special-discount-in-a-shop/index.ts
@@ -0,0 +1,17 @@
+export default function finalPrices(prices: number[]): number[] {
+
+    const n = prices.length;
+    const ans : number[] = Array.from(prices)
+    const stack: number[]  = [];
+    for (let i = n - 1; i >= 0; i--) {
+        while (stack.length && stack[stack.length - 1] > prices[i]) {
+            stack.pop();
+        }
+        
+        if(stack.length){ans[i]-= stack[stack.length - 1];
+        }
+        stack.push(prices[i]);
+    }
+    return ans;
+
+}
