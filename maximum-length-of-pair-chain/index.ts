199f6913d127c316af02eb63d336270d7c7561e1
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-03 07:06:16 +0800

    Create index.ts

---
+++ maximum-length-of-pair-chain/index.ts
@@ -0,0 +1,12 @@
+export default function findLongestChain(pairs: number[][]): number {
+
+    let curr = -Infinity, res = 0;
+    pairs.sort((a, b) => a[1] - b[1]);
+    for (const p of pairs) {
+        if (curr < p[0]) {
+            curr = p[1];
+            res++;
+        }
+    }
+    return res;
+}
