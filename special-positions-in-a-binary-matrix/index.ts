e2d215a0c32732a38b59bb60ea64a7456d1151f7
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-04 13:58:48 +0800

    Update index.ts

--- special-positions-in-a-binary-matrix/index.ts
+++ special-positions-in-a-binary-matrix/index.ts
@@ -1,18 +1,14 @@
-
 export default numSpecial;
 function numSpecial(mat: number[][]): number {
-const rowsum=mat.map(a=>a.reduce((q,v)=>q+v,0))
-const colsum=mat[0].map((_,i)=>mat.reduce((q,v)=>q+v[i],0))
+    const rowsum = mat.map((a) => a.reduce((q, v) => q + v, 0));
+    const colsum = mat[0].map((_, i) => mat.reduce((q, v) => q + v[i], 0));
 
-let ans=0
+    let ans = 0;
 
+    mat.forEach((a, i) =>
+        rowsum[i] === 1 &&
+        a.forEach((v, j) => ans += Number(v === 1 && colsum[j] === 1))
+    );
 
-mat.forEach((a,i)=>rowsum[i]===1&&a.forEach((v,j)=>
-
-ans+=Number(v===1&&colsum[j]===1)
-
-))
-
-
-return ans
+    return ans;
 }
