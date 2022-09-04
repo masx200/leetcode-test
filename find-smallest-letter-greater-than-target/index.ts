21da3be9308267bba98570a609fa58dcf2c91b07
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-04 15:41:33 +0800

    Update index.ts

--- find-smallest-letter-greater-than-target/index.ts
+++ find-smallest-letter-greater-than-target/index.ts
@@ -1,5 +1,20 @@
 export default function nextGreatestLetter(
     letters: string[],
-    target: string,
+    target: string
 ): string {
+    const length = letters.length;
+    if (target >= letters[length - 1]) {
+        return letters[0];
+    }
+    let low = 0,
+        high = length - 1;
+    while (low < high) {
+        const mid = Math.floor((high + low) / 2);
+        if (letters[mid] > target) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return letters[low];
 }
