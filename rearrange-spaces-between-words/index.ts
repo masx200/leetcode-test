932a2517145c98b173f9e56314eacb779def13bd
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-07 10:31:12 +0800

    https://leetcode.cn/problems/rearrange-spaces-between-words/

---
+++ rearrange-spaces-between-words/index.ts
@@ -0,0 +1,17 @@
+// deno-lint-ignore-file ban-ts-comment
+export default function reorderSpaces(text: string): string {
+    const words = text.split(/\s+/g).filter(Boolean);
+
+    //@ts-ignore
+    const spaces = Array.prototype.reduce.call(
+        text,
+        //@ts-ignore
+        (a, v) => a + Number(v === " "),
+        0
+    ) as number;
+
+    if (words.length <= 1) return words.join("") + " ".repeat(spaces);
+    const sep = " ".repeat(Math.floor(spaces / (words.length - 1)));
+
+    return words.join(sep) + " ".repeat(spaces % (words.length - 1));
+}
