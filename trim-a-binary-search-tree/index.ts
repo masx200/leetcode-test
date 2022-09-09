2c30297e440d216bee3b1f29fb88a6242a9ab1cb
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-09 23:09:51 +0800

    Create index.ts

---
+++ trim-a-binary-search-tree/index.ts
@@ -0,0 +1,12 @@
+import {TreeNode}from"../mod.ts"
+export default trimBST
+function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | null {
+if(!root)return null
+
+if(root.val<low)return trimBST(root.right,low,high)
+
+
+if(root.val>high)return trimBST(root.left,low,high)
+
+return new TreeNode(root.val, trimBST(root.left,low,high), trimBST(root.right,low,high))
+}
