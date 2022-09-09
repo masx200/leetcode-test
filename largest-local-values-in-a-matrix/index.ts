92acb07adcc3d158dcd1049d1f7fecd63bf1222e
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-09 14:13:09 +0800

    格式化

--- largest-local-values-in-a-matrix/index.ts
+++ largest-local-values-in-a-matrix/index.ts
@@ -1,40 +1,29 @@
 export default function largestLocal(grid: number[][]): number[][] {
-const dirs = [ 
+    const dirs = [
+        [-1, -1],
 
-[-1, -1], 
+        [-1, 0],
 
-[-1, 0], 
+        [-1, 1],
 
-[-1, 1], 
+        [0, -1],
 
-[0, -1], 
+        [0, 1],
 
-[0, 1], 
+        [1, -1],
 
-[1, -1], 
+        [1, 0],
 
-[1, 0], 
+        [1, 1],
+    ];
 
-[1, 1], 
-
-]; 
-
-return new Array(grid.length-2).fill(0).map((_, i) => 
-
-Array(grid.length-2).fill(0).map((_, j) => { 
-
-
-return Math.max(
-grid[i+1][j+1], 
-
-...dirs 
-
-.map(([r, c]) => grid[i +1+ r]?.[j+1 + c]) 
-); 
-
-}) 
-
-);
-
-
+    return new Array(grid.length - 2).fill(0).map((_, i) =>
+        Array(grid.length - 2).fill(0).map((_, j) => {
+            return Math.max(
+                grid[i + 1][j + 1],
+                ...dirs
+                    .map(([r, c]) => grid[i + 1 + r]?.[j + 1 + c]),
+            );
+        })
+    );
 }
