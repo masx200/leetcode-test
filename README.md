0ee16d72113b532f8149e39700c7320b0abee2fb
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-10 11:12:51 +0800

    https://leetcode.cn/problems/binary-tree-paths/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/binary-tree-paths/
+
 https://leetcode-cn.com/problems/trim-a-binary-search-tree/
 
 https://leetcode.cn/problems/course-schedule/
---
+++ binary-tree-paths/index.ts
@@ -0,0 +1,23 @@
+import { TreeNode } from "../binary-tree-inorder-traversal/TreeNode.ts";
+
+export default function binaryTreePaths(root: TreeNode | null): string[] {
+    const res: string[] = [];
+    if (!root) {
+        return res;
+    }
+    const path: number[] = [];
+    dfs(root, res, path);
+    return res;
+}
+function dfs(root: TreeNode, res: string[], path: number[]) {
+    path.push(root.val);
+    if (!root.left && !root.right) {
+        res.push(path.join("->"));
+        return;
+    }
+
+    for (const node of [root.left, root.right].filter(Boolean) as TreeNode[]) {
+        dfs(node, res, path);
+        path.pop();
+    }
+}
