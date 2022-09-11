89f816b1eaa08bf9761da3a3d5068058f496434b
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-11 11:14:37 +0800

    Create index.ts

---
+++ check-distances-between-same-letters/index.ts
@@ -0,0 +1,20 @@
+export default function checkDistances(s: string, distance: number[]): boolean {
+const mp=new Map<number,number>()
+
+for(const [i,c]of Array.prototype.entries.call(s)){
+    
+    
+    
+    const key=c.charCodeAt(0)-"a".charCodeAt(0)
+    const last=mp.get(key)
+    
+    if(mp.has(key)&&typeof last !=="undefined"&&i-last-1!==distance[key]){
+        
+        return false
+        
+    }else{
+        mp.set(key,i)
+    }
+}
+return true
+}
