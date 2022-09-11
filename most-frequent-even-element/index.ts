01125115008c05019033cc3a571ec7dd49ae53b9
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-11 15:57:41 +0800

    https://leetcode.cn/problems/most-frequent-even-element/

---
+++ most-frequent-even-element/index.ts
@@ -0,0 +1,12 @@
+import { counter } from "../substring-with-concatenation-of-all-words/counter.ts";
+export default mostFrequentEven;
+function mostFrequentEven(nums: number[]): number {
+    const evens = nums.filter((a) => !(a % 2));
+    if (evens.length === 0) return -1;
+    const cnt = counter(evens);
+
+    const freq = Math.max(...cnt.values());
+    return Math.min(
+        ...[...cnt.entries()].filter(([_k, v]) => v === freq).map((a) => a[0]),
+    );
+}
