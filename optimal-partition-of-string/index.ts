33861e91a585d442a16d123cced5cbfac0c8f662
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-12 13:14:57 +0800

    https://leetcode.cn/problems/optimal-partition-of-string/

---
+++ optimal-partition-of-string/index.ts
@@ -0,0 +1,16 @@
+export default function partitionString(s: string): number {
+    let set = 0;
+    let res = 0;
+    for (const c of s) {
+        if (
+            set & (1 << c.charCodeAt(0) - "a".charCodeAt(0))
+        ) {
+            res++;
+
+            set = 0;
+        }
+
+        set |= 1 << c.charCodeAt(0) - "a".charCodeAt(0);
+    }
+    return res + 1;
+}
