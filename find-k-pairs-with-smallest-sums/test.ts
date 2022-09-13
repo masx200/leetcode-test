b8c92027dbfc2e2b08169b381bf18a48d06adebc
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-13 16:00:00 +0800

    Create test.ts

---
+++ find-k-pairs-with-smallest-sums/test.ts
@@ -0,0 +1,16 @@
+import { assertEquals } from "https://deno.land/std@0.155.0/testing/asserts.ts";
+import kSmallestPairs from "./index.ts";
+Deno.test("find-k-pairs-with-smallest-sums", () => {
+    assertEquals(
+        [[1, 2], [1, 4], [1, 6]],
+        kSmallestPairs([1, 7, 11], [2, 4, 6], 3),
+    );
+    assertEquals(
+        [[1, 1], [1, 1]],
+        kSmallestPairs([1, 1, 2], [1, 2, 3], 2),
+    );
+    assertEquals(
+        [[1, 3], [2, 3]],
+        kSmallestPairs([1, 2], [3], 3),
+    );
+});
