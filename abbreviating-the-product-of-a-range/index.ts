f61765c18693c2abd54c4d8c914d28c9bae8bdc3
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-13 23:33:25 +0800

    Update index.ts

--- abbreviating-the-product-of-a-range/index.ts
+++ abbreviating-the-product-of-a-range/index.ts
@@ -1,7 +1,29 @@
-export default function abbreviateProduct(left: number, right: number): string {
-    const a = Array(right - left + 1).fill(0).map((_, i) =>
-        BigInt(i) + BigInt(left)
-    ).reduce((a, v) => a * v).toString();
+export default  abbreviateProduct
+
+const factorials:bigint[]=[]
+function init(){
+    
+    if(factorials.length===0){
+        for (let i=0;i<=10000;i++){
+            
+            factorials[i]=i===0?1n:BigInt(i)*factorials[i-1]
+                
+            
+            
+            
+        }
+    }
+}
+
+function abbreviateProduct(left: number, right: number): string {
+    
+    
+    init()
+    
+    
+    const a=(left===right?left:factorials[right]/factorials[left-1]).toString()
+    
+    
     //@ts-ignore
     const index = Array.prototype.findLastIndex.call(a, (v) => v !== "0");
 
