402f86bc1a6302d3c45c767b58bd466b1500f1d8
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-14 13:28:38 +0800

    Update index.ts

--- guess-the-word/index.ts
+++ guess-the-word/index.ts
@@ -1,16 +1,18 @@
 function findSecretWord(words: string[], master: Master) {
+    const seen = new Set();
     while (true) {
         const j = Math.floor(Math.random() * words.length);
         const w = words[j];
-
+        seen.add(w);
         const d = master.guess(w);
         if (d === 6) return;
         if (d >= 0) {
             words = words.filter((v) =>
+                !seen.has(v) &&
                 d === Array.prototype.filter.call(
-                    v,
-                    (c, k) => w[k] === c,
-                ).length
+                        v,
+                        (c, k) => w[k] === c,
+                    ).length
             );
         }
     }
