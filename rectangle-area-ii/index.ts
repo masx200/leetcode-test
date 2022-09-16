20c0f52ff8e22cd2ba29a7905f4c2fb4ce87e5a2
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-16 12:14:32 +0800

    Update index.ts

--- rectangle-area-ii/index.ts
+++ rectangle-area-ii/index.ts
@@ -1,68 +1,74 @@
 function rectangleArea(rectangles: number[][]): number {
-    const root = new Node();
     const left = Math.min(...rectangles.map((a) => a[0]));
     const down = Math.min(...rectangles.map((a) => a[1]));
     const right = Math.max(...rectangles.map((a) => a[2]));
     const up = Math.max(...rectangles.map((a) => a[3]));
 
-    const total = ({ left, right, down, up });
+    const total = { left, right, down, up };
+    const root = new Node(total);
     // console.log(root, total);
     for (const [left, down, right, up] of rectangles) {
-        change(root, total, { left, right, down, up }, 1n);
+        change(root, /*  total, */ { left, right, down, up }, 1n);
         // console.log(root, total);
     }
-    // console.log(root, total);
-    // console.log(
-    //     JSON.stringify(
-    //         root,
-    //         (_k, v) => typeof v === "bigint" ? v.toString() : v,
-    //         4,
-    //     ),
-    // );
+
     // console.log(root)
     return Number(root.value % BigInt(10 ** 9 + 7));
 }
 function change(
     node: Node,
-    current: Interval,
+    // current: Interval,
     target: Interval,
     value: bigint,
 ) {
+    const current = node.interval;
     if (
-        target.up < current.down || target.right < current.left ||
-        target.down > current.up || target.left > current.right
-    ) return;
+        target.up < current.down ||
+        target.right < current.left ||
+        target.down > current.up ||
+        target.left > current.right
+    ) {
+        return;
+    }
     // console.log({ node, current, target, value });
-    const subinterval = node.intervals.length
-        ? node.intervals
-        : binarySplit(current);
-    node.intervals = subinterval;
+    const subinterval = binarySplit(current);
+
     if (
-        target.left <= current.left && target.down <= current.down &&
-        target.right >= current.right && target.up >= current.up
+        target.left <= current.left &&
+        target.down <= current.down &&
+        target.right >= current.right &&
+        target.up >= current.up
     ) {
         // console.log("找到了包含target的范围了");
         if (node.children.length === 0) {
-            node.value = value * BigInt(current.right - current.left) *
+            node.value = value *
+                BigInt(current.right - current.left) *
                 BigInt(current.up - current.down);
             return;
         }
     } else {
         if (node.children.length === 0) {
             if (subinterval.length) {
-                node.children = subinterval.map((c) =>
-                    new Node(
-                        BigInt(c.right - c.left) *
-                            BigInt(c.up - c.down) * BigInt(node.value > 0),
-                    )
+                node.children = subinterval.map(
+                    (c) =>
+                        new Node(
+                            c,
+                            BigInt(c.right - c.left) *
+                                BigInt(c.up - c.down) *
+                                BigInt(node.value > 0),
+                        ),
                 );
             }
             // console.log("创建子节点", node.value, node.children);
         }
     }
     if (subinterval.length) {
-        for (const [index, next] of subinterval.entries()) {
-            change(node.children[index], next, target, value);
+        // for (const [index, next] of subinterval.entries()) {
+        //     change(node.children[index], /* next, */ target, value);
+        // }
+
+        for (const child of node.children) {
+            change(child, target, value);
         }
         //可能没有子节点
         node.value = node.children.length
@@ -79,27 +85,38 @@ export function binarySplit(current: Interval): Interval[] {
     const mx = Math.floor((left + right) / 2);
     const my = Math.floor((down + up) / 2);
 
-    const lr = [{ left: left, right: mx }, { left: mx, right: right }];
-    const du = [{ down: down, up: my }, { down: my, up: up }];
-    const result = lr.map(({ left, right }) =>
-        du.map(({ up, down }) => ({ left, right, down, up }))
-    ).flat();
-    return result.filter(({ left, right, up, down }) =>
-        left <= right && down <= up && (right - left) * (up - down)
+    const lr = [
+        { left: left, right: mx },
+        { left: mx, right: right },
+    ];
+    const du = [
+        { down: down, up: my },
+        { down: my, up: up },
+    ];
+    const result = lr
+        .map(({ left, right }) =>
+            du.map(({ up, down }) => ({ left, right, down, up }))
+        )
+        .flat();
+    return result.filter(
+        ({ left, right, up, down }) =>
+            left <= right && down <= up && (right - left) * (up - down),
     );
 }
 export class Node {
     constructor(
+        public interval: Interval = new Interval(),
         public value: bigint = 0n,
         public children: Node[] = [],
-        public intervals: Interval[] = [],
     ) {}
 }
-export interface Interval {
-    left: number;
-    right: number;
-    down: number;
-    up: number;
+export class Interval {
+    constructor(
+        public left = 0,
+        public right = 0,
+        public down = 0,
+        public up = 0,
+    ) {}
 }
 
 export default rectangleArea;
