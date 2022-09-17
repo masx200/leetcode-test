5f09f9d481dd6d02560448122888ca5261a3fbe9
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-17 23:35:47 +0800

    Update index.ts

--- maximum-rows-covered-by-columns/index.ts
+++ maximum-rows-covered-by-columns/index.ts
@@ -1,12 +1,12 @@
 export default function maximumRows(mat: number[][], numSelect: number): number {
 const n = mat[0].length
-const m = mat.length
-const rows=mat.map((v,i)=>([...v.keys()].filter(j=>v[j])))
+//const m = mat.length
+const rows=mat.map((v,)=>([...v.keys()].filter(j=>v[j])))
 let ans=0
 
 for(const selected  of combinations(Array(n).keys(),numSelect)){
 const set=new Set(selected)
-const count=mat.filter((v,i)=>rows[i].every(j=>set.has(j))).length
+const count=mat.filter((_v,i)=>rows[i].every(j=>set.has(j))).length
 
 ans=Math.max(ans,count)
 }
