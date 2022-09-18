15f5273fcd15e80fd7383b6ea4d5e8c00a83203f
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-18 13:19:43 +0800

    Update index.ts

--- design-underground-system/index.ts
+++ design-underground-system/index.ts
@@ -1,23 +1,23 @@
 export default class UndergroundSystem {
-    id2info = new Map<number, [string, number]>();
-    station2time = new Map<string, number[]>();
+    #id2info = new Map<number, [string, number]>();
+    #station2time = new Map<string, number[]>();
     constructor() {
     }
 
     checkIn(id: number, stationName: string, t: number): void {
         const info: [string, number] = [stationName, t];
-        this.id2info.set(id, info);
+        this.#id2info.set(id, info);
     }
 
     checkOut(id: number, stationName: string, t: number): void {
-        const info = this.id2info.get(id);
+        const info = this.#id2info.get(id);
         if (info) {
             const key = JSON.stringify([info[0], stationName]);
-            const time = this.station2time.get(
+            const time = this.#station2time.get(
                 key,
             ) ?? [];
             time.push(t - info[1]);
-            this.station2time.set(key, time);
+            this.#station2time.set(key, time);
         } else {
             throw Error("accident");
         }
@@ -25,7 +25,7 @@ export default class UndergroundSystem {
 
     getAverageTime(startStation: string, endStation: string): number {
         const key = JSON.stringify([startStation, endStation]);
-        const time = this.station2time.get(
+        const time = this.#station2time.get(
             key,
         );
         if (!time) throw Error("accident");
