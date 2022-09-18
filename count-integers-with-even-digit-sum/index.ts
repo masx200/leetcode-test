2f6647c7e393fce1630928f89c0a7bb2b8c3996e
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-19 07:04:52 +0800

    Create index.ts

---
+++ count-integers-with-even-digit-sum/index.ts
@@ -0,0 +1,4 @@
+export default function countEven(num: number): number {
+return [...num.toString()].map(Number).reduce((a,v)=>a+v,0)%2?Math.floor((num-1)/2):Math.floor((num)/2)
+        }
+        
