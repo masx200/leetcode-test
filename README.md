26b3a37e1fc4eb9f191c88281a30d16916f54d94
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-19 20:13:22 +0800

    https://leetcode.cn/problems/partition-to-k-equal-sum-subsets/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/partition-to-k-equal-sum-subsets/
+
 https://leetcode.cn/problems/count-integers-with-even-digit-sum
 
 https://leetcode.cn/problems/sum-of-prefix-scores-of-strings/
---
+++ partition-to-k-equal-sum-subsets/index.ts
@@ -0,0 +1,44 @@
+export default function canPartitionKSubsets(
+    nums: number[],
+    k: number,
+): boolean {
+    const sum = nums.reduce((a, v) => a + v, 0);
+    if (sum % k != 0) return false;
+    const sub = Math.floor(sum / k);
+    nums.sort((a, b) => a - b);
+    if (nums[nums.length - 1] > sub) return false;
+    for (
+        let i = nums.length - 1;
+        i >= 0 && nums[i] === sub;
+        i--, k--, nums.pop()
+    // deno-lint-ignore no-empty
+    ) {
+    }
+
+    const n = nums.length;
+    const dp = new Array<number>(1 << n);
+    dp[0] = 0;
+
+    const queue = [0];
+
+    for (const i of queue) {
+        if (i === (1 << n) - 1) return true;
+        const cur = dp[i];
+
+        for (let j = 0; j < n; j++) {
+            if (cur + nums[j] > sub) {
+                break;
+            }
+            if (!((i) & (1 << j))) {
+                const next = i | (1 << j);
+
+                if ("undefined" === typeof dp[next]) {
+                    dp[next] = (cur + nums[j]) % sub;
+                    queue.push(next);
+                }
+            }
+        }
+    }
+
+    return false;
+}
