56798a48d2816d22089635dd7c16ab5983059709
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-20 23:37:48 +0800

    https://leetcode.cn/problems/ZbAuEH/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/ZbAuEH/
+
 https://leetcode.cn/problems/network-delay-time/
 
 https://leetcode.cn/problems/maximum-number-of-pairs-in-array/
---
+++ ZbAuEH/index.ts
@@ -0,0 +1,33 @@
+function getMaximumNumber(moles: number[][]): number {
+    moles.sort((a, b) => a[0] - b[0]);
+    let first = 0;
+
+    let res = 0;
+    while (moles.length) {
+        const [t, x, y] = moles[0];
+
+        if (t === 0) {
+            if (y === 1 && x == 1) first++;
+            moles.shift();
+        } else break;
+    }
+
+    moles = [[0, 1, 1], ...moles];
+    const len = moles.length;
+    const dp = Array(len).fill(0).map(() => [0, 0]);
+    for (let i = 1; i < len; i++) {
+        const [t, x, y] = moles[i];
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+        for (let cnt = 0, j = i - 1; cnt < 10 && j >= 0; j--, cnt++) {
+            if (
+                Math.abs(x - moles[j][1]) + Math.abs(y - moles[j][2]) <=
+                    t - moles[j][0]
+            ) {
+                dp[i][1] = Math.max(dp[i][1], dp[j][1] + 1);
+            }
+        }
+        res = Math.max(res, dp[i][0], dp[i][1]);
+    }
+    return res + first;
+}
+export default getMaximumNumber;
