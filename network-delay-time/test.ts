22d35314f9735ab3cd1f4de3a6824abfe629e2bc
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-20 20:26:10 +0800

    Create test.ts

---
+++ network-delay-time/test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.156.0/testing/asserts.ts";
+import networkDelayTime from "./index.ts";
+Deno.test("network-delay-time", () => {
+    const inputs = [
+        [[[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2],
+        [[[1, 2, 1]], 2, 1],
+        [[[1, 2, 1]], 2, 2],
+    ];
+    const outputs = [2, 1, -1];
+    assertEquals(
+        inputs.map((a) => Reflect.apply(networkDelayTime, null, a)),
+        outputs,
+    );
+});
