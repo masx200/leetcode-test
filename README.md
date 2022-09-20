e7ee796aef3c6cd68eef0a7ebb5d92064a808833
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-20 20:14:16 +0800

    https://leetcode.cn/problems/network-delay-time/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/network-delay-time/
+
 https://leetcode.cn/problems/maximum-number-of-pairs-in-array/
 
 https://leetcode.cn/problems/check-array-formation-through-concatenation/
---
+++ network-delay-time/index.ts
@@ -0,0 +1,28 @@
+import { Heap } from "https://esm.sh/@datastructures-js/heap@4.1.2/src/heap.js";
+export default function networkDelayTime(
+    times: number[][],
+    n: number,
+    k: number,
+): number {
+    const pq = new Heap<[number, number]>((a, b) => a[1] - b[1]);
+    const edge = new Map<number, [number, number][]>();
+
+    for (const [u, v, w] of times) {
+        const arr = edge.get(u) ?? [];
+        arr.push([v, w]);
+        edge.set(u, arr);
+    }
+    const dist = new Map<number, number>();
+
+    pq.insert([k, 0]);
+    while (!pq.isEmpty()) {
+        const [c, d] = pq.pop();
+        if (dist.has(c)) continue;
+        dist.set(c, d);
+        if (dist.size === n) break;
+        for (const [v, w] of edge.get(c) ?? []) {
+            pq.insert([v, w + d]);
+        }
+    }
+    return dist.size === n ? Math.max(...dist.values()) : -1;
+}
