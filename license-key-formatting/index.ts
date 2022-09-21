1a30e201dfa59b1217f66f6211c8c7c314abc324
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-21 18:29:22 +0800

    https://leetcode.cn/problems/license-key-formatting/

---
+++ license-key-formatting/index.ts
@@ -0,0 +1,17 @@
+export default function licenseKeyFormatting(s: string, k: number): string {
+    //@ts-ignore
+    const str = s.replaceAll("-", "").toUpperCase();
+
+    return Array(Math.ceil(str.length / k)).fill(0).map((_, i) =>
+        i === 0
+            ? str.length % k
+                ? str.slice(0, str.length % k)
+                : str.slice(i * k, i * k + k)
+            : str.length % k
+            ? str.slice(
+                -k + str.length % k + i * k,
+                -k + str.length % k + i * k + k,
+            )
+            : str.slice(i * k, i * k + k)
+    ).join("-");
+}
