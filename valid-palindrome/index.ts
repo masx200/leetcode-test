783b94edc99ee182a0bd19dfce32d4189e545ff7
masx200 <masx200@qq.com>
2022-09-21 15:40:29 +0800

    优化

--- valid-palindrome/index.ts
+++ valid-palindrome/index.ts
@@ -1,7 +1,7 @@
 function isPalindrome(s: string): boolean {
-const a=(s.match(/[A-Za-z0-9]/g)?.join("")??"").toLowerCase()
+const a=(s.match(/[A-Za-z0-9]+/g)?.join("")??"").toLowerCase()
 
 const mid=Math.floor(a.length/2)
 return a.slice(0,mid)===[...a.slice(a.length-mid)].reverse().join("")
 }
-export default isPalindrome
+export default isPalindromedrome
