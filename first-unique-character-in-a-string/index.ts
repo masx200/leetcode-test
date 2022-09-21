09ce0fe1c03d409268b21c75674409b1a79d9553
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-21 18:00:17 +0800

    https://leetcode.cn/problems/first-unique-character-in-a-string/

---
+++ first-unique-character-in-a-string/index.ts
@@ -0,0 +1,15 @@
+export default function firstUniqChar(s: string): number {
+    const cnts: Record<string, number> = {};
+    for (const w of (s)) {
+        const arr = (cnts[w] ?? 0);
+
+        cnts[w] = arr + 1;
+    }
+
+    for (const [i, w] of Array.prototype.entries.call(s)) {
+        if (cnts[w] === 1) {
+            return i;
+        }
+    }
+    return -1;
+}
