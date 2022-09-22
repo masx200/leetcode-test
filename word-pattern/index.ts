446d7d742e011c9aef1d6e35e95a360de74de97c
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-22 20:32:56 +0800

    Update index.ts

--- word-pattern/index.ts
+++ word-pattern/index.ts
@@ -2,6 +2,7 @@ import { zip } from "../deps.ts";
 
 export default function wordPattern(s: string, str: string): boolean {
     const t = str.split(/\s+/g);
+    if (t.length !== s.length) return false;
     const ent = zip(Object(s), Object(t));
     const map = new Map(ent);
     const maa = new Map(ent.map(([k, v]) => [v, k]));
