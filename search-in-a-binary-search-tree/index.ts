319ff584c8ec205ea004fc1d3f9e2cc07c79e91a
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-23 19:09:13 +0800

    https://leetcode.cn/problems/search-in-a-binary-search-tree/

---
+++ search-in-a-binary-search-tree/index.ts
@@ -0,0 +1,13 @@
+import { TreeNode } from "../binary-tree-inorder-traversal/TreeNode.ts";
+
+export default function searchBST(
+    root: TreeNode | null,
+    val: number,
+): TreeNode | null | undefined {
+    if (!root) return null;
+    if (root.val > val) {
+        return searchBST(root.left, val);
+    } else if (root.val < val) return searchBST(root.right, val);
+    if (root.val === val) return root;
+    return;
+}
