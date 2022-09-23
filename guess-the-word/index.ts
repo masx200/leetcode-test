5450c68072cd3058a2283362b1d166fc370fb718
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-23 12:08:05 +0800

    Update index.ts

--- guess-the-word/index.ts
+++ guess-the-word/index.ts
@@ -1,24 +1,63 @@
 import { Master } from "./Master.ts";
 
-function findSecretWord(words: string[], master: Master) {
-    const seen = new Set<string>();
-    while (words.length) {
-        const j = Math.floor(Math.random() * words.length);
-        const w = words[j];
-        seen.add(w);
-        const d = master.guess(w);
+function findSecretWord(wordlist: string[], master: Master) {
+    const N = wordlist.length;
+    const H: number[][] = new Array(N).fill(0).map(() => Array(N).fill(0));
+    for (let i = 0; i < N; ++i) {
+        for (let j = i; j < N; ++j) {
+            const match = getMatch(wordlist[i], wordlist[j]);
+
+            H[i][j] = H[j][i] = match;
+        }
+    }
+    let possible = [...wordlist.keys()];
+    const seen = new Set<number>();
+    while (possible.length) {
+        const ansguesses: number[] = solve(possible, H);
+        const j = ansguesses[Math.floor(Math.random() * ansguesses.length)];
+
+        seen.add(j);
+        const d = master.guess(wordlist[j]);
         if (d === 6) return;
         if (d >= 0) {
-            words = words.filter((v) =>
-                !seen.has(v) &&
-                d === Array.prototype.filter.call(
-                        v,
-                        (c, k) => w[k] === c,
-                    ).length
+            possible = possible.filter((i) =>
+                !seen.has(i) &&
+                d === H[i][j]
             );
         } else {
             return;
         }
     }
 }
+
+function solve(
+    possible: number[],
+    H: number[][],
+) {
+    const ansgrps: [number, number][] = [];
+    for (const i of possible) {
+        const groups: Array<number> = Array(7).fill(0);
+        for (const j of possible) {
+            if (j !== i) {
+                groups[H[i][j]]++;
+            }
+        }
+        const maxgroup = groups.reduce((a, v) => a > v ? a : v);
+        ansgrps.push([maxgroup, i]);
+    }
+    const minlen = Math.min(...ansgrps.map((a) => a[0]));
+
+    const ansguesses: number[] = ansgrps.filter((a) => a[0] === minlen).map(
+        (a) => a[1],
+    );
+    return ansguesses;
+}
+
+function getMatch(v: string, w: string) {
+    return Array.prototype.filter.call(
+        v,
+        (c, k) => w[k] === c,
+    ).length;
+}
+
 export default findSecretWord;
