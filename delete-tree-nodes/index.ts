42c5104a8a0ed78e3ba5f0f6144b2d86001348d0
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-23 16:28:30 +0800

    测试

--- delete-tree-nodes/index.ts
+++ delete-tree-nodes/index.ts
@@ -10,18 +10,20 @@ export default function deleteTreeNodes(
         }
     }
     function dfs(node: number) {
+        // console.log(node);
         for (const c of children[node]) {
             dfs(c);
         }
         sum[node] = value[node] +
-            children[node].reduce((a, c) => a + value[c], 0);
+            children[node].reduce((a, c) => a + sum[c], 0);
         count[node] = sum[node] !== 0
             ? 1 + children[node].reduce((a, c) => a + count[c], 0)
             : 0;
+        // console.log(node);
     }
     const sum: number[] = Array.from(parent).fill(0);
     const count: number[] = Array(nodes).fill(0);
     dfs(0);
-
+    // console.log({ parent, value, children, sum, count });
     return count[0];
 }
--- delete-tree-nodes/test.ts
+++ delete-tree-nodes/test.ts
@@ -7,3 +7,21 @@ Deno.test("delete-tree-nodes", () => {
         value = [1, -2, 4, 0, -2, -1, -1];
     assertEquals(deleteTreeNodes(nodes, parent, value), 2);
 });
+Deno.test("delete-tree-nodes", () => {
+    const nodes = 8,
+        parent = [-1, 0, 0, 0, 0, 0, 0, 1],
+        value = [1, -2, 4, 0, -2, -1, -1, 1];
+    assertEquals(deleteTreeNodes(nodes, parent, value), 0);
+});
+Deno.test("delete-tree-nodes", () => {
+    const nodes = 7,
+        parent = [-1, 0, 0, 1, 2, 2, 2],
+        value = [1, -2, 4, 0, -2, -1, 0];
+    assertEquals(deleteTreeNodes(nodes, parent, value), 0);
+});
+Deno.test("delete-tree-nodes", () => {
+    const nodes = 7,
+        parent = [-1, 0, 0, 1, 2, 2, 2],
+        value = [0, 0, 0, 0, 0, 0, 0];
+    assertEquals(deleteTreeNodes(nodes, parent, value), 0);
+});
