7fd65cbcdfaafdd066b9992c297c6df0633b50b9
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-24 20:02:19 +0800

    Update index.ts

--- fraction-addition-and-subtraction/index.ts
+++ fraction-addition-and-subtraction/index.ts
@@ -3,10 +3,7 @@ import { deduplication } from "./deduplication.ts";
 import { Fraction } from "./Fraction.ts";
 import { parseFraction } from "./parseFraction.ts";
 import { FractionToString } from "./Fraction.ts";
-
-function fractionAddition(expression: string): string {
-    const fractions = parseFraction(expression);
-
+export function fractionAdd(fractions: Fraction[]): Fraction {
     const denominator = deduplication(
         fractions.map((f) => f.denominator),
     ).reduce((a, b) => a * b);
@@ -14,12 +11,16 @@ function fractionAddition(expression: string): string {
         .map((f) => (f.sign * f.molecular * denominator) / f.denominator)
         .reduce((a, b) => a + b);
     const gcd = greatestCommonDivisor(molecular, denominator);
+    return new Fraction({
+        denominator: denominator / gcd,
+        molecular: molecular / gcd,
+    });
+}
+function fractionAddition(expression: string): string {
+    const fractions = parseFraction(expression);
 
     return FractionToString(
-        new Fraction({
-            denominator: denominator / gcd,
-            molecular: molecular / gcd,
-        }),
+        fractionAdd(fractions),
     );
 }
 export default fractionAddition;
