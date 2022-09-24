17f21d8e036062c21df029062f9c8e8107e17d09
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-24 21:05:05 +0800

    Update index.ts

--- deep-dark-fraction/index.ts
+++ deep-dark-fraction/index.ts
@@ -2,27 +2,16 @@ import { Fraction } from "../fraction-addition-and-subtraction/Fraction.ts";
 import { fractionAdd } from "../fraction-addition-and-subtraction/index.ts";
 
 export default function fraction(cont: number[]): number[] {
-    if (cont.length === 1) {
-        return [cont[0], 1];
-    }
-    if (cont.length >= 2) {
-        const last = fraction(cont.slice(-1));
-        let result = new Fraction({ molecular: last[0], denominator: last[1] });
-        for (let i = cont.length - 2; i >= 0; i--) {
-            result = fractionAdd(
-                [
-                    new Fraction({ molecular: cont[i], denominator: 1 }),
-                    fractionReciprocal(
-                        result,
-                    ),
-                ],
-            );
-        }
-
-        return [result.molecular, result.denominator];
-    }
-
-    return [1, 0];
+    const result = cont.reduceRight((p, c) =>
+        fractionAdd(
+            [
+                new Fraction({ molecular: c, denominator: 1 }),
+                fractionReciprocal(
+                    p,
+                ),
+            ],
+        ), new Fraction({ molecular: 1, denominator: 0 }));
+    return [result.molecular, result.denominator];
 }
 export function fractionReciprocal(fraction: Fraction): Fraction {
     return new Fraction({
