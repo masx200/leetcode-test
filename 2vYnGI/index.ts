ee7e571901eea46c98236592b9a62af12a467b08
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-24 19:49:09 +0800

    https://leetcode.cn/problems/2vYnGI/

---
+++ 2vYnGI/index.ts
@@ -0,0 +1,27 @@
+export default function breakfastNumber(
+    staple: number[],
+    drinks: number[],
+    x: number,
+): number {
+    staple.sort((a, b) => a - b);
+    drinks.sort((a, b) => a - b);
+
+    let i = 0;
+    let j = drinks.length - 1;
+
+    while (drinks[j] >= x) {
+        j--;
+    }
+    let res = 0;
+    while (i < staple.length && j >= 0) {
+        if (staple[i] >= x) break;
+
+        if (staple[i] + drinks[j] <= x) {
+            res += j + 1;
+            i++;
+        } else {
+            j--;
+        }
+    }
+    return res % 1000000007;
+}
