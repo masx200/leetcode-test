c68441bd4e0a17dac95d856a1f38891a73ae2b6c
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-25 14:17:17 +0800

    Update index.ts

--- find-all-good-indices/index.ts
+++ find-all-good-indices/index.ts
@@ -8,36 +8,29 @@ export default function goodIndices(nums: number[], k: number): number[] {
     for (let i = 0; i < pre.length; i++) {
         if (i === 0) {
             for (let j = 1; j < k; j++) {
-                // console.log('pre')
                 pre[i] = nums[j] <= nums[j - 1] ? pre[i] + 1 : 1;
             }
         } else {
             const j = i + k - 1;
-            // console.log(j,j-1)
-            // console.log(i)
+
             pre[i] = nums[j] <= nums[j - 1] ? pre[i - 1] + 1 : 1;
         }
     }
     for (let i = 0; i < post.length; i++) {
         if (i === 0) {
             for (let j = n - 2; j > n - k - 1; j--) {
-                // console.log('post')
                 post[post.length - 1 - i] = nums[j] <= nums[j + 1]
                     ? post[post.length - 1 - i] + 1
                     : 1;
             }
         } else {
             const j = nums.length - i - k;
-            //   console.log(j,j+1)
-            // console.log(nums[j+1],nums[j+2],nums[j+1] <= nums[j + 2])
-            //   console.log()
-            // console.log(post.length - 1 - i)
+
             post[post.length - 1 - i] = nums[j] <= nums[j + 1]
                 ? post[post.length - i] + 1
                 : 1;
         }
     }
-    // console.log(pre, post)
 
     return [...Array(n - k * 2).keys()]
         .filter((i) => pre[i] >= k && post[i] >= k)
