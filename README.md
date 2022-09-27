3ff1b8f0a840591e9604e94530fab9c4f190f5d9
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-27 18:08:30 +0800

    https://leetcode.cn/problems/matrix-diagonal-sum/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/matrix-diagonal-sum/
+
 https://leetcode.cn/problems/thousand-separator/
 
 https://leetcode.cn/problems/count-odd-numbers-in-an-interval-range/
---
+++ matrix-diagonal-sum/index.ts
@@ -0,0 +1,9 @@
+export default function diagonalSum(e: number[][]) {
+    return e.reduce(
+        (e, _n, r, t) =>
+            r === t.length - r - 1
+                ? e + t[r][r]
+                : e + t[r][r] + t[r][t.length - r - 1],
+        0
+    );
+}
