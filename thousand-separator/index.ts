4fc81904d49e9cca14df9d097b905f0bb22282e5
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-27 18:02:31 +0800

    https://leetcode.cn/problems/thousand-separator/

---
+++ thousand-separator/index.ts
@@ -0,0 +1,6 @@
+export default function thousandSeparator(n: number): string {
+    const str = [...n.toString()].reverse();
+    return Array(Math.ceil(str.length / 3)).fill(0).map((_, i) =>
+        str.slice(i * 3, i * 3 + 3).reverse().join("")
+    ).reverse().join(".");
+}
