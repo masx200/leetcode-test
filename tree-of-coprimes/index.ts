3da1cb9f6e5b0af49771b9bda8f70489237d2ccb
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-27 20:06:58 +0800

    Update index.ts

--- tree-of-coprimes/index.ts
+++ tree-of-coprimes/index.ts
@@ -1,3 +1,5 @@
+import { greatestCommonDivisor } from "../max-points-on-a-line/greatest_common_divisor.ts";
+
 export default getCoprimes;
 function getCoprimes(nums: number[], edges: number[][]): number[] {
     const edge = Array(nums.length)
@@ -13,39 +15,44 @@ function getCoprimes(nums: number[], edges: number[][]): number[] {
             prime[i] = Array(51).fill(false);
         }
 
-        for (const i of Array(51).keys())
+        for (const i of Array(51).keys()) {
             for (let j = i; j < 51; j++) {
                 prime[i][j] = prime[j][i] = greatestCommonDivisor(i, j) === 1;
             }
+        }
     }
 
     const visited = new Set<number>();
-    function dfs(node: number, ancestors: number[]) {
-        if (!visited.has(node)) {
-            visited.add(node);
+    const currents: [number, number][][] = Array(51)
+        .fill(0)
+        .map(() => []);
+    function dfs(node: number, depth: number) {
+        if (visited.has(node)) {
+            return;
+        }
+        visited.add(node);
+        const value = nums[node];
 
-            if (ancestors.length) {
-                for (let i = ancestors.length - 1; i >= 0; i--) {
-                    if (prime[nums[node]][nums[ancestors[i]]]) {
-                        results[node] = ancestors[i];
-                        break;
-                    }
+        let ans = [-1, -1];
+        for (const i of Array(51).keys()) {
+            if (currents[i].length && prime[value][i]) {
+                const get = currents[i].at(-1) as [number, number];
+
+                if (get[1] > ans[1]) {
+                    ans = get;
                 }
             }
-            for (const child of edge[node]) {
-                ancestors.push(node);
-                dfs(child, ancestors);
-                ancestors.pop();
-            }
+        }
+        results[node] = ans[0];
+        for (const child of edge[node]) {
+            currents[value].push([node, depth]);
+            dfs(child, depth + 1);
+            currents[value].pop();
         }
     }
     const results: number[] = Array(nums.length).fill(-1);
-    dfs(0, []);
+    dfs(0, 0);
     return results;
 }
 
 const prime: boolean[][] = [];
-
-function greatestCommonDivisor(a: number, b: number): number {
-    return b != 0 ? greatestCommonDivisor(b, a % b) : a;
-}
