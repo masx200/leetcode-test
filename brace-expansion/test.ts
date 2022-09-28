35ed31e374ff3f5ebb6697282276095cb7fea8cf
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-28 14:41:16 +0800

    Update test.ts

--- brace-expansion/test.ts
+++ brace-expansion/test.ts
@@ -1,11 +1,17 @@
 import { assertEquals } from "https://deno.land/std@0.157.0/testing/asserts.ts";
 import expand from "./index.ts";
 Deno.test("brace-expansion", () => {
-    const inputs = ["{a,b}c{d,e}f", "abcd", "abc{x,y,m,n,p,q}"];
+    const inputs = [
+        "{a,b}c{d,e}f",
+        "abcd",
+        "abc{x,y,m,n,p,q}",
+        "{x,y,m,n,p,q}",
+    ];
     const outputs = [
         ["acdf", "acef", "bcdf", "bcef"],
         ["abcd"],
         ["abcm", "abcn", "abcp", "abcq", "abcx", "abcy"],
+        ["m", "n", "p", "q", "x", "y"],
     ];
     assertEquals(inputs.map(expand), outputs);
 });
