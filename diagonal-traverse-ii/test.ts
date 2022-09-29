838712105f22d9ca0cfb1d961e62d968164ec81d
GitHub <noreply@github.com>
2022-09-29 19:49:56 +0000

    Update dependencies

--- diagonal-traverse-ii/test.ts
+++ diagonal-traverse-ii/test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.157.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
 
 import findDiagonalOrder from "./index.ts";
 
--- tree-of-coprimes/test.ts
+++ tree-of-coprimes/test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.157.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
 import getCoprimes from "./index.ts";
 Deno.test("tree-of-coprimes", () => {
     const inputs: [nums: number[], edges: number[][]][] = [
