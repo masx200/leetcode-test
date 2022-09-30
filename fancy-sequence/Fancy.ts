36bce6f35aafbfc1fea6d8b0bf5b6b9b5b73ca84
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-30 10:52:57 +0800

    测试

--- fancy-sequence/Fancy.ts
+++ fancy-sequence/Fancy.ts
@@ -45,8 +45,9 @@ const multiplicativeInverse = function (x: number, mod: bigint): bigint {
         p = xb;
     m = m - 2n;
 
-    for (let i = 0n; 1n << i < m; i++, p = (p * p) % mb)
+    for (let i = 0n; 1n << i < m; i++, p = (p * p) % mb) {
         if (((m >> i) & 1n) == 1n) y = (y * p) % mb;
+    }
     INVERSES[x] = y;
     return y;
 };
