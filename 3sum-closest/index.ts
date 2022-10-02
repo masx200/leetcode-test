5c30283c88ba9b25d8451532ea1c1f9a0837b1b7
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-02 22:52:17 +0800

    Update index.ts

--- 3sum-closest/index.ts
+++ 3sum-closest/index.ts
@@ -2,14 +2,14 @@ export default function threeSumClosest(
     nums: number[],
     target: number
 ): number {
-    let N = nums.length;
+    const N = nums.length;
     let res = Number.MAX_SAFE_INTEGER;
     nums.sort((a, b) => a - b);
     for (let i = 0; i < N; i++) {
         let left = i + 1;
         let right = N - 1;
         while (left < right) {
-            let sum = nums[i] + nums[left] + nums[right];
+            const sum = nums[i] + nums[left] + nums[right];
             if (Math.abs(sum - target) < Math.abs(res - target)) {
                 res = sum;
             }
