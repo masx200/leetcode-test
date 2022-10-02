9764281b0570a64fdcde4e08eb3f0f8c807c5bfd
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-02 11:19:03 +0800

    Update Fancy.ts

--- fancy-sequence/Fancy.ts
+++ fancy-sequence/Fancy.ts
@@ -36,20 +36,22 @@ export default function Fancy(): Fancy {
     return { getIndex, multAll, addAll, append };
 }
 
-const multiplicativeInverse = function (x: number, mod: bigint): bigint {
-    if (typeof INVERSES[x] !== "undefined") return INVERSES[x];
+function multiplicativeInverse(x: number, mod: bigint): bigint {
+    if (typeof INVERSES[x] !== "undefined") {
+        return INVERSES[x];
+    }
     const xb = BigInt(x);
     const mb = BigInt(mod);
-    let y = 1n,
-        m = mb,
-        p = xb;
+    let y = 1n, m = mb, p = xb;
     m = m - 2n;
 
     for (let i = 0n; 1n << i < m; i++, p = (p * p) % mb) {
-        if (((m >> i) & 1n) == 1n) y = (y * p) % mb;
+        if (((m >> i) & 1n) == 1n) {
+            y = (y * p) % mb;
+        }
     }
     INVERSES[x] = y;
     return y;
-};
+}
 
 const INVERSES: bigint[] = [];
