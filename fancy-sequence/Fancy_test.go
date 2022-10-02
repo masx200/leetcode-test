0d8b691acab6c8695dfc0fd64f8e12e8798d4eed
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-02 11:48:53 +0800

    TestFancy

---
+++ fancy-sequence/Fancy_test.go
@@ -0,0 +1,23 @@
+package index
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestFancy(t *testing.T) {
+	var fancy = Constructor()
+	fancy.Append(2)                        // 奇妙序列：[2]
+	fancy.AddAll(3)                        // 奇妙序列：[2+3] -> [5]
+	fancy.Append(7)                        // 奇妙序列：[5, 7]
+	fancy.MultAll(2)                       // 奇妙序列：[5*2, 7*2] -> [10, 14]
+	assert.Equal(t, 10, fancy.GetIndex(0)) // 返回 10
+	fancy.AddAll(3)                        // 奇妙序列：[10+3, 14+3] -> [13, 17]
+	fancy.Append(10)                       // 奇妙序列：[13, 17, 10]
+	fancy.MultAll(2)                       // 奇妙序列：[13*2, 17*2, 10*2] -> [26, 34, 20]
+	assert.Equal(t, 26, fancy.GetIndex(0)) // 返回 26
+	assert.Equal(t, 34, fancy.GetIndex(1)) // 返回 34
+	assert.Equal(t, 20, fancy.GetIndex(2)) // 返回 20
+
+}
--- remove-letter-to-equalize-frequency/index.ts
+++ remove-letter-to-equalize-frequency/index.ts
@@ -3,10 +3,8 @@ import { countBy } from "../deps.ts";
 function equalFrequency(word: string): boolean {
     for (let i = 0; i < word.length; i++) {
         const obj = countBy(word.slice(0, i) + word.slice(i + 1)),
-            set = new Set();
-        for (const k in obj) {
-            set.add(obj[k]);
-        }
+            set = new Set<number>(Object.values(obj));
+
         if (set.size === 1) return true;
     }
     return false;
