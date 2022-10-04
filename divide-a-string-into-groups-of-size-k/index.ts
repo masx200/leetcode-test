4f4fd08f064e321dc032fce68a2c45e58e80f53e
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-04 16:10:16 +0800

    https://leetcode.cn/problems/divide-a-string-into-groups-of-size-k/

---
+++ divide-a-string-into-groups-of-size-k/index.ts
@@ -0,0 +1,11 @@
+export default function divideString(
+    s: string,
+    k: number,
+    fill: string,
+): string[] {
+    const str = s.length % k ? s.padEnd(Math.ceil(s.length / k) * k, fill) : s;
+
+    return Array(str.length / k).fill(0).map((_, i) =>
+        str.slice(i * k, i * k + k)
+    );
+}
