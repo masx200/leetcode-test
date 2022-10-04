4b75b99e9203f806f348c5252c4d079e3e95ba84
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-04 17:10:57 +0800

    https://leetcode.cn/problems/change-minimum-characters-to-satisfy-one-of-three-conditions/

---
+++ change-minimum-characters-to-satisfy-one-of-three-conditions/index.ts
@@ -0,0 +1,23 @@
+export default function minCharacters(a: string, b: string): number {
+    const fA = new Array(26).fill(0);
+    const fB = new Array(26).fill(0);
+    const fC = new Array(26).fill(0);
+    for (let i = 0; i < a.length; ++i) fA[a[i].charCodeAt(0) - 97]++;
+    for (let i = 0; i < b.length; ++i) fB[b[i].charCodeAt(0) - 97]++;
+    const c = a + b;
+    for (let i = 0; i < c.length; ++i) fC[c[i].charCodeAt(0) - 97]++;
+    let ans = fC.reduce((a, b) => a + b, 0) - Math.max(...fC);
+    for (let i = 1; i < 26; ++i) {
+        ans = Math.min(
+            ans,
+            fA.slice(0, i).reduce((a, b) => a + b, 0) +
+                fB.slice(i).reduce((a, b) => a + b, 0)
+        );
+        ans = Math.min(
+            ans,
+            fB.slice(0, i).reduce((a, b) => a + b, 0) +
+                fA.slice(i).reduce((a, b) => a + b, 0)
+        );
+    }
+    return ans;
+}
