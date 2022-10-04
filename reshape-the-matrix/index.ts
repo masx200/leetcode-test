0cf08f5a6f32f2851f419e116f391c206421c8bc
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-04 19:31:18 +0800

    https://leetcode.cn/problems/reshape-the-matrix/

---
+++ reshape-the-matrix/index.ts
@@ -0,0 +1,10 @@
+export default function matrixReshape(
+    mat: number[][],
+    r: number,
+    c: number,
+): number[][] {
+    if (r * c !== mat.length * mat[0].length) return mat;
+    const arr = mat.flat();
+
+    return [...Array(r).keys()].map((i) => arr.slice(i * c, i * c + c));
+}
