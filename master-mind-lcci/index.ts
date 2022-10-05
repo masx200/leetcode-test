fbd712b4d7cc6b1cac53b0bf55ac8022802fc475
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-05 12:37:36 +0800

    https://leetcode.cn/problems/master-mind-lcci/

---
+++ master-mind-lcci/index.ts
@@ -0,0 +1,21 @@
+export default function masterMind(solution: string, guess: string): number[] {
+    let m = 0;
+    let n = 0;
+    const obj: Record<any, number> = {
+        R: 0,
+        G: 0,
+        B: 0,
+        Y: 0,
+    };
+    for (let i = 0; i < solution.length; i++) {
+        const s = solution[i];
+        let g = guess[i];
+        if (s === g) {
+            m++;
+        } else {
+            if (obj[s]++ < 0) n++;
+            if (obj[g]-- > 0) n++;
+        }
+    }
+    return [m, n];
+}
