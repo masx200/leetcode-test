d3a82177621a5c0c6a44c2c84465f0abca6cba4b
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-06 21:40:41 +0800

    Update index.ts

--- smallest-difference-lcci/index.ts
+++ smallest-difference-lcci/index.ts
@@ -1,13 +1,17 @@
 export default function smallestDifference(a: number[], b: number[]): number {
-    return Math.min(
-        2147483647,
-        ...[
-            ...a.sort((x, y) => x - y).map((v) => [v, 1]),
-            ...b.sort((x, y) => x - y).map((v) => [v, 0]),
-        ].sort((a, b) => a[0] - b[0]).map((v, i, a) =>
-            i === 0 || v[1] === a[i - 1][1]
-                ? 2147483647
-                : Math.abs(v[0] - a[i - 1][0])
-        ).filter((a) => a < 2147483647),
-    );
+    return [
+        ...a.sort((x, y) => x - y).map((v) => [v, 1]),
+        ...b.sort((x, y) => x - y).map((v) => [v, 0]),
+    ]
+        .sort((a, b) => a[0] - b[0])
+        .reduce(
+            (p, v, i, a) =>
+                Math.min(
+                    p,
+                    i === 0 || v[1] === a[i - 1][1]
+                        ? 2147483647
+                        : Math.abs(v[0] - a[i - 1][0]),
+                ),
+            2147483647,
+        );
 }
