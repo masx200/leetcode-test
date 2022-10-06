1a8035f1cf1ba2cf82c3572ede75758ac6aec663
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-06 15:04:19 +0800

    https://leetcode.cn/problems/all-possible-full-binary-trees/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/all-possible-full-binary-trees/
+
 https://leetcode.cn/problems/numbers-with-repeated-digits
 
 https://leetcode.cn/problems/count-numbers-with-unique-digits/
---
+++ all-possible-full-binary-trees/index.ts
@@ -0,0 +1,31 @@
+import { TreeNode } from "../binary-tree-inorder-traversal/TreeNode.ts";
+
+export default allPossibleFBT;
+
+const memo = new Map<number, Array<TreeNode | null>>();
+function allPossibleFBT(N: number): Array<TreeNode | null> {
+    const cached = memo.get(N);
+
+    if (!cached) {
+        const ans = new Array<TreeNode | null>();
+        if (N == 1) {
+            ans.push(new TreeNode(0));
+        } else if (N % 2 == 1) {
+            for (let x = 0; x < N; ++x) {
+                const y = N - 1 - x;
+                for (const left of allPossibleFBT(x)) {
+                    for (const right of allPossibleFBT(y)) {
+                        const bns = new TreeNode(0);
+                        bns.left = left;
+                        bns.right = right;
+                        ans.push(bns);
+                    }
+                }
+            }
+        }
+        memo.set(N, ans);
+        return ans;
+    }
+
+    return cached;
+}
