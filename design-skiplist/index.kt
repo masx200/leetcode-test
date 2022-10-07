8a92915508260c814013eb87b870e66772e2ac2e
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-07 15:16:10 +0800

    JAVA_MODULE

---
+++ design-skiplist/index.kt
@@ -0,0 +1,17 @@
+package masx200.leetcode_test.design_skiplist
+
+class Skiplist() {
+
+    fun search(target: Int): Boolean {
+
+    }
+
+    fun add(num: Int) {
+
+    }
+
+    fun erase(num: Int): Boolean {
+
+    }
+
+}
