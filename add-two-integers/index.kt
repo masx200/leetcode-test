8e99e6749211feba69b09fc30976b8120b12e337
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-07 12:59:21 +0800

    Update index.kt

--- add-two-integers/index.kt
+++ add-two-integers/index.kt
@@ -1,12 +1,14 @@
+package masx200.leetcode_test.add_two_integers
+
 class Solution {
     fun sum(num1: Int, num2: Int): Int {
         return num1 + num2
     }
 }
-//
-//fun main(
-//    /* args: Array<String> */
-//) {
-//    println(Solution().sum(111, 555))
-//    println(Solution().sum(1191, 555))
-//}
+
+fun main(
+    /* args: Array<String> */
+) {
+    println(Solution().sum(111, 555))
+    println(Solution().sum(1191, 555))
+}
