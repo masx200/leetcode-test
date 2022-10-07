32dda74d8983ad9c91141ee0e8c73891e913fc3a
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-07 15:52:05 +0800

    masx200.leetcode_test.design_skiplist

--- design-skiplist/index.kt
+++ design-skiplist/index.kt
@@ -1,17 +1,26 @@
-//package masx200.leetcode_test.design_skiplist
-//
-//class Skiplist() {
-//
-//    fun search(target: Int): Boolean {
-//
-//    }
-//
-//    fun add(num: Int) {
-//
-//    }
-//
-//    fun erase(num: Int): Boolean {
-//
-//    }
-//
-//}
+package masx200.leetcode_test.design_skiplist
+
+class Skiplist {
+    private val storage = hashMapOf<Int, Int>()
+    fun search(target: Int): Boolean {
+        return storage.getOrDefault(target, 0) > 0
+    }
+
+    fun add(num: Int) {
+        storage.set(num, storage.getOrDefault(num, 0) + 1)
+    }
+
+    fun erase(num: Int): Boolean {
+        if (storage.getOrDefault(num, 0) == 1) {
+            storage.remove(num)
+            return true
+        }
+        if (search(num)) {
+            storage.set(num, storage.getOrDefault(num, 0) - 1)
+            return true
+        } else {
+            return false
+        }
+    }
+
+}
