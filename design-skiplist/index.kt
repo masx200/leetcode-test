5aac39d6ae3d49c406096ec8ea52343eeed0d0ab
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-07 15:27:31 +0800

    测试

--- design-skiplist/index.kt
+++ design-skiplist/index.kt
@@ -1,17 +1,17 @@
-package masx200.leetcode_test.design_skiplist
-
-class Skiplist() {
-
-    fun search(target: Int): Boolean {
-
-    }
-
-    fun add(num: Int) {
-
-    }
-
-    fun erase(num: Int): Boolean {
-
-    }
-
-}
+//package masx200.leetcode_test.design_skiplist
+//
+//class Skiplist() {
+//
+//    fun search(target: Int): Boolean {
+//
+//    }
+//
+//    fun add(num: Int) {
+//
+//    }
+//
+//    fun erase(num: Int): Boolean {
+//
+//    }
+//
+//}
