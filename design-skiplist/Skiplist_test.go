37a6ec754c52f897d10dd9b0ed6ff35135239f90
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-07 16:40:18 +0800

    测试

--- design-skiplist/Skiplist_test.go
+++ design-skiplist/Skiplist_test.go
@@ -14,5 +14,8 @@ func TestSkiplist(t *testing.T) {
 	assert.Equal(t, false, skiplist.Erase(0))  // 返回 false，0 不在跳表中
 	assert.Equal(t, true, skiplist.Erase(1))   // 返回 true
 	assert.Equal(t, false, skiplist.Search(1)) // 返回 false，1 已被擦除
-
+	skiplist.Add(4)
+	skiplist.Add(4)
+	assert.Equal(t,true, skiplist.Erase(4))
+	assert.Equal(t,true, skiplist.Search(4))
 }
--- masx200/leetcode_test/design_skiplist/SkiplistTest.kt
+++ masx200/leetcode_test/design_skiplist/SkiplistTest.kt
@@ -7,18 +7,21 @@ internal class SkiplistTest {
 
     @Test
     fun search() {
-        val skiplist = Skiplist();
-        skiplist.add(1);
-        skiplist.add(2);
-        skiplist.add(3);
-        assertEquals(false, skiplist.search(0));   // 返回 false
-        skiplist.add(4);
-        assertEquals(true, skiplist.search(1));   // 返回 true
-        assertEquals(false, skiplist.erase(0))    // 返回 false，0 不在跳表中
-        assertEquals(true, skiplist.erase(1));    // 返回 true
-        assertEquals(false, skiplist.search(1));   // 返回 false，1 已被擦除
-//
-//        println("masx200.leetcode_test.design_skiplist")
+        val skiplist = Skiplist()
+        skiplist.add(1)
+        skiplist.add(2)
+        skiplist.add(3)
+        assertEquals(false, skiplist.search(0)) // 返回 false
+        skiplist.add(4)
+        assertEquals(true, skiplist.search(1)) // 返回 true
+        assertEquals(false, skiplist.erase(0)) // 返回 false，0 不在跳表中
+        assertEquals(true, skiplist.erase(1)) // 返回 true
+        assertEquals(false, skiplist.search(1)) // 返回 false，1 已被擦除
+        //
+        //        println("masx200.leetcode_test.design_skiplist")
+        skiplist.add(4)
+        skiplist.add(4)
+        assertEquals(true, skiplist.erase(4))
+        assertEquals(true, skiplist.search(4))
     }
-
 }
