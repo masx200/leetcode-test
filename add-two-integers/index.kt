31b8fb590d8dfbd2cd8a0088d02b738e6460ef38
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-07 12:49:21 +0800

    kt

--- add-two-integers/index.kt
+++ add-two-integers/index.kt
@@ -3,8 +3,10 @@ class Solution {
         return num1 + num2
     }
 }
-
-// fun main(
-// /* args: Array<String> */ ) {
-//     println(Solution().sum(111, 555))
-// }
+//
+//fun main(
+//    /* args: Array<String> */
+//) {
+//    println(Solution().sum(111, 555))
+//    println(Solution().sum(1191, 555))
+//}
