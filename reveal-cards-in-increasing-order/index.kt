a3ecbe33fb3cbae086f8145c912157e956b16654
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-08 14:43:22 +0800

    Create index.kt

---
+++ reveal-cards-in-increasing-order/index.kt
@@ -0,0 +1,20 @@
+package masx200.leetcode_test.reveal_cards_in_increasing_order
+
+import java.util.LinkedList
+
+class Solution {
+    fun deckRevealedIncreasing(deck: IntArray): IntArray {
+        if (deck.isEmpty()) return deck
+        deck.sortDescending()
+        val queue = LinkedList<Int>()
+        deck.forEachIndexed { i, d ->
+            queue.addFirst(d)
+
+            if (i != deck.size - 1) {
+                queue.addFirst(queue.last())
+                queue.removeLast()
+            }
+        }
+        return queue.toIntArray()
+    }
+}
