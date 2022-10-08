db24c11bf271ecb5bc9cf5f10cd49d358e404b93
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-08 15:24:43 +0800

    https://leetcode.cn/problems/image-overlap/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/image-overlap/
+
 https://leetcode.cn/problems/reveal-cards-in-increasing-order/
 
 https://leetcode.cn/problems/insert-into-a-binary-search-tree/
---
+++ image-overlap/index.ts
@@ -0,0 +1,31 @@
+export default /**
+ * @param {number[][]} img1
+ * @param {number[][]} img2
+ * @return {number}
+ */
+function largestOverlap(img1: number[][], img2: number[][]) {
+    const len = img1.length;
+    const count: number[][] = Array(2 * len + 1)
+        .fill(0)
+        .map(() => Array(2 * len + 1).fill(0));
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (img1[i][j] === 1) {
+                for (let i2 = 0; i2 < len; i2++) {
+                    for (let j2 = 0; j2 < len; j2++) {
+                        if (img2[i2][j2] === 1) {
+                            count[i - i2 + len][j - j2 + len] += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let ans = 0;
+    for (const row of count) {
+        ans = Math.max(...row, ans);
+    }
+    // console.log(count);
+    return ans;
+}
