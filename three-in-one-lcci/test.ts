701952455f89f9e6be6d08b0672bbe23393a2881
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-09 14:40:50 +0800

    Create test.ts

---
+++ three-in-one-lcci/test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "https://deno.land/std@0.159.0/testing/asserts.ts";
+import TripleInOne from "./index.ts";
+
+Deno.test("three-in-one-lcci", () => {
+    const tio = new TripleInOne(1);
+    tio.push(0, 1);
+    tio.push(0, 2);
+    assertEquals(1, tio.pop(0));
+    assertEquals(-1, tio.pop(0));
+    assertEquals(-1, tio.pop(0));
+    assertEquals(true, tio.isEmpty(0));
+});
