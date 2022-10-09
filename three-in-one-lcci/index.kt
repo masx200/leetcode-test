2bbcb7edfee474e34ae0251475ddd17ba3772f63
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-09 14:14:04 +0800

    Update index.kt

--- three-in-one-lcci/index.kt
+++ three-in-one-lcci/index.kt
@@ -1,11 +1,34 @@
 package masx200.leetcode_test.three_in_one_lcci
 class TripleInOne(stackSize: Int) {
+    private val stack: IntArray
+    private val size = stackSize
 
-    fun push(stackNum: Int, value: Int) {}
+    init {
+        stack = IntArray((stackSize + 1) * 3) { 0 }
+    }
+    fun push(stackNum: Int, value: Int) {
 
-    fun pop(stackNum: Int): Int {}
+        if (stack[stackNum] == size) return
+        stack[3 + stackNum * size + stack[stackNum]] = value
 
-    fun peek(stackNum: Int): Int {}
+        stack[stackNum]++
+    }
 
-    fun isEmpty(stackNum: Int): Boolean {}
+    fun pop(stackNum: Int): Int {
+        if (this.isEmpty(stackNum)) return -1
+        val result = this.peek(stackNum)
+
+        stack[stackNum]--
+
+        return result
+    }
+
+    fun peek(stackNum: Int): Int {
+        if (this.isEmpty(stackNum)) return -1
+        return stack[3 + stackNum * size + stack[stackNum] - 1]
+    }
+
+    fun isEmpty(stackNum: Int): Boolean {
+        return stack[stackNum] == 0
+    }
 }
