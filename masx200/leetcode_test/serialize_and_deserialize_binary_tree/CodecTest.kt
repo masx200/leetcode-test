8ee5585608a47496715058e25854c66e25ed0d14
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-09 22:17:51 +0800

    Update CodecTest.kt

--- masx200/leetcode_test/serialize_and_deserialize_binary_tree/CodecTest.kt
+++ masx200/leetcode_test/serialize_and_deserialize_binary_tree/CodecTest.kt
@@ -1,5 +1,6 @@
 package masx200.leetcode_test.serialize_and_deserialize_binary_tree
 
+import masx200.leetcode_test.insert_into_a_binary_search_tree.TreeNode
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -10,6 +11,20 @@ internal class CodecTest {
             "[100,[-200,null,null],[300,[100,[20,null,null],[300,null,null]],null]]",
             Codec().serialize(Codec().deserialize("[100,[-200,null,null],[300,[100,[20,null,null],[300,null,null]],null]]"))
         )
+        assertEquals(
+            Codec().serialize(
+                (TreeNode(100, TreeNode(-200), TreeNode(300, TreeNode(100, TreeNode(20), TreeNode(300)))))),
+                Codec().serialize(
+                    Codec().deserialize(
+                        Codec().serialize(
+                            TreeNode(
+                                100,
+                                TreeNode(-200),
+                                TreeNode(300, TreeNode(100, TreeNode(20), TreeNode(300)))
+                            )
+                        )
+                    )
+                ))
     }
 
     @Test
@@ -21,6 +36,10 @@ internal class CodecTest {
         assertEquals(
             "null",
             Codec().serialize(Codec().deserialize("null"))
+        )
+        assertEquals(
+            null,
+            Codec().deserialize(Codec().serialize(null))
         )
     }
 
