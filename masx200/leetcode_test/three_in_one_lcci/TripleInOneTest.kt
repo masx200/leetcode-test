57b762dfeee0c3dac79972162ea5fd86bc91b220
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-09 14:37:56 +0800

    Create TripleInOneTest.kt

---
+++ masx200/leetcode_test/three_in_one_lcci/TripleInOneTest.kt
@@ -0,0 +1,17 @@
+package masx200.leetcode_test.three_in_one_lcci
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class TripleInOneTest {
+    @Test
+    fun push() {
+        val tio = TripleInOne(1)
+        tio.push(0, 1)
+        tio.push(0, 2)
+        assertEquals(1, tio.pop(0))
+        assertEquals(-1, tio.pop(0))
+        assertEquals(-1, tio.pop(0))
+        assertEquals(true, tio.isEmpty(0))
+    }
+}
