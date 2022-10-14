ba75f98fb1b41faf228685528a6bde5f2b548a9e
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-14 23:29:51 +0800

    Update index.ts

--- minimum-absolute-difference-queries/index.ts
+++ minimum-absolute-difference-queries/index.ts
@@ -1,24 +1,27 @@
 export default function minDifference(
     nums: number[],
-    queries: number[][],
+    queries: number[][]
 ): number[] {
     const n = nums.length;
 
-    const presum = Array(100)
+const sorted=[...new Set(nums)].sort((a,b)=>a-b)
+ 
+   const presum = Array(100)
         .fill(0)
         .map(() => Array<number>(n).fill(0));
 
-    for (let k = 1; k <= 100; k++) {
+    for (const k of sorted) {
         for (let i = 0; i < n; i++) {
-            presum[k - 1][i] = (i == 0 ? 0 : presum[k - 1][i - 1]) +
-                Number(nums[i] == k);
+            presum[k - 1][i] =
+                (i == 0 ? 0 : presum[k - 1][i - 1]) + Number(nums[i] == k);
         }
     }
 
     return queries.map(([left, right]) => {
         const arr = Array<number>();
-        for (let k = 1; k <= 100; k++) {
-            const count = presum[k - 1][right] -
+        for (const k of sorted) {
+            const count =
+                presum[k - 1][right] -
                 (left == 0 ? 0 : presum[k - 1][left - 1]);
 
             if (count) {
@@ -28,14 +31,13 @@ export default function minDifference(
             if (
                 arr.length >= 2 &&
                 arr[arr.length - 1] - arr[arr.length - 2] == 1
-            ) {
-                return 1;
-            }
+            )
+              return 1;
         }
         if (arr.length <= 1) return -1;
         else {
             return Math.min(
-                ...arr.map((v, i, a) => (i === 0 ? Infinity : v - a[i - 1])),
+                ...arr.map((v, i, a) => (i === 0 ? Infinity : v - a[i - 1]))
             );
         }
     });
