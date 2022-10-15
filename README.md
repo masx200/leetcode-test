3e7359c6a0cc842869247973c538f7031db2ba66
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-15 18:57:32 +0800

    https://leetcode.cn/problems/possible-bipartition/

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ leetcode 测试
 
 ##### 包含的内容如下
 
+https://leetcode.cn/problems/possible-bipartition/
+
 https://leetcode.cn/problems/fruit-into-baskets/
 
 https://leetcode.cn/problems/min-cost-to-connect-all-points/
---
+++ possible-bipartition/index.ts
@@ -0,0 +1,25 @@
+import { UnionFind } from "../largest-component-size-by-common-factor/UnionFind.ts";
+export default function possibleBipartition(
+    n: number,
+    dislikes: number[][]
+): boolean {
+    const uf = new UnionFind();
+
+    const e = Array<Array<number>>(n + 1);
+
+    for (const [a, b] of dislikes) {
+        e[a] ??= [];
+        e[b] ??= [];
+        e[a].push(b);
+        e[b].push(a);
+    }
+
+    for (const i of Object.keys(e)) {
+        for (const d of e[Number(i)]) {
+            uf.union(d, e[Number(i)][0]);
+            if (uf.connected(d, Number(i))) return false;
+        }
+    }
+
+    return true;
+}
