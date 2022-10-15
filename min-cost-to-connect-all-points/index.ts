d4afae1c7cf3b3b7f81bfb84a2440264f5f77cb1
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-15 16:36:08 +0800

    Update index.ts

--- min-cost-to-connect-all-points/index.ts
+++ min-cost-to-connect-all-points/index.ts
@@ -1,31 +1,30 @@
 function minCostConnectPoints(points: number[][]): number {
     const p0 = points[0];
-    const ps: Map<number, number[]> = new Map(points.map((p, i) => [i, p]));
-    ps.delete(0);
+
     const ds: Map<number, number> = new Map(
-        points.map((p, i) => [i, 曼哈顿距离(p, p0)]),
+        points.map((p, i) => [i, 曼哈顿距离(p, p0)])
     );
     ds.delete(0);
     let ans = 0;
 
-    while (ps.size) {
+    while (ds.size) {
         let mi = 0;
         let md = Infinity;
-        let p1: number[] = [];
-        for (const [i, p] of ps) {
-            const d = ds.get(i) ?? 0;
+        let p1 = [];
+        for (const [i, d] of ds) {
+            const p = points[i];
             if (d < md) {
                 mi = i;
                 p1 = p;
                 md = d;
             }
         }
-        ps.delete(mi);
+
         ds.delete(mi);
         ans += md;
 
         for (const [i, d] of ds) {
-            const p: number[] = ps.get(i) ?? [];
+            const p = points[i];
             ds.set(i, Math.min(d, 曼哈顿距离(p, p1)));
         }
     }
@@ -35,4 +34,5 @@ function minCostConnectPoints(points: number[][]): number {
 function 曼哈顿距离(a: number[], b: number[]) {
     return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
 }
+
 export default minCostConnectPoints;
