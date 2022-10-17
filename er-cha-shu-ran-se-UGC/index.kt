fc8aa0b87fcd6d8c90f863b2dd957e7d058d30de
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-17 19:25:19 +0800

    Update index.kt

--- er-cha-shu-ran-se-UGC/index.kt
+++ er-cha-shu-ran-se-UGC/index.kt
@@ -1,7 +1,32 @@
 package com.github.masx200.leetcode_test.er_cha_shu_ran_se_ugc
+
 import com.github.masx200.leetcode_test.insert_into_a_binary_search_tree.TreeNode
+
 class Solution {
     fun maxValue(root: TreeNode?, k: Int): Int {
-
+        return dfs(root, k).max() ?: Int.MIN_VALUE
     }
+}
+
+fun dfs(root: TreeNode?, k: Int): IntArray {
+    val ans = IntArray(k + 1)
+
+    if (root == null) {
+        return ans
+    }
+    val left = dfs(root.left, k)
+    val right = dfs(root.right, k)
+
+    ans.fill(left[k] + right[k])
+
+    for (i in 1..k) {
+        var temp = Int.MIN_VALUE
+        for (
+        j in 0 until i
+        ) {
+            temp = Math.max(temp, left[j] + right[i - 1 - j])
+        }
+        ans[i] = Math.max(ans[i], temp + root.`val`)
+    }
+    return ans
 }
