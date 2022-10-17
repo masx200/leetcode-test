8cd9bf4a334c591cb2fabcac35f8646260a7eb0a
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-17 18:17:02 +0800

    package com.github.masx200.leetcode_test.er_cha_shu_ran_se_ugc

---
+++ er-cha-shu-ran-se-UGC/index.kt
@@ -0,0 +1,7 @@
+package com.github.masx200.leetcode_test.er_cha_shu_ran_se_ugc
+import com.github.masx200.leetcode_test.insert_into_a_binary_search_tree.TreeNode
+class Solution {
+    fun maxValue(root: TreeNode?, k: Int): Int {
+
+    }
+}
