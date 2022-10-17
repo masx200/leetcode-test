df73c286eb1800c7fce0e3294abda5047ad01cec
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-17 14:58:32 +0800

    Create test.ts

---
+++ er-cha-shu-ran-se-UGC/test.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "https://deno.land/std@0.159.0/testing/asserts.ts";
+import maxValue from "./index.ts";
+import { TreeNodeLeetCodeFromJSON } from "../utils/TreeNodeLeetCodeParse.ts";
+Deno.test("er-cha-shu-ran-se-UGC", () => {
+    assertEquals(12, maxValue(TreeNodeLeetCodeFromJSON([5, 2, 3, 4]), 2));
+    assertEquals(
+        16,
+        maxValue(TreeNodeLeetCodeFromJSON([4, 1, 3, 9, null, null, 2]), 2)
+    );
+});
