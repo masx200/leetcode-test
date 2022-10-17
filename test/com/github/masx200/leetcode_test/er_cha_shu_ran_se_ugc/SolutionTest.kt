062407a301fc45e62c85aa5b52d2ca245f2951d6
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-17 19:58:51 +0800

    TreeNodeLeetCodeParse

---
+++ test/com/github/masx200/leetcode_test/er_cha_shu_ran_se_ugc/SolutionTest.kt
@@ -0,0 +1,19 @@
+package com.github.masx200.leetcode_test.er_cha_shu_ran_se_ugc
+
+import com.github.masx200.leetcode_test.utils.TreeNodeLeetCodeParse
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class SolutionTest {
+
+    @Test
+    fun testmaxValue() {
+        assertEquals(12, Solution().maxValue(TreeNodeLeetCodeParse("[5, 2, 3, 4]"), 2));
+        assertEquals(
+            16,
+            Solution().maxValue(TreeNodeLeetCodeParse("[4, 1, 3, 9, null, null, 2]"), 2),
+        );
+    }
+
+
+}
---
+++ utils/TreeNodeLeetCodeParse.kt
@@ -0,0 +1,20 @@
+package com.github.masx200.leetcode_test.utils
+import com.github.masx200.leetcode_test.insert_into_a_binary_search_tree.TreeNode
+import com.github.masx200.leetcode_treenode_java.TreeNode.constructTree
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+fun TreeNodeLeetCodeParse(s: String): TreeNode? {
+    return treeNodeNew(constructTree(Json.decodeFromString(s)))
+}
+typealias TreeNodeClass = com.github.masx200.leetcode_treenode_java.TreeNode
+
+private fun treeNodeNew(t: TreeNodeClass?): TreeNode? {
+    if (t == null) {
+        return null;
+    }
+    val n = TreeNode();
+    n.`val` = t.`val`;
+    n.left = treeNodeNew(t.left);
+    n.right = treeNodeNew(t.right);
+    return n;
+}
