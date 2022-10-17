aba3b627cd208e49000238e1166f84af65717140
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-17 22:08:16 +0800

    Update test.ts

--- construct-binary-search-tree-from-preorder-traversal/test.ts
+++ construct-binary-search-tree-from-preorder-traversal/test.ts
@@ -1,6 +1,7 @@
 import bstFromPreorder from "./index.ts";
 import { assertEquals } from "https://deno.land/std@0.159.0/testing/asserts.ts";
 import { TreeNodeLeetCodeFromJSON } from "../utils/TreeNodeLeetCodeParse.ts";
+import { TreeNodeLeetCodeToJSON } from "../utils/TreeNodeLeetCodeStringify.ts";
 
 Deno.test("construct-binary-search-tree-from-preorder-traversal", () => {
     assertEquals(
@@ -11,6 +12,18 @@ Deno.test("construct-binary-search-tree-from-preorder-traversal", () => {
         [
             [8, 5, 10, 1, 7, null, 12],
             [1, null, 3],
-        ].map(TreeNodeLeetCodeFromJSON),
+        ].map(TreeNodeLeetCodeFromJSON)
+    );
+    assertEquals(
+        [
+            [8, 5, 1, 7, 10, 12],
+            [1, 3],
+        ]
+            .map(bstFromPreorder)
+            .map(TreeNodeLeetCodeToJSON),
+        [
+            [8, 5, 10, 1, 7, null, 12],
+            [1, null, 3],
+        ]
     );
 });
