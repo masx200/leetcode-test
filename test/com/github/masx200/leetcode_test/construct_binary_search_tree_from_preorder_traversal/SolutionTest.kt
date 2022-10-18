da27934fde65777ddd3ec3a594a85c81c03c70ea
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-18 10:53:22 +0800

    Update SolutionTest.kt

--- test/com/github/masx200/leetcode_test/construct_binary_search_tree_from_preorder_traversal/SolutionTest.kt
+++ test/com/github/masx200/leetcode_test/construct_binary_search_tree_from_preorder_traversal/SolutionTest.kt
@@ -2,13 +2,12 @@ package com.github.masx200.leetcode_test.construct_binary_search_tree_from_preor
 
 import com.cedarsoftware.util.DeepEquals.deepEquals
 import com.github.masx200.leetcode_test.utils.TreeNodeLeetCodeParse
-
 import com.github.masx200.leetcode_test.utils.TreeNodeLeetCodeStringify
+import kotlin.test.assertTrue
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.test.assertTrue
 
 internal class SolutionTest {
 
@@ -16,18 +15,23 @@ internal class SolutionTest {
     fun testbstFromPreorder() {
 
         assertEquals(
-            listOf(intArrayOf(8, 5, 1, 7, 10, 12), intArrayOf(1, 3)).map { Solution().bstFromPreorder(it) }
-                .map { TreeNodeLeetCodeStringify(it) },
-            listOf(arrayOf(8, 5, 10, 1, 7, null, 12), arrayOf(1, null, 3)).map { Json.encodeToString(it) }
+                listOf(intArrayOf(8, 5, 1, 7, 10, 12), intArrayOf(1, 3))
+                        .map { Solution().bstFromPreorder(it) }
+                        .map { TreeNodeLeetCodeStringify(it) },
+                listOf(arrayOf(8, 5, 10, 1, 7, null, 12), arrayOf(1, null, 3)).map {
+                    Json.encodeToString(it)
+                }
         )
 
         assertTrue(
-            deepEquals(
-                listOf(intArrayOf(8, 5, 1, 7, 10, 12), intArrayOf(1, 3)).map { Solution().bstFromPreorder(it) },
-                listOf(arrayOf(8, 5, 10, 1, 7, null, 12), arrayOf(1, null, 3)).map { Json.encodeToString(it) }
-                    .map { TreeNodeLeetCodeParse(it) }
-
-            )
+                deepEquals(
+                        listOf(intArrayOf(8, 5, 1, 7, 10, 12), intArrayOf(1, 3)).map {
+                            Solution().bstFromPreorder(it)
+                        },
+                        listOf("[8, 5, 10, 1, 7, null, 12]", "[1, null, 3]").map {
+                            TreeNodeLeetCodeParse(it)
+                        }
+                )
         )
     }
 }
