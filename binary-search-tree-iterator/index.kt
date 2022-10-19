ebb5d9a73b83ecd3f10f7008a741650523525ba3
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-19 10:43:58 +0800

    package com.github.masx200.leetcode_test.binary_search_tree_iterator

---
+++ binary-search-tree-iterator/index.kt
@@ -0,0 +1,27 @@
+package com.github.masx200.leetcode_test.binary_search_tree_iterator
+
+import com.github.masx200.leetcode_test.insert_into_a_binary_search_tree.TreeNode
+
+class BSTIterator(root: TreeNode?) {
+    private val generator = InOrderIterator(root).iterator()
+    fun next(): Int {
+        return this.generator.next()
+    }
+
+    fun hasNext(): Boolean {
+        return this.generator.hasNext()
+    }
+
+}
+
+fun InOrderIterator(root: TreeNode?): Sequence<Int> {
+    return sequence {
+
+        if (root == null) return@sequence
+
+        yieldAll(InOrderIterator(root.left))
+        yield(root.`val`)
+        yieldAll(InOrderIterator(root.right))
+    }
+}
+
---
+++ test/com/github/masx200/leetcode_test/binary_search_tree_iterator/BSTIteratorTest.kt
@@ -0,0 +1,20 @@
+package com.github.masx200.leetcode_test.binary_search_tree_iterator
+
+import com.github.masx200.leetcode_test.utils.TreeNodeLeetCodeParse
+import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
+
+internal class BSTIteratorTest {
+
+    @Test
+    fun testnext() {
+        val iterator = BSTIterator(TreeNodeLeetCodeParse("[7, 3, 15, null, null, 9, 20]"))
+
+        val res = mutableListOf<Int>()
+        while (iterator.hasNext()) {
+            res.add(iterator.next())
+        }
+        assertContentEquals(res, listOf(3, 7, 9, 15, 20))
+
+    }
+}
