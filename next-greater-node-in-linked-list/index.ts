17872b2a6ff92b3b2b11d156e19f3215ff66c2ad
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-26 15:47:12 +0800

    https://leetcode.cn/problems/next-greater-node-in-linked-list/

---
+++ next-greater-node-in-linked-list/index.ts
@@ -0,0 +1,34 @@
+import { ListNode } from "../reverse-linked-list/ListNode.ts";
+
+export default function nextLargerNodes(head: ListNode | null): number[] {
+    const res: number[] = [];
+
+    const stack: Array<ListNode | null> = [];
+
+    function dfs(head: ListNode | null) {
+        if (head === null) return;
+        stack.push(head);
+        dfs(head.next);
+    }
+    // 入栈 逆序遍历
+    dfs(head);
+    // 使用单调栈 栈顶元素记录距离最近的更大元素
+    const monotoneStack: number[] = [];
+    while (stack.length > 0) {
+        const node = stack.pop() as ListNode;
+        while (
+            monotoneStack.length > 0 &&
+            monotoneStack[monotoneStack.length - 1] <= node.val
+        ) {
+            monotoneStack.pop();
+        }
+        res.push(
+            monotoneStack.length > 0
+                ? monotoneStack[monotoneStack.length - 1]
+                : 0
+        );
+        monotoneStack.push(node.val);
+    }
+
+    return res.reverse();
+}
