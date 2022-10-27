add9d1f2d40969fab6288f96b9c5b498ae50a2d9
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-27 17:44:50 +0800

    https://leetcode.cn/problems/car-fleet/

---
+++ car-fleet/index.ts
@@ -0,0 +1,19 @@
+export default function carFleet(
+    target: number,
+    position: number[],
+    speed: number[],
+): number {
+    const n = position.length;
+    const cars = position.map((v, i) => [v, (target - v) / speed[i]]);
+
+    cars.sort((a, b) => a[0] - b[0]);
+
+    let ans = 0, t = n;
+    while (--t > 0) {
+        if (cars[t][1] < cars[t - 1][1]) ans++;
+        else {
+            cars[t - 1] = cars[t];
+        }
+    }
+    return ans + Number(t == 0);
+}
