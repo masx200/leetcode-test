93165f9da2671716c83c5ed599dd15ab9d869909
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-28 18:12:53 +0800

    https://leetcode.cn/problems/count-items-matching-a-rule/

---
+++ count-items-matching-a-rule/index.ts
@@ -0,0 +1,11 @@
+export default function countMatches(
+    items: string[][],
+    ruleKey: string,
+    ruleValue: string,
+): number {
+    return items.filter(([t, c, n]) =>
+        (ruleKey === "type" && t == ruleValue) ||
+        (ruleKey === "color" && c == ruleValue) ||
+        (ruleKey === "name" && n == ruleValue)
+    ).length;
+}
