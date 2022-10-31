da95b292f9b130e97b8ee745a28daa3864b7dc5d
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-31 09:45:59 +0800

    https://leetcode.cn/problems/minimum-addition-to-make-integer-beautiful/

--- magical-string/index.ts
+++ magical-string/index.ts
@@ -1,5 +1,6 @@
 export default function magicalString(n: number): number {
-    if (cache.length === 0) {
+    if (results.length === 0) {
+        const cache: number[] = [];
         cache.push(1, 2, 2);
         let c = 2;
         while (cache.length <= 1e5) {
@@ -8,12 +9,14 @@ export default function magicalString(n: number): number {
             else cache.push(ch, ch);
             c += 1;
         }
+        {
+            let c = 0;
+            for (let i = 0; i <= 1e5; i++) {
+                if (cache[i] === 1) c += 1;
+                results[i] = c;
+            }
+        }
     }
-
-    let c = 0;
-    for (let i = 0; i < n; i++) {
-        if (cache[i] === 1) c += 1;
-    }
-    return c;
+    return results[n - 1];
 }
-const cache: number[] = [];
+const results: number[] = [];
---
+++ minimum-addition-to-make-integer-beautiful/index.ts
@@ -0,0 +1,25 @@
+export default function makeIntegerBeautiful(
+    n: number,
+    target: number,
+): number {
+    // 分隔n为数字数组
+    const nArr = n.toString().split("").map(Number);
+    // 数组求和
+    let sum = nArr.reduce((pre, cur) => pre + cur);
+    // 需要添加的数字
+    let rz = 0;
+    // 记录当前执行到第 base + 1 次
+    let base = 0;
+    for (let i = nArr.length - 1; i >= 0; i--) {
+        // 每次执行判断是否符合结果，若符合直接输出
+        if (sum <= target) return rz;
+        const currNum = nArr[i];
+        // 更新结果
+        sum = sum - currNum + 1;
+        rz += Math.pow(10, base) * (10 - currNum);
+        // 每次操作前一位进一
+        nArr[nArr.length - 2 - base]++;
+        base++;
+    }
+    return rz;
+}
