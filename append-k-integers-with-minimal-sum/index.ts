a0394ed2162ab9bcf20f907991cf01ee7b811ece
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-31 19:48:22 +0800

    https://leetcode.cn/problems/append-k-integers-with-minimal-sum/

---
+++ append-k-integers-with-minimal-sum/index.ts
@@ -0,0 +1,15 @@
+export default function minimalKSum(nums: number[], k: number): number {
+    nums = [...new Set(nums)].sort((a, b) => a - b);
+    const n: number = nums.length;
+    let exist: number = 0;
+    for (let i: number = 0; i < n; i++) {
+        if (nums[i] <= k) {
+            k++;
+            exist += nums[i];
+        } else {
+            break;
+        }
+    }
+
+    return k * (k + 1) / 2 - exist;
+}
