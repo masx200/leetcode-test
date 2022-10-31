1b31927a2a84efc0cdb927b1cd1f56a882946b09
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-31 14:44:56 +0800

    https://leetcode.cn/problems/the-kth-factor-of-n/

---
+++ the-kth-factor-of-n/index.ts
@@ -0,0 +1,24 @@
+export default function kthFactor(n: number, k: number): number {
+    let count = 0, factor = 0;
+    for (factor = 1; factor * factor <= n; ++factor) {
+        if (n % factor == 0) {
+            ++count;
+            if (count == k) {
+                return factor;
+            }
+        }
+    }
+    --factor;
+    if (factor * factor == n) {
+        --factor;
+    }
+    for (; factor > 0; --factor) {
+        if (n % factor == 0) {
+            ++count;
+            if (count == k) {
+                return n / factor;
+            }
+        }
+    }
+    return -1;
+}
