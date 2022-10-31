aa8495b5f85d5603378fbc7f49d4a5f4099eae26
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-31 16:19:46 +0800

    测试

--- inorder-successor-in-bst-ii/test.ts
+++ inorder-successor-in-bst-ii/test.ts
@@ -4,6 +4,33 @@ import { assert, assertEquals } from "asserts";
 Deno.test("inorder-successor-in-bst-ii", () => {
     const map = new Map<number, Node>();
 
+    also(new Node(5), (n) => {
+        map.set(n.val, n);
+        n.left = also(new Node(3, null, null, n), (n) => {
+            map.set(n.val, n);
+            n.left = also(new Node(2, null, null, n), (n) => {
+                map.set(n.val, n);
+                n.left = also(new Node(1, null, null, n), (n) => {
+                    map.set(n.val, n);
+                });
+            });
+            n.right = also(
+                new Node(4, null, null, n),
+                (n) => map.set(n.val, n),
+            );
+        });
+        n.right = also(new Node(6, null, null, n), (n) => map.set(n.val, n));
+    });
+
+    const input = map.get(6);
+    const output = null;
+
+    assert(input);
+    assertEquals(inorderSuccessor(input), output);
+});
+Deno.test("inorder-successor-in-bst-ii", () => {
+    const map = new Map<number, Node>();
+
     also(new Node(2), (n) => {
         map.set(n.val, n);
         n.left = also(new Node(1, null, null, n), (n) => map.set(n.val, n));
@@ -12,7 +39,7 @@ Deno.test("inorder-successor-in-bst-ii", () => {
 
     const input = map.get(1);
     const output = map.get(2);
-    // console.log({map,input,output})
+
     assert(input);
     assertEquals(inorderSuccessor(input), output);
 });
