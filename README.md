1c235eb8da6d62f60390e044ec8b054eb811da85
masx200 <34191203+masx200@users.noreply.github.com>
2022-11-02 15:44:54 +0800

    https://leetcode.cn/problems/bulls-and-cows

--- README.md
+++ README.md
@@ -45,6 +45,8 @@ Step 2. Add the dependency
 
 <summary>展开查看</summary>
 
+https://leetcode.cn/problems/bulls-and-cows
+
 https://leetcode.cn/problems/h-index-ii/
 
 https://leetcode.cn/problems/maximum-repeating-substring/
---
+++ bulls-and-cows/index.ts
@@ -0,0 +1,13 @@
+import { counter } from "../substring-with-concatenation-of-all-words/counter.ts";
+function getHint(secret: string, guess: string): string {
+    const bulls = [...secret].filter((v, i) => guess[i] === v).length;
+
+    const cntS = counter(secret);
+    const cntG = counter(guess);
+    const cows = [...cntS.entries()].reduce(
+        (p, [k, v]) => p + Math.min(v, cntG.get(k) ?? 0),
+        0,
+    ) - bulls;
+    return `${bulls}A${cows}B`;
+}
+export default getHint;
