909dee254c57e8cce8b7a20c4ce24ff28514f6c4
masx200 <masx200@qq.com>
2022-11-06 10:18:44 +0800

    https://leetcode.cn/problems/zL2zJU

---
+++ zL2zJU/index.ts
@@ -0,0 +1,44 @@
+export default function observingPeriodicity(
+    record: number[],
+    robot: number[],
+): number[] {
+    const n = robot.length;
+    const res: number[] = Array(n).fill(0);
+    backtrack(0, record, robot, res);
+    return res;
+}
+
+function backtrack(
+    robotIndex: number,
+    record: number[],
+    robot: number[],
+    res: number[],
+): boolean {
+    const n = robot.length;
+    const m = record.length;
+    if (robotIndex === n) {
+        return record.every((a) => a === 0);
+    }
+
+    const maxPeriod = m - robot[robotIndex];
+
+    loop:
+    for (let i = 1; i <= maxPeriod; i++) {
+        for (let j = robot[robotIndex]; j < m; j += i) {
+            if (record[j] === 0) continue loop;
+        }
+
+        for (let j = robot[robotIndex]; j < m; j += i) {
+            record[j]--;
+        }
+        res[robotIndex] = i;
+
+        if (backtrack(robotIndex + 1, record, robot, res)) {
+            return true;
+        }
+        for (let j = robot[robotIndex]; j < m; j += i) {
+            record[j]++;
+        }
+    }
+    return false;
+}
