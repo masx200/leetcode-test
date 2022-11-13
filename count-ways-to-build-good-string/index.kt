6ab0783ff5d658ea138d5373570cdf602e8f0724
masx200 <masx200@qq.com>
2022-11-13 11:26:40 +0800

    <source>count-ways-to-build-good-string</source>

---
+++ count-ways-to-build-good-string/index.kt
@@ -0,0 +1,18 @@
+package com.github.masx200.leetcode_test.count_ways_to_build_good_string
+
+const val mod = 1e9 + 7;
+
+class Solution {
+    fun countGoodStrings(low: Int, high: Int, zero: Int, one: Int): Int {
+        val dp = IntArray(high + 1) { 0 }
+        dp[0] = 1
+        var ans = 0
+        for (i in 1..high) {
+            if (i >= zero) dp[i] += dp[i - zero];
+            if (i >= one) dp[i] += dp[i - one];
+            dp[i] = (dp[i] % mod).toInt();
+            if (i >= low) ans = ((ans + dp[i]) % mod).toInt();
+        }
+        return ans;
+    }
+}
