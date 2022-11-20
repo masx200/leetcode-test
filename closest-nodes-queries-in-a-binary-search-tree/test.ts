6ba39728c096226952cc6308f34d443c9d538df7
masx200 <masx200@qq.com>
2022-11-20 13:47:38 +0800

    测试

---
+++ closest-nodes-queries-in-a-binary-search-tree/test.ts
@@ -0,0 +1,28 @@
+import { assertEquals } from "asserts";
+import { TreeNodeLeetCodeFromJSON } from "../utils/TreeNodeLeetCodeParse.ts";
+import closestNodes from "./index.ts";
+Deno.test("closest-nodes-queries-in-a-binary-search-tree", () => {
+    const root = [
+            6,
+            2,
+            13,
+            1,
+            4,
+            9,
+            15,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            14,
+        ],
+        queries = [2, 5, 16];
+    const result = [
+        [2, 2],
+        [4, 6],
+        [15, -1],
+    ];
+    assertEquals(closestNodes(TreeNodeLeetCodeFromJSON(root), queries), result);
+});
