71484df8d1ef718dee1eafff3dc24933a976d9ab
masx200 <masx200@qq.com>
2022-11-28 15:40:59 +0800

    https://leetcode.cn/problems/minimum-changes-to-make-alternating-binary-string

--- README.md
+++ README.md
@@ -45,6 +45,8 @@ Step 2. Add the dependency
 
 <summary>展开查看</summary>
 
+https://leetcode.cn/problems/minimum-changes-to-make-alternating-binary-string
+
 https://leetcode.cn/problems/sum-of-all-odd-length-subarrays
 
 https://leetcode.cn/problems/combination-sum-iv/
---
+++ minimum-changes-to-make-alternating-binary-string/index.ts
@@ -0,0 +1,10 @@
+function minOperations(s: string): number {
+    const res = Array.prototype.reduce.call(
+        s,
+        //@ts-ignore
+        (p: number, c: string, i: number) => p + Number(i % 2 === Number(c)),
+        0
+    ) as number;
+    return Math.min(res, s.length - res);
+}
+export default minOperations;
