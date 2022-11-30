7429db99722285481e72523778df1f167f5218a1
masx200 <masx200@qq.com>
2022-11-30 13:41:55 +0800

    nearestValidPoint

--- README.md
+++ README.md
@@ -45,6 +45,8 @@ Step 2. Add the dependency
 
 <summary>展开查看</summary>
 
+https://leetcode.cn/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
+
 https://leetcode.cn/problems/leaf-similar-trees
 
 https://leetcode.cn/problems/find-the-town-judge
---
+++ find-nearest-point-that-has-the-same-x-or-y-coordinate/index.ts
@@ -0,0 +1,10 @@
+function nearestValidPoint(x: number, y: number, points: number[][]): number {
+    return points.reduce(
+        ([p, d], [a, b], i) =>
+            (x === a || y === b) && Math.abs(x - a) + Math.abs(y - b) < d
+                ? [i, Math.abs(x - a) + Math.abs(y - b)]
+                : [p, d],
+        [-1, Infinity],
+    )[0];
+}
+export default nearestValidPoint;
