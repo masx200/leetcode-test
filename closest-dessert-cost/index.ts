7c190724b2f94c88a8324fd493601fe95fe9579e
masx200 <masx200@qq.com>
2022-12-03 21:18:30 +0800

    https://leetcode.cn/problems/closest-dessert-cost

---
+++ closest-dessert-cost/index.ts
@@ -0,0 +1,33 @@
+function closestCost(
+    baseCosts: number[],
+    toppingCosts: number[],
+    target: number,
+): number {
+    const m = toppingCosts.length;
+    let ret = Infinity;
+    let diff = Infinity;
+    for (const base of baseCosts) {
+        for (let state = 0; state < Math.pow(3, m); ++state) {
+            const top = topping(toppingCosts, state);
+            const temp = Math.abs(base + top - target);
+            if (temp < diff) {
+                ret = base + top;
+                diff = temp;
+            } else if (temp === diff && base + top < ret) {
+                ret = base + top;
+            }
+        }
+    }
+    return ret;
+}
+export default closestCost;
+
+function topping(toppingCosts: number[], state: number) {
+    const m = toppingCosts.length;
+    let top = 0;
+    for (let i = 0; i < m; i++) {
+        top += toppingCosts[i] * (state % 3);
+        state = Math.floor(state / 3);
+    }
+    return top;
+}
