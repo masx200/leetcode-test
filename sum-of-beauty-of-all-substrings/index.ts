c70fe4cebbfbb0db9c4ec81353e5f179a3a4748f
masx200 <masx200@qq.com>
2022-12-12 11:46:07 +0800

    beautySum

---
+++ sum-of-beauty-of-all-substrings/index.ts
@@ -0,0 +1,23 @@
+function beautySum(s: string): number {
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        const cnt = new Array<number>(26).fill(0);
+        let maxFreq = 0;
+        for (let j = i; j < s.length; j++) {
+            cnt[s[j].charCodeAt(0) - "a".charCodeAt(0)]++;
+            maxFreq = Math.max(
+                maxFreq,
+                cnt[s[j].charCodeAt(0) - "a".charCodeAt(0)]
+            );
+            let minFreq = s.length;
+            for (let k = 0; k < 26; k++) {
+                if (cnt[k] > 0) {
+                    minFreq = Math.min(minFreq, cnt[k]);
+                }
+            }
+            res += maxFreq - minFreq;
+        }
+    }
+    return res;
+}
+export default beautySum;
