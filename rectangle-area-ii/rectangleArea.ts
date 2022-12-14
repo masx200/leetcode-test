10aa5a0462b5a0c8cfca8f378401d8bda0e8bffe
masx200 <34191203+masx200@users.noreply.github.com>
2022-12-14 12:11:00 +0800

    Update rectangleArea.ts

--- rectangle-area-ii/rectangleArea.ts
+++ rectangle-area-ii/rectangleArea.ts
@@ -8,17 +8,6 @@ export default function rectangleArea(rectangles: number[][]): number {
         insert(sweep, left, down, up, 1);
         insert(sweep, right, down, up, -1);
     }
-    // for (const arr of sweep.values()) {
-    //     arr.sort((a, b) => {
-    //         if (a[0] != b[0]) {
-    //             return a[0] - b[0];
-    //         } else if (a[1] != b[1]) {
-    //             return a[1] - b[1];
-    //         } else {
-    //             return a[2] - b[2];
-    //         }
-    //     });
-    // }
     let ans = 0n;
     const sorted = [...sweep.entries()].sort((a, b) => a[0] - b[0]);
     for (const [index, [key, arr]] of sorted.entries()) {
