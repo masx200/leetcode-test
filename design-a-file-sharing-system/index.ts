b02e5dfa6308bc1f55bed1ef60551c496d911602
GitHub <noreply@github.com>
2022-12-19 19:47:29 +0000

    Update dependencies

--- design-a-file-sharing-system/index.ts
+++ design-a-file-sharing-system/index.ts
@@ -65,4 +65,4 @@ export default class FileSharing {
         return usersList;
     }
 }
-import { BinaryHeap } from "https://deno.land/std@0.168.0/collections/binary_heap.ts";
+import { BinaryHeap } from "https://deno.land/std@0.170.0/collections/binary_heap.ts";
