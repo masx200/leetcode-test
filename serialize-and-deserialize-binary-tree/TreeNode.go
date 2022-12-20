c2f396d75be7dff4bd82d1c29cfd14dc617fd328
masx200 <masx200@qq.com>
2022-12-20 12:21:03 +0800

    TreeNodeInterFace

--- serialize-and-deserialize-binary-tree/TreeNode.go
+++ serialize-and-deserialize-binary-tree/TreeNode.go
@@ -1,7 +1,57 @@
 package index
 
+type TreeNodeInterFace interface {
+	GetVal() int
+	SetVal(int)
+	GetLeft() TreeNodeInterFace
+	SetLeft(TreeNodeInterFace)
+	GetRight() TreeNodeInterFace
+	SetRight(TreeNodeInterFace)
+}
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func (t *TreeNode) GetVal() int {
+	return t.Val
+}
+func (t *TreeNode) SetVal(val int) {
+
+	t.Val = val
+}
+func (t *TreeNode) GetLeft() TreeNodeInterFace {
+
+	return t.Left
+}
+func (t *TreeNode) SetLeft(n TreeNodeInterFace) {
+	if n == nil {
+		t.Left = nil
+		return
+	}
+	new := TreeNodeInterFaceToStruct(n)
+	t.Left = new
+}
+func (t *TreeNode) GetRight() TreeNodeInterFace {
+	return t.Right
+}
+func (t *TreeNode) SetRight(n TreeNodeInterFace) {
+
+	if n == nil {
+		t.Right = nil
+		return
+	}
+	new := TreeNodeInterFaceToStruct(n)
+	t.Right = new
+}
+
+func TreeNodeInterFaceToStruct(n TreeNodeInterFace) *TreeNode {
+	new := &TreeNode{Val: n.GetVal()}
+
+	new.SetLeft(n.GetLeft())
+
+	new.SetRight(n.GetRight())
+	return new
 }
--- serialize-and-deserialize-binary-tree/go.mod
+++ serialize-and-deserialize-binary-tree/go.mod
@@ -5,3 +5,5 @@ go 1.18
 require gotest.tools/v3 v3.4.0
 
 require github.com/google/go-cmp v0.5.9 // indirect
+
+
