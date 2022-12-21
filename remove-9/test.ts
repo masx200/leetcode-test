5c025bfc5ba0e6340ccc27cbab7646647b07179d
masx200 <masx200@qq.com>
2022-12-21 15:21:43 +0800

    测试

---
+++ remove-9/test.ts
@@ -0,0 +1,20 @@
+Deno.test("remove-9", () => {
+    assertEquals(
+        [1, 2, 3, 4, 5, 6, 7, 8, 10, 11],
+        [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+        ].map(solution),
+    );
+});
+
+import solution from "./index.ts";
+import { assertEquals } from "asserts";
