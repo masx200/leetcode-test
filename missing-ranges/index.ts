85357b27de4fb5e61003b668a53bd59edc30dab1
masx200 <masx200@qq.com>
2022-12-22 15:56:36 +0800

    https://leetcode.cn/problems/missing-ranges/

---
+++ missing-ranges/index.ts
@@ -0,0 +1,20 @@
+export default function findMissingRanges(
+    nums: number[],
+    lower: number,
+    upper: number,
+): string[] {
+    const ans: string[] = [];
+    nums.push(upper + 1);
+
+    let last = lower - 1;
+
+    for (const num of nums) {
+        if (num - last === 2) {
+            ans.push(String(last + 1));
+        } else if (num - last > 2) {
+            ans.push(`${last + 1}->${num - 1}`);
+        }
+        last = num;
+    }
+    return ans;
+}
---
+++ missing-ranges/test.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from "asserts";
+import findMissingRanges from "./index.ts";
+Deno.test("missing-ranges", () => {
+    assertEquals(
+        ["2", "4->49", "51->74", "76->99"],
+        findMissingRanges([0, 1, 3, 50, 75], 0, 99),
+    );
+
+    assertEquals(
+        ["-100->-1", "2", "4->49", "51->74"],
+        findMissingRanges([0, 1, 3, 50, 75], -100, 75),
+    );
+
+    assertEquals(
+        ["2", "4->49", "51->74", "76->200"],
+        findMissingRanges([0, 1, 3, 50, 75], 0, 200),
+    );
+    assertEquals(
+        ["2", "4->49", "51->74", "76->89", "91->99"],
+        findMissingRanges([0, 1, 3, 50, 75, 90], 0, 99),
+    );
+});
