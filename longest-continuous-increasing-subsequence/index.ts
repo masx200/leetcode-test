a488412925d49cec45c2f96a50193d61e47f4d0b
masx200 <masx200@qq.com>
2022-12-23 10:09:33 +0800

    https://leetcode.cn/problems/longest-continuous-increasing-subsequence/

---
+++ longest-continuous-increasing-subsequence/index.ts
@@ -0,0 +1,13 @@
+function findLengthOfLCIS(nums: number[]): number {
+    let ans = 0;
+    const n = nums.length;
+    let start = 0;
+    for (let i = 0; i < n; i++) {
+        if (i > 0 && nums[i] <= nums[i - 1]) {
+            start = i;
+        }
+        ans = Math.max(ans, i - start + 1);
+    }
+    return ans;
+}
+export default findLengthOfLCIS;
