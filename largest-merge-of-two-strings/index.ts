f47ccfeb22ae60b8742720ee116fc685257a3cb6
masx200 <masx200@qq.com>
2022-12-23 20:00:22 +0800

    https://leetcode.cn/problems/largest-merge-of-two-strings

---
+++ largest-merge-of-two-strings/index.ts
@@ -0,0 +1,15 @@
+function largestMerge(word1: string, word2: string): string {
+    let merge = "";
+    let i = 0, j = 0;
+    while (i < word1.length || j < word2.length) {
+        if (i < word1.length && word1.slice(i) > word2.slice(j)) {
+            merge += word1[i];
+            i++;
+        } else {
+            merge += word2[j];
+            j++;
+        }
+    }
+    return merge;
+}
+export default largestMerge;
