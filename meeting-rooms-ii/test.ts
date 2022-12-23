bb2751c05596652da69e0c4c465bebc13b940900
masx200 <masx200@qq.com>
2022-12-23 14:38:01 +0800

    Create test.ts

---
+++ meeting-rooms-ii/test.ts
@@ -0,0 +1,16 @@
+import { assertEquals } from "asserts";
+import minMeetingRooms from "./index.ts";
+Deno.test("meeting-rooms-ii", () => {
+    const Input = [
+            [0, 30],
+            [5, 10],
+            [15, 20],
+        ],
+        Output = 2;
+    assertEquals(minMeetingRooms(Input), Output);
+});
+Deno.test("meeting-rooms-ii", () => {
+    const Input = [[7, 10], [2, 4]],
+        Output = 1;
+    assertEquals(minMeetingRooms(Input), Output);
+});
