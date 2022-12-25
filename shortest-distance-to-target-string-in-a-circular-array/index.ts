4c8d971731fbed5e1d909184b5b81ecb3d15cb24
masx200 <masx200@qq.com>
2022-12-25 10:47:04 +0800

    https://leetcode.cn/contest/weekly-contest-325/problems/shortest-distance-to-target-string-in-a-circular-array/

---
+++ shortest-distance-to-target-string-in-a-circular-array/index.ts
@@ -0,0 +1,21 @@
+function closetTarget(
+    words: string[],
+    target: string,
+    startIndex: number,
+): number {
+    if (!words.includes(target)) return -1;
+
+    if (words[startIndex] === target) return 0;
+    const indexes: number[] = [];
+
+    for (const [index, word] of words.entries()) {
+        if (word === target) indexes.push(index);
+    }
+
+    return Math.min(
+        ...indexes.map((index) => Math.abs(index - startIndex)),
+        ...indexes.map((index) => Math.abs(words.length + index - startIndex)),
+        ...indexes.map((index) => Math.abs(-words.length + index - startIndex)),
+    );
+}
+export default closetTarget;
