a478f692794be4a7f6c5dd2df61361c43e18bba3
masx200 <masx200@qq.com>
2022-12-25 16:09:38 +0800

    Create test.ts

---
+++ online-stock-span/test.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from "asserts";
+import { runScript } from "leetcode-class";
+import StockSpanner from "./index.ts";
+
+Deno.test("online-stock-span", () => {
+    assertEquals(
+        runScript(
+            [
+                "StockSpanner",
+                "next",
+                "next",
+                "next",
+                "next",
+                "next",
+                "next",
+                "next",
+            ],
+            [[], [100], [80], [60], [70], [60], [75], [85]],
+            StockSpanner,
+        ),
+        [null, 1, 1, 1, 2, 1, 4, 6],
+    );
+});
