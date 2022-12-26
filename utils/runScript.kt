743b309cc20b6e36aef4c4c41b5ea94704af148e
masx200 <masx200@qq.com>
2022-12-26 20:38:08 +0800

    Update runScript.kt

--- utils/runScript.kt
+++ utils/runScript.kt
@@ -5,13 +5,15 @@ import kotlin.reflect.KFunction
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.jvm.jvmName
 
-fun runScript(commands: List<String>, inputs: ArrayList<ArrayList<Any>>, classes: KClass<*>): List<Any?> {
-//    println(commands)
-//    println(inputs)
-//    println(classes)
+fun runScript(
+        commands: List<String>,
+        inputs: ArrayList<ArrayList<Any>>,
+        classes: KClass<*>
+): List<Any?> {
+
     val kFunction = classes.constructors.first()
     if (kFunction.parameters.size != inputs[0].size) throw Error("constructor parameters mismatch")
-//    println(kFunction.parameters)
+
     val instance = kFunction.call(*Array(inputs[0].size) { inputs[0][it] })
     val res = List<Any?>(commands.size) { null }.toMutableList()
     val methodMap = hashMapOf<String, KFunction<*>>()
@@ -19,14 +21,11 @@ fun runScript(commands: List<String>, inputs: ArrayList<ArrayList<Any>>, classes
 
         methodMap[fu.name] = fu
     }
-//    println(methodMap)
-//    println(instance)
+
     for ((i, arg) in inputs.withIndex()) {
         if (i != 0) {
             val name = commands[i]
             val fu = methodMap[name] ?: throw Error("method not found")
-//            (fu.parameters).forEach(::println)
-//            println(arg[0].javaClass)
 
             for (j in arg.indices) {
                 @Suppress("KotlinConstantConditions")
@@ -34,24 +33,20 @@ fun runScript(commands: List<String>, inputs: ArrayList<ArrayList<Any>>, classes
                     val jvmName = (fu.parameters[j + 1].type.classifier as KClass<*>).jvmName
                     val old = arg[j]
                     if (old is Number) {
-                        arg[j] = when (jvmName) {
-                            "int" -> old.toInt()
-                            "double" -> old.toDouble()
-                            "float" -> old.toFloat()
-                            "long" -> old.toLong()
-                            "char" -> old.toChar()
-                            "short" -> old.toShort()
-                            "byte" -> old.toByte()
-                            else -> {
-                                throw Error("error number type:$jvmName")
-                            }
-                        }
+                        arg[j] =
+                                when (jvmName) {
+                                    "int" -> old.toInt()
+                                    "double" -> old.toDouble()
+                                    "float" -> old.toFloat()
+                                    "long" -> old.toLong()
+                                    "char" -> old.toChar()
+                                    "short" -> old.toShort()
+                                    "byte" -> old.toByte()
+                                    else -> {
+                                        throw Error("error number type:$jvmName")
+                                    }
+                                }
                     }
-//                    println(arg[j].javaClass.name)
-
-//                    println(jvmName)
-
-//                    println(arg[j] is Number)
                 }
             }
 
@@ -63,7 +58,6 @@ fun runScript(commands: List<String>, inputs: ArrayList<ArrayList<Any>>, classes
                 res[i] = re
             }
         }
-
     }
 
     return res
