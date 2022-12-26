d8aa1d55f50818a836c3882d5ad8927352698f16
masx200 <masx200@qq.com>
2022-12-26 15:20:57 +0800

    package com.github.masx200.leetcode_test.operations_lcci

---
+++ operations-lcci/index.kt
@@ -0,0 +1,15 @@
+package com.github.masx200.leetcode_test.operations_lcci
+class Operations() {
+
+    fun minus(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun divide(a: Int, b: Int): Int {
+        return a / b
+    }
+}
