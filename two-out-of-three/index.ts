10a2ee96f7da464c014c57fe129dd4eb13443afc
masx200 <masx200@qq.com>
2022-12-28 12:20:13 +0800

    https://leetcode.cn/problems/two-out-of-three

---
+++ two-out-of-three/index.ts
@@ -0,0 +1,18 @@
+function twoOutOfThree(
+    nums1: number[],
+    nums2: number[],
+    nums3: number[],
+): number[] {
+    const map = new Map<number, Set<number>>();
+
+    for (const [i, n] of [nums1, nums2, nums3].entries()) {
+        for (const v of n) {
+            const value = map.get(v) ?? new Set();
+            value.add(i);
+            map.set(v, value);
+        }
+    }
+    // console.log(map)
+    return [...map.entries()].filter(([_, s]) => s.size >= 2).map((a) => a[0]);
+}
+export default twoOutOfThree;
