d10dd441602a0cdbf7499867170900d56fa06b48
masx200 <masx200@qq.com>
2022-12-29 17:46:54 +0800

    Update test.ts

--- exam-room/test.ts
+++ exam-room/test.ts
@@ -6,8 +6,52 @@ Deno.test("exam-room", () => {
         runScript(
             ["ExamRoom", "seat", "seat", "seat", "seat", "leave", "seat"],
             [[10], [], [], [], [], [4], []],
-            ExamRoom,
+            ExamRoom
         ),
-        [null, 0, 9, 4, 2, null, 5],
+        [null, 0, 9, 4, 2, null, 5]
+    );
+});
+Deno.test("exam-room", () => {
+    assertEquals(
+        runScript(
+            [
+                "ExamRoom",
+                "seat",
+                "seat",
+                "seat",
+                "leave",
+                "leave",
+                "seat",
+                "seat",
+                "seat",
+                "seat",
+                "seat",
+                "seat",
+                "seat",
+                "seat",
+                "seat",
+                "leave",
+            ],
+            [
+                [10],
+                [],
+                [],
+                [],
+                [0],
+                [4],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [0],
+            ],
+            ExamRoom
+        ),
+        [null, 0, 9, 4, null, null, 0, 4, 2, 6, 1, 3, 5, 7, 8, null]
     );
 });
