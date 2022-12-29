2db6129a21feb59ef73d1dc6129c6d24405eb221
masx200 <masx200@qq.com>
2022-12-29 15:59:56 +0800

    Update runScript.kt

--- utils/runScript.kt
+++ utils/runScript.kt
@@ -7,9 +7,9 @@ import kotlin.reflect.full.memberFunctions
 import kotlin.reflect.jvm.jvmName
 
 fun runScript(
-    commands: List<String>,
-    inputs: ArrayList<ArrayList<Any>>,
-    classes: KClass<*>
+        commands: List<String>,
+        inputs: ArrayList<ArrayList<Any>>,
+        classes: KClass<*>
 ): List<Any?> {
 
     val kFunction = classes.constructors.first()
@@ -18,18 +18,18 @@ fun runScript(
     val instance = kFunction.call(*Array(inputs[0].size) { inputs[0][it] })
     val res = List<Any?>(commands.size) { null }.toMutableList()
     val methodMap = hashMapOf<String, KFunction<*>>()
-   
-for (fs in listOf(classes.memberFunctions,classes.declaredFunctions))
- for (fu in fs) {
 
-        methodMap[fu.name] = fu
+    for (fs in listOf(classes.memberFunctions, classes.declaredFunctions)) {
+        for (fu in fs) {
+
+            methodMap[fu.name] = fu
+        }
     }
 
     for ((i, arg) in inputs.withIndex()) {
         if (i != 0) {
             val name = commands[i]
-            val fu = methodMap[name] 
-            ?: throw Error("method not found:$name")
+            val fu = methodMap[name] ?: throw Error("method not found:$name")
             methodMap[name] = fu
 
             for (j in arg.indices) {
@@ -39,18 +39,18 @@ for (fs in listOf(classes.memberFunctions,classes.declaredFunctions))
                     val old = arg[j]
                     if (old is Number) {
                         arg[j] =
-                            when (jvmName) {
-                                "int" -> old.toInt()
-                                "double" -> old.toDouble()
-                                "float" -> old.toFloat()
-                                "long" -> old.toLong()
-                                "char" -> old.toChar()
-                                "short" -> old.toShort()
-                                "byte" -> old.toByte()
-                                else -> {
-                                    throw Error("error number type:$jvmName")
+                                when (jvmName) {
+                                    "int" -> old.toInt()
+                                    "double" -> old.toDouble()
+                                    "float" -> old.toFloat()
+                                    "long" -> old.toLong()
+                                    "char" -> old.toChar()
+                                    "short" -> old.toShort()
+                                    "byte" -> old.toByte()
+                                    else -> {
+                                        throw Error("error number type:$jvmName")
+                                    }
                                 }
-                            }
                     }
                 }
             }
