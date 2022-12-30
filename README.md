c000828230574a75e28f312629b64a6b0ba25c2b
masx200 <masx200@qq.com>
2022-12-30 16:26:11 +0800

    https://leetcode.cn/problems/minimum-number-of-moves-to-seat-everyone/

--- README.md
+++ README.md
@@ -49,6 +49,8 @@ Step 2. Add the dependency
 
 <summary>展开查看</summary>
 
+https://leetcode.cn/problems/minimum-number-of-moves-to-seat-everyone/
+
 https://leetcode.cn/problems/time-needed-to-inform-all-employees
 
 https://leetcode.cn/problems/rotting-oranges
---
+++ minimum-number-of-moves-to-seat-everyone/index.ts
@@ -0,0 +1,9 @@
+export default function minMovesToSeat(
+    seats: number[],
+    students: number[],
+): number {
+    seats.sort((a, b) => a - b);
+    students.sort((a, b) => a - b);
+
+    return seats.reduce((p, c, i) => p + Math.abs(c - students[i]), 0);
+}
