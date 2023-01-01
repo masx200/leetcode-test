42a6ae4d67a2bad8418bcb7dd9d7b2e3169e30b4
masx200 <masx200@qq.com>
2023-01-01 17:03:05 +0800

    https://leetcode.cn/problems/number-of-orders-in-the-backlog

---
+++ number-of-orders-in-the-backlog/index.ts
@@ -0,0 +1,59 @@
+function getNumberOfBacklogOrders(orders: number[][]): number {
+    const MOD = 1000000007;
+
+    const buyOrders = new Heap<[number, number]>((a, b) => b[0] - a[0]);
+    const sellOrders = new Heap<[number, number]>((a, b) => a[0] - b[0]);
+
+    for (const order of orders) {
+        const [price, amount, orderType] = order;
+
+        if (orderType == 0) {
+            let count = amount;
+            while (
+                count > 0 &&
+                !sellOrders.isEmpty() &&
+                sellOrders.top()[0] <= price
+            ) {
+                const sellOrder = sellOrders.pop();
+                const sellAmount = Math.min(count, sellOrder[1]);
+                count -= sellAmount;
+                sellOrder[1] -= sellAmount;
+                if (sellOrder[1] > 0) {
+                    sellOrders.push(sellOrder);
+                }
+            }
+            if (count > 0) {
+                buyOrders.push([price, count]);
+            }
+        } else {
+            let count = amount;
+            while (
+                count > 0 &&
+                !buyOrders.isEmpty() &&
+                buyOrders.top()[0] >= price
+            ) {
+                const buyOrder = buyOrders.pop();
+                const buyAmount = Math.min(count, buyOrder[1]);
+                count -= buyAmount;
+                buyOrder[1] -= buyAmount;
+                if (buyOrder[1] > 0) {
+                    buyOrders.push(buyOrder);
+                }
+            }
+            if (count > 0) {
+                sellOrders.push([price, count]);
+            }
+        }
+    }
+    let total = 0;
+    for (const pq of [buyOrders, sellOrders]) {
+        //@ts-ignore
+        for (const order of pq._nodes) {
+            total = (total + order[1]) % MOD;
+        }
+    }
+    return total;
+}
+
+export default getNumberOfBacklogOrders;
+import { Heap } from "https://cdn.skypack.dev/@datastructures-js/heap@4.2.2/src/heap.js?dts";
---
+++ number-of-orders-in-the-backlog/test.ts
@@ -0,0 +1,24 @@
+import { assertEquals } from "asserts";
+import getNumberOfBacklogOrders from "./index.ts";
+Deno.test("number-of-orders-in-the-backlog", () => {
+    assertEquals(
+        getNumberOfBacklogOrders([
+            [10, 5, 0],
+            [15, 2, 1],
+            [25, 1, 1],
+            [30, 4, 0],
+        ]),
+        6,
+    );
+});
+Deno.test("number-of-orders-in-the-backlog", () => {
+    assertEquals(
+        getNumberOfBacklogOrders([
+            [7, 1000000000, 1],
+            [15, 3, 0],
+            [5, 999999995, 0],
+            [5, 1, 1],
+        ]),
+        999999984,
+    );
+});
