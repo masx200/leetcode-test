ccb459aababe82a277f592574cc940a20b6b77e9
masx200 <masx200@qq.com>
2023-01-05 19:59:21 +0800

    测试

--- shuffle-string/index.ts
+++ shuffle-string/index.ts
@@ -1,6 +1,7 @@
 export default function restoreString(s: string, indices: number[]): string {
     //@ts-ignore
     return (
+        //@ts-ignore
         Array.prototype.reduce
             //@ts-ignore
             .call(s, (p, c, i) => ((p[indices[i]] = c), p), [...s])
---
+++ shuffle-string/test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "asserts";
+import restoreString from "./index.ts";
+
+Deno.test("shuffle-string", () => {
+    const s = "codeleet", indices = [4, 5, 6, 7, 0, 2, 1, 3], 输出 = "leetcode";
+    assertEquals(restoreString(s, indices), 输出);
+});
+
+Deno.test("shuffle-string", () => {
+    const s = "abc", indices = [0, 1, 2], 输出 = "abc";
+    assertEquals(restoreString(s, indices), 输出);
+});
