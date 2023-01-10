ec002f6f92354a2f34ec4665bca08d4c27cb2cfb
masx200 <masx200@qq.com>
2023-01-10 14:45:05 +0800

    Update index.ts

--- reconstruct-itinerary/index.ts
+++ reconstruct-itinerary/index.ts
@@ -1,24 +1,21 @@
 function findItinerary(tickets: string[][]): string[] {
-    const map: Map<string, string[]> = new Map(); // 图
+    const map: Map<string, string[]> = new Map();
     tickets.forEach(([from, to]) => {
         const array = map.get(from);
-        // 设置from和to的映射关系
+
         if (map.has(from) && array) array.push(to);
         else map.set(from, [to]);
     });
     for (const val of map.values()) {
-        // 按照字典序降序，tos中的最后一个字典序最小
         val.sort((a, b) => (a < b ? 1 : -1));
     }
-    // console.log(map)
+
     const start: string = "JFK";
     const res: string[] = [];
     function dfs(node: string) {
-        while (true) {
-            const array = map.get(node);
-
-            if (array?.length) dfs(array.pop() as string);
-            else break;
+        const array = map.get(node);
+        while (array?.length) {
+            dfs(array.pop() as string);
         }
 
         res.push(node);
