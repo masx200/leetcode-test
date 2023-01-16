92ad179445a7ea37013f96ed1549ff6948a7530b
masx200 <34191203+masx200@users.noreply.github.com>
2023-01-16 17:27:38 +0800

    Create index.ts

---
+++ count-nice-pairs-in-an-array/index.ts
@@ -0,0 +1,17 @@
+function countNicePairs(nums: number[]): number {
+
+   const MOD = 1000000007;
+    let res = 0;
+    const h = new Map<number,number>();
+    for (const i of nums) {
+        let temp = i, j = 0;
+        while (temp > 0) {
+            j = j * 10 + temp % 10;
+            temp = Math.floor(temp / 10);
+        }
+        res = (res + (h.get(i - j) || 0)) % MOD;
+        h.set(i - j, (h.get(i - j) || 0) + 1);
+    }
+    return res;
+};
+export default countNicePairs
