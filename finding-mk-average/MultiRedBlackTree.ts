2d487b6f25ca8d1bf6ec808cdee2b70a039617e0
masx200 <masx200@qq.com>
2023-01-19 16:02:21 +0800

    Update MultiRedBlackTree.ts

--- finding-mk-average/MultiRedBlackTree.ts
+++ finding-mk-average/MultiRedBlackTree.ts
@@ -1,3 +1,4 @@
+import { Direction } from "https://deno.land/std@0.173.0/collections/binary_search_node.ts";
 import RedBlackTreeExtended from "../dinner-plate-stacks/RedBlackTree.ts";
 import { MultiRedBlackNode } from "./MultiRedBlackNode.ts";
 
@@ -6,7 +7,65 @@ export class MultiRedBlackTree<T> extends RedBlackTreeExtended<T> {
     getRoot(): MultiRedBlackNode<T> | null {
         return this.root;
     }
-    remove(value: T): boolean {}
+    findNode(value: T): MultiRedBlackNode<T> | null {
+        return super.findNode(value) as MultiRedBlackNode<T> | null;
+    }
+    remove(value: T): boolean {
+        const node = this.findNode(value);
+
+        if (!node) {
+            return false;
+        } else {
+            node.count--;
+            if (node.count <= 0) this.removeTreeNode(node);
+            return true;
+        }
+    }
     insert(value: T): boolean {
+        const node = this.findNode(value);
+
+        if (node) {
+            node.count++;
+            return true;
+        } else {
+            this.insertGetNode(value);
+        }
+        return true;
+    }
+    insertGetNode(value: T): MultiRedBlackNode<T> | null {
+        let node = this.insertNode(
+            MultiRedBlackNode,
+            value,
+        ) as MultiRedBlackNode<T> | null;
+        if (node) {
+            while (node.parent?.red) {
+                let parent: MultiRedBlackNode<T> = node.parent!;
+                const parentDirection: Direction = parent
+                    .directionFromParent()!;
+                const uncleDirection: Direction = parentDirection === "right"
+                    ? "left"
+                    : "right";
+                const uncle: MultiRedBlackNode<T> | null =
+                    parent.parent![uncleDirection] ?? null;
+
+                if (uncle?.red) {
+                    parent.red = false;
+                    uncle.red = false;
+                    parent.parent!.red = true;
+                    node = parent.parent!;
+                } else {
+                    if (node === parent[uncleDirection]) {
+                        node = parent;
+                        this.rotateNode(node, parentDirection);
+                        parent = node.parent!;
+                    }
+                    parent.red = false;
+                    parent.parent!.red = true;
+                    this.rotateNode(parent.parent!, uncleDirection);
+                }
+            }
+            this.root!.red = false;
+        }
+        return node;
     }
 }
