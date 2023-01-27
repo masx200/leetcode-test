e1c9a217aa59e031c26935e640154d92a44312af
masx200 <masx200@qq.com>
2023-01-27 10:14:41 +0800

    Update index.ts

--- strong-password-checker-ii/index.ts
+++ strong-password-checker-ii/index.ts
@@ -2,7 +2,5 @@ function strongPasswordCheckerII(password: string): boolean {
     return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-+])(?!.*(.)\1+).{8,}$/
         .test(password);
 }
-// 执行用时：56 ms, 在所有 JavaScript 提交中击败了84.13% 的用户
-// 内存消耗：41 MB, 在所有 JavaScript 提交中击败了95.24% 的用户
 
 export default strongPasswordCheckerII;
