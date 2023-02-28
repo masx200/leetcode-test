80e9c8e73c107e2c92c2c4d6cf1dedc0f93c20d8
GitHub <noreply@github.com>
2023-02-28 04:57:48 +0000

    Update dependencies

--- basic-calculator-iv/test.ts
+++ basic-calculator-iv/test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 
 import basicCalculatorIV from "./index.ts";
 import { Poly } from "./Poly.ts";
--- dinner-plate-stacks/index.ts
+++ dinner-plate-stacks/index.ts
@@ -1,4 +1,4 @@
-import { ascend } from "https://deno.land/std@0.177.0/collections/binary_search_tree.ts";
+import { ascend } from "https://deno.land/std@0.178.0/collections/binary_search_tree.ts";
 import RedBlackTree from "./RedBlackTree.ts";
 import { reverseInOrderIterator } from "./reverseInOrderIterator.ts";
 
