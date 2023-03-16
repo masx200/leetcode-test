804a07a475a13a6a7121cd7ea4090d6503519cb0
GitHub <noreply@github.com>
2023-03-16 19:47:25 +0000

    Update dependencies

--- find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/findTheCity.ts
+++ find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/findTheCity.ts
@@ -1,4 +1,4 @@
-import { BinaryHeap } from "https://deno.land/std@0.179.0/collections/binary_heap.ts";
+import { BinaryHeap } from "https://deno.land/std@0.180.0/collections/binary_heap.ts";
 
 export default function findTheCity(
     n: number,
