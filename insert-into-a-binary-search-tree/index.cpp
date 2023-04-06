518e7790020e9d5cc050711bf29130b36274b2a8
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-06 19:48:38 +0800

    Create index.cpp

---
+++ insert-into-a-binary-search-tree/index.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == NULL) {
+            TreeNode* node = new TreeNode(val);
+            return node;
+        }
+        if (root->val > val) root->left = insertIntoBST(root->left, val);
+        if (root->val < val) root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
