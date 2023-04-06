0e6f306bceb1717dad8ac419892ee69e134aaab7
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-06 23:54:38 +0800

    Update test.cpp

--- insert-into-a-binary-search-tree/test.cpp
+++ insert-into-a-binary-search-tree/test.cpp
@@ -5,8 +5,15 @@
 #include <iostream>
 #include <stdio.h>
 #include "serializeTreeNode.cpp"
+
+// #include <stdexcept>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TextOutputter.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+// #include "ss"
 using namespace std;
-#include <stdexcept>
 void println(string s)
 {
     cout << s << endl;
@@ -41,19 +48,81 @@ void test2()
     println(serializeTreeNode(result));
     assertEquals(serializeTreeNode(result), "TreeNode{val:99,left:NULL,right:TreeNode{val:111,left:NULL,right:NULL}}");
 }
-int main(int argc, char const *argv[])
+
+// 定义测试类
+class StringTest : public CppUnit::TestFixture
 {
-    try
+    CPPUNIT_TEST_SUITE(StringTest); // 定义测试包
+    CPPUNIT_TEST(testSwap);         // 添加测试用例1
+    CPPUNIT_TEST(testFind);         // 添加测试用例2
+    CPPUNIT_TEST_SUITE_END();       // 结束测试包定义
+
+public:
+    void setUp() // 初始化
     {
-        test1();
-        test2();
-        // assertEquals("a", "b");
-    }
-    catch (string e)
-    {
-        cerr << e << endl;
-        exit(1);
+        // m_str1 =  "Hello, world";
+        // m_str2 =  "Hi, cppunit";
     }
 
-    return 0;
+    void tearDown() // 清理
+    {
+    }
+
+    void testSwap() // 测试方法1
+    {
+        test1();
+        //  std:: string str1 = m_str1;
+        //  std:: string str2 = m_str2;
+        // m_str1.swap(m_str2);
+
+        // CPPUNIT_ASSERT(m_str1 == str2);
+        // CPPUNIT_ASSERT(m_str2 == str1);
+    }
+
+    void testFind() // 测试方法2
+    {
+        test2();
+        //  int pos1 = m_str1.find(',');
+        //  int pos2 = m_str2.rfind(',');
+
+        // CPPUNIT_ASSERT_EQUAL(5, pos1);
+        // CPPUNIT_ASSERT_EQUAL(2, pos2);
+    }
+
+    // protected:
+    //      std:: string     m_str1;
+    //      std:: string     m_str2;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(StringTest); // 自动注册测试包
+// int main(int argc, char const *argv[])
+// {
+//     try
+//     {
+//         test1();
+//         test2();
+//         // assertEquals("a", "b");
+//     }
+//     catch (string e)
+//     {
+//         cerr << e << endl;
+//         exit(1);
+//     }
+
+//     return 0;
+// }
+int main(int argc, char *argv[])
+{
+    CppUnit::TestResult r;
+    CppUnit::TestResultCollector rc;
+    r.addListener(&rc); // 准备好结果收集器
+
+    CppUnit::TestRunner runner; // 定义执行实体
+    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+    runner.run(r); // 运行测试
+
+    CppUnit::TextOutputter o(&rc, std::cout);
+    o.write(); // 将结果输出
+
+    return rc.wasSuccessful() ? 0 : -1;
 }
