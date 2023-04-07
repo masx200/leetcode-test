6e3e8132149116a164f35e382329a37ff54e3d4f
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-07 14:26:51 +0800

    格式化

--- insert-into-a-binary-search-tree/LeetCodeTreeNodeToString.hpp
+++ insert-into-a-binary-search-tree/LeetCodeTreeNodeToString.hpp
@@ -12,12 +12,13 @@ using namespace std;
 #include <iterator>
 #include <regex>
 #include <sstream>
-std::string LeetCodeTreeNodeToString(TreeNode *root) {
-  stringstream sstream;
-  auto result = bfsTravelsal(root);
-  std::regex vowel_re("(,null){1,}\\]$");
+std::string LeetCodeTreeNodeToString(TreeNode* root)
+{
+    stringstream sstream;
+    auto result = bfsTravelsal(root);
+    std::regex vowel_re("(,null){1,}\\]$");
 
-  std::regex_replace(std::ostreambuf_iterator<char>(sstream), result.begin(),
-                     result.end(), vowel_re, "]");
-  return sstream.str();
+    std::regex_replace(std::ostreambuf_iterator<char>(sstream), result.begin(),
+        result.end(), vowel_re, "]");
+    return sstream.str();
 }
--- insert-into-a-binary-search-tree/test.cpp
+++ insert-into-a-binary-search-tree/test.cpp
@@ -20,111 +20,121 @@ using namespace std;
 #include "EqualTreeNode.hpp"
 #include "HashTreeNode.hpp"
 // #include "bfsTravelsal.hpp"
+#include "LeetCodeTreeNodeToString.hpp"
 #include "debugTreeNode.hpp"
 #include "printTreeNode.hpp"
 #include "treeparse.hpp"
-#include "LeetCodeTreeNodeToString.hpp"
 
-void println(string s) { cout << s << endl; }
-
-void assertEquals(string s1, string s2) {
-  CPPUNIT_ASSERT_EQUAL(s1, s2);
-  // if (s1 != s2)
-  // {
-  //     throw(("assertion error: " + s1 + " != " + s2));
-  // }
+void println(string s)
+{
+    cout << s << endl;
 }
 
-void test1() {
-  println("test1 start");
-  TreeNode *none = NULL;
-
-  auto result2 = Solution().insertIntoBST(none, 111);
-
-  println(serializeTreeNode(none));
-  assertEquals(serializeTreeNode(none), "null");
-  println(serializeTreeNode(result2));
-  assertEquals(serializeTreeNode(result2),
-               "TreeNode{val:111,left:null,right:null}");
-
-  auto nodes =
-      unordered_set<TreeNode *, HashTreeNode, EqualTreeNode>{none, result2};
-  for (auto node : nodes) {
-    printTreeNode(node);
-    freeTreeNode(node);
-  }
-  println("test1 end");
+void assertEquals(string s1, string s2)
+{
+    CPPUNIT_ASSERT_EQUAL(s1, s2);
+    // if (s1 != s2)
+    // {
+    //     throw(("assertion error: " + s1 + " != " + s2));
+    // }
 }
 
-void test2() {
-  println("test2 start");
-  auto tree = new TreeNode(99);
-  println(serializeTreeNode(tree));
-  assertEquals(serializeTreeNode(tree),
-               "TreeNode{val:99,left:null,right:null}");
-  auto result = Solution().insertIntoBST(tree, 111);
+void test1()
+{
+    println("test1 start");
+    TreeNode* none = NULL;
 
-  println(serializeTreeNode(result));
-  assertEquals(serializeTreeNode(result),
-               "TreeNode{val:99,left:null,right:TreeNode{val:111,left:null,"
-               "right:null}}");
+    auto result2 = Solution().insertIntoBST(none, 111);
 
-  auto nodes =
-      unordered_set<TreeNode *, HashTreeNode, EqualTreeNode>{tree, result};
-  for (auto node : nodes) {
-    printTreeNode(node);
-    freeTreeNode(node);
-  }
-  println("test2 end");
+    println(serializeTreeNode(none));
+    assertEquals(serializeTreeNode(none), "null");
+    println(serializeTreeNode(result2));
+    assertEquals(serializeTreeNode(result2),
+        "TreeNode{val:111,left:null,right:null}");
+
+    auto nodes = unordered_set<TreeNode*, HashTreeNode, EqualTreeNode> { none, result2 };
+    for (auto node : nodes) {
+        printTreeNode(node);
+        freeTreeNode(node);
+    }
+    println("test1 end");
+}
+
+void test2()
+{
+    println("test2 start");
+    auto tree = new TreeNode(99);
+    println(serializeTreeNode(tree));
+    assertEquals(serializeTreeNode(tree),
+        "TreeNode{val:99,left:null,right:null}");
+    auto result = Solution().insertIntoBST(tree, 111);
+
+    println(serializeTreeNode(result));
+    assertEquals(serializeTreeNode(result),
+        "TreeNode{val:99,left:null,right:TreeNode{val:111,left:null,"
+        "right:null}}");
+
+    auto nodes = unordered_set<TreeNode*, HashTreeNode, EqualTreeNode> { tree, result };
+    for (auto node : nodes) {
+        printTreeNode(node);
+        freeTreeNode(node);
+    }
+    println("test2 end");
 }
 
 class StringTest : public CppUnit::TestFixture {
-  CPPUNIT_TEST_SUITE(StringTest);
-  CPPUNIT_TEST(testSwap);
-  CPPUNIT_TEST(testFind);
+    CPPUNIT_TEST_SUITE(StringTest);
+    CPPUNIT_TEST(testSwap);
+    CPPUNIT_TEST(testFind);
 
-  CPPUNIT_TEST(test3);
-  CPPUNIT_TEST_SUITE_END();
+    CPPUNIT_TEST(test3);
+    CPPUNIT_TEST_SUITE_END();
 
 public:
-  void setUp() {}
+    void setUp() { }
 
-  void tearDown() {}
+    void tearDown() { }
 
-  void testSwap() { test1(); }
+    void testSwap() { test1(); }
 
-  void testFind() { test2(); }
+    void testFind() { test2(); }
 
-  void test3() {
-    println("test3 start");
-    auto rawString = string("[4,2,7,1,3]");
-    auto val = 5;
-    TreeNode *root = nullptr;
-    int status = parseLeetCodeBinaryTree(rawString, &root);
-    CPPUNIT_ASSERT_EQUAL(0, status);
+    void test3()
+    {
+        println("test3 start");
+        auto rawString = string("[4,2,7,1,3]");
+        auto val = 5;
+        TreeNode* root = nullptr;
+        int status = parseLeetCodeBinaryTree(rawString, &root);
+        CPPUNIT_ASSERT_EQUAL(0, status);
 
-    printTreeNode(root);
-    std::string result = LeetCodeTreeNodeToString(root);
-    CPPUNIT_ASSERT_EQUAL(result, rawString);
-    println(result);
-    cleanTreeNode(root);
-    println("test3 end");
-  }
+        printTreeNode(root);
+        std::string result = LeetCodeTreeNodeToString(root);
+        CPPUNIT_ASSERT_EQUAL(result, rawString);
+        println(result);
+        println(serializeTreeNode(root));
+        auto serialized = string("TreeNode{val:4,left:TreeNode{val:2,left:TreeNode{val:1,left:null,right:null},right:TreeNode{val:3,left:null,right:null}},right:TreeNode{val:7,left:null,right:null}}");
+        CPPUNIT_ASSERT_EQUAL(serializeTreeNode(root),
+            serialized);
+        cleanTreeNode(root);
+        println("test3 end");
+    }
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(StringTest);
 
-int main(int argc, char *argv[]) {
-  CppUnit::TestResult r;
-  CppUnit::TestResultCollector rc;
-  r.addListener(&rc);
+int main(int argc, char* argv[])
+{
+    CppUnit::TestResult r;
+    CppUnit::TestResultCollector rc;
+    r.addListener(&rc);
 
-  CppUnit::TestRunner runner;
-  runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
-  runner.run(r);
+    CppUnit::TestRunner runner;
+    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+    runner.run(r);
 
-  CppUnit::TextOutputter o(&rc, std::cout);
-  o.write();
+    CppUnit::TextOutputter o(&rc, std::cout);
+    o.write();
 
-  return rc.wasSuccessful() ? 0 : -1;
+    return rc.wasSuccessful() ? 0 : -1;
 }
