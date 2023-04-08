925183b83ce74c004ad1c64265096ff93471c60e
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-08 17:26:47 +0800

    测试

--- make.sh
+++ make.sh
@@ -1,1 +1,4 @@
 g++ ./insert-into-a-binary-search-tree/test.cpp -l cppunit -o ./insert-into-a-binary-search-tree/test.bin -I ./insert-into-a-binary-search-tree -v
+
+
+g++ ./check-if-it-is-a-good-array/test.cpp -l gtest -o ./check-if-it-is-a-good-array/test.bin -I ./check-if-it-is-a-good-array -v -l pthread
