256ecb1d14497601cf938f57e957207f4483de26
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-08 18:27:25 +0800

    测试

--- make.sh
+++ make.sh
@@ -2,3 +2,5 @@ g++ ./insert-into-a-binary-search-tree/test.cpp -l cppunit -o ./insert-into-a-bi
 
 
 g++ ./check-if-it-is-a-good-array/test.cpp -l gtest -o ./check-if-it-is-a-good-array/test.bin -I ./check-if-it-is-a-good-array -v -l pthread
+
+g++ ./insufficient-nodes-in-root-to-leaf-paths/test.cpp -l gtest -o ./insufficient-nodes-in-root-to-leaf-paths/test.bin -I ./insufficient-nodes-in-root-to-leaf-paths -v -l pthread
--- test.sh
+++ test.sh
@@ -3,3 +3,6 @@ chmod +x ./insert-into-a-binary-search-tree/test.bin
 
 chmod +x ./check-if-it-is-a-good-array/test.bin
 ./check-if-it-is-a-good-array/test.bin
+
+chmod +x ./insufficient-nodes-in-root-to-leaf-paths/test.bin
+./insufficient-nodes-in-root-to-leaf-paths/test.bin
