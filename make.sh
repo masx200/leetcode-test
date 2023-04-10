253008528c736bb96bbdfaa47fb1cf6b59848057
masx200 <masx200@qq.com>
2023-04-10 11:11:05 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 g++  ~/cppmodules/leetcode-treenode-cpp-1.1.1/*.cxx ./insert-into-a-binary-search-tree/test.cpp -fmodules-ts  -l cppunit  -I ./insert-into-a-binary-search-tree -v -c
 
-g++ ./insert-into-a-binary-search-tree/test.cpp ~/cppmodules/leetcode-treenode-cpp-1.1.1/*.cxx -fmodules-ts -l cppunit -o ./insert-into-a-binary-search-tree/test.bin -I ./insert-into-a-binary-search-tree -v
+g++ ~/cppmodules/leetcode-treenode-cpp-1.1.1/*.cxx ./insert-into-a-binary-search-tree/test.cpp -fmodules-ts -l cppunit -o ./insert-into-a-binary-search-tree/test.bin -I ./insert-into-a-binary-search-tree -v
 
 
 g++ ./check-if-it-is-a-good-array/test.cpp -l gtest -o ./check-if-it-is-a-good-array/test.bin -I ./check-if-it-is-a-good-array -v -l pthread
