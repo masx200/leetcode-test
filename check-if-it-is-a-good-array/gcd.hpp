d51828d5323a4511bd9b15ab43883104ab23933a
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-10 14:16:27 +0800

    Update gcd.hpp

--- check-if-it-is-a-good-array/gcd.hpp
+++ check-if-it-is-a-good-array/gcd.hpp
@@ -1,18 +1,19 @@
 // +build ignore
 
 #pragma once
-#include <vector>
+// #include <vector>
 
-using namespace std;
-#include <algorithm>
-#include <numeric>
+// using namespace std;
+// #include <algorithm>
+// #include <numeric>
 
-
-int gcd(int num1, int num2) {
-  while (num2 != 0) {
-    int temp = num1;
-    num1 = num2;
-    num2 = temp % num2;
-  }
-  return num1;
+int gcd(int num1, int num2)
+{
+    while (num2 != 0)
+    {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp % num2;
+    }
+    return num1;
 };
