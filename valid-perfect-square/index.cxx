8c22a7c2382f372f5fa706d4a08e592a0c9bde6a
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-10 13:54:59 +0800

    valid_perfect_square

---
+++ valid-perfect-square/index.cxx
@@ -0,0 +1,35 @@
+module;
+#include <cmath>
+
+export module valid_perfect_square;
+namespace valid_perfect_square
+{
+
+    class Solution
+    {
+
+    private:
+        auto sqrt(int num)
+        {
+            auto x0 = double(num);
+            auto x1 = (10 * double(num) + 10);
+            while (true)
+            {
+                x1 = -((x0)-num / x0) / (2) + x0;
+                if (abs(x0 - x1) < 1e-14)
+                    break;
+                x0 = x1;
+            }
+            return x0;
+        }
+
+    public:
+        bool isPerfectSquare(int num)
+        {
+
+            auto x = floor(sqrt(num));
+            return x * x == num;
+        }
+    };
+    export Solution;
+}
