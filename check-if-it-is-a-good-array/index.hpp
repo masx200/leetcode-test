9b8260377934af898eceb35b0cd7e37b8fe5baee
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-10 15:26:16 +0800

    Update index.hpp

--- check-if-it-is-a-good-array/index.hpp
+++ check-if-it-is-a-good-array/index.hpp
@@ -16,10 +16,8 @@ namespace check_if_it_is_a_good_array
     public:
         bool isGoodArray(vector<int> &nums)
         {
-            // auto a = std::gcd(15, 20);
-            int divisor = accumulate<std::vector<int>::iterator, int, int(int, int)>(
-                nums.begin(), nums.end(), nums[0], gcd);
-            return divisor == 1;
+            return 1 == accumulate<std::vector<int>::iterator, int, int(int, int)>(nums.begin(), nums.end(), nums[0], gcd);
         }
     };
+
 }
