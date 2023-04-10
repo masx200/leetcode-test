49dd49fa7e186079f33a28c0e5072e1b0f88732f
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-10 15:15:54 +0800

    namespace insert_into_a_binary_search_tree

--- insert-into-a-binary-search-tree/index.hpp
+++ insert-into-a-binary-search-tree/index.hpp
@@ -1,26 +1,29 @@
 // +build ignore
 
 #pragma once
-// #ifndef _insert_into_a_binary_search_tree_SOLUTION_
-// #define _insert_into_a_binary_search_tree_SOLUTION_
 
-// #include "TreeNode.hpp"
 #include <stdio.h>
 import leetcode_treenode_cpp.TreeNode;
 using namespace leetcode_treenode_cpp;
-class Solution {
-public:
-    TreeNode* insertIntoBST(TreeNode* root, int val)
+
+namespace insert_into_a_binary_search_tree
+{
+    class Solution
     {
-        if (root == NULL) {
-            TreeNode* node = new TreeNode(val);
-            return node;
+    public:
+        TreeNode *insertIntoBST(TreeNode *root, int val)
+        {
+            if (root == NULL)
+            {
+                TreeNode *node = new TreeNode(val);
+                return node;
+            }
+            if (root->val > val)
+                root->left = insertIntoBST(root->left, val);
+            if (root->val < val)
+                root->right = insertIntoBST(root->right, val);
+            return root;
         }
-        if (root->val > val)
-            root->left = insertIntoBST(root->left, val);
-        if (root->val < val)
-            root->right = insertIntoBST(root->right, val);
-        return root;
-    }
-};
-// #endif //
+    };
+
+}
