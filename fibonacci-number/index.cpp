808f866a9fe3efd7296548116484bde87034f8a2
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-14 12:46:21 +0800

    Create index.cpp

---
+++ fibonacci-number/index.cpp
@@ -0,0 +1,49 @@
+
+
+namespace fibonacci_number{
+
+class Solution {
+
+    
+
+    
+
+    
+
+    
+
+    
+
+ // static unordered_map<int,int> cache;
+
+    
+
+    
+
+public:
+
+    int fib(int n) {
+
+        
+static   unordered_map<int,int> cache;
+        
+
+        if(cache.count(n))return cache.at(n);
+
+        
+
+        
+
+auto res= n==0?0:n == 1 || n == 2 ? 1 : fib(n-1) + fib(n-2);
+
+        cache[n]=res;
+
+        return res;
+
+    }
+
+    
+
+    
+
+};}
