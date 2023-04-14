b597a89b5881ae3d0cd255bfc42565ad79f9d15e
GitHub <noreply@github.com>
2023-04-14 19:47:38 +0000

    Update dependencies

--- camelcase-matching/index.ts
+++ camelcase-matching/index.ts
@@ -1,9 +1,9 @@
 function camelMatch(queries: string[], pattern: string): boolean[] {
+    const reg = new RegExp(
+        `^[a-z]*${pattern.split("").map((v) => v + "[a-z]*").join("")}$`,
+    );
 
-    const reg=new RegExp(`^[a-z]*${pattern.split('').map(v=>v+'[a-z]*').join('')}$`)
-
-    return queries.map(v=>reg.test(v))
-
+    return queries.map((v) => reg.test(v));
 }
 
-export  default camelMatch
+export default camelMatch;
