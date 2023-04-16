0900c262446b300a22a71850ef3bd44a38f7b3a9
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-16 17:10:26 +0800

    格式化

--- fibonacci-number/index.cpp
+++ fibonacci-number/index.cpp
@@ -1,50 +1,27 @@
 
-#include<unordered_map>
+#include <unordered_map>
 
-namespace fibonacci_number{
+namespace fibonacci_number {
 using std::unordered_map;
 class Solution {
 
-    
-
-    
-
-    
-
-    
-
-    
-
- // static unordered_map<int,int> cache;
-
-    
-
-    
+    // static unordered_map<int,int> cache;
 
 public:
+    int fib(int n)
+    {
 
-    int fib(int n) {
+        static unordered_map<int, int> cache;
 
-        
-static   unordered_map<int,int> cache;
-        
+        if (cache.count(n))
+            return cache.at(n);
 
-        if(cache.count(n))return cache.at(n);
+        auto res = n == 0 ? 0 : n == 1 || n == 2 ? 1
+                                                 : fib(n - 1) + fib(n - 2);
 
-        
-
-        
-
-auto res= n==0?0:n == 1 || n == 2 ? 1 : fib(n-1) + fib(n-2);
-
-        cache[n]=res;
+        cache[n] = res;
 
         return res;
-
     }
-
-    
-
-    
-
-};}
+};
+}
