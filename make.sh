b6fcf56eac5f0216d6870bddfbfe606827433a57
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-18 10:58:51 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,8 +2,9 @@ g++  ~/cppmodules/leetcode-treenode-cpp-1.1.2/*.cxx ./insert-into-a-binary-searc
 
 g++ ~/cppmodules/leetcode-treenode-cpp-1.1.2/*.cxx ./insert-into-a-binary-search-tree/test.cpp -fmodules-ts -l cppunit -o ./insert-into-a-binary-search-tree/test.bin -I ./insert-into-a-binary-search-tree -v
 
+g++ ./check-if-it-is-a-good-array/*.ixx ./check-if-it-is-a-good-array/test.cpp -l gtest -c -I ./check-if-it-is-a-good-array -v -l pthread -fmodules-ts
 
-g++ ./check-if-it-is-a-good-array/test.cpp -l gtest -o ./check-if-it-is-a-good-array/test.bin -I ./check-if-it-is-a-good-array -v -l pthread
+g++ ./check-if-it-is-a-good-array/*.ixx ./check-if-it-is-a-good-array/test.cpp -l gtest -o ./check-if-it-is-a-good-array/test.bin -I ./check-if-it-is-a-good-array -v -l pthread -fmodules-ts
 
 g++ ./insufficient-nodes-in-root-to-leaf-paths/test.cpp ~/cppmodules/leetcode-treenode-cpp-1.1.2/*.cxx -fmodules-ts -l gtest  -I ./insufficient-nodes-in-root-to-leaf-paths -v -l pthread -c
 
