4010a3fa476a5ba323d0e17a80250ea31eef0ccd
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-18 11:04:33 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 chmod +x ./insert-into-a-binary-search-tree/test.bin
 ./insert-into-a-binary-search-tree/test.bin
 
-chmod +x ./check-if-it-is-a-good-array/test.bin
-./check-if-it-is-a-good-array/test.bin
+#chmod +x ./check-if-it-is-a-good-array/test.bin
+#./check-if-it-is-a-good-array/test.bin
 
 chmod +x ./insufficient-nodes-in-root-to-leaf-paths/test.bin
 ./insufficient-nodes-in-root-to-leaf-paths/test.bin
