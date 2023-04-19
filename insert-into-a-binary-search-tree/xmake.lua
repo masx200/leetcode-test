5231f79ab4e9f7c48c6cc007c507405b54fa0ad2
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-19 20:00:18 +0800

    Update xmake.lua

--- insert-into-a-binary-search-tree/xmake.lua
+++ insert-into-a-binary-search-tree/xmake.lua
@@ -1,7 +1,7 @@
 
-add_repositories("repo2 https://gitee.com/masx200/xmake-repo.git")
+add_repositories("masx200/xmake-repo https://gitee.com/masx200/xmake-repo.git")
 
-add_repositories("repo1 https://ghproxy.com/https://github.com/masx200/xmake-repo.git")
+add_repositories("masx200/xmake-repo https://ghproxy.com/https://github.com/masx200/xmake-repo.git")
 
 add_rules("mode.debug", "mode.release")
 add_requires("leetcode-treenode-cpp")
