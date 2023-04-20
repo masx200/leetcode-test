6ea07a1b6c8e7f08b5a6ccfbca86460797a0cdfb
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-20 10:46:59 +0800

    export module fibonacci_number.Solution;

--- fibonacci-number/index.cpp
+++ fibonacci-number/index.cpp
@@ -1,27 +1,29 @@
+module;
 
 #include <unordered_map>
 
+export module fibonacci_number.Solution;
 namespace fibonacci_number {
-using std::unordered_map;
-class Solution {
+    using std::unordered_map;
+    export
+    class Solution {
 
-    // static unordered_map<int,int> cache;
+        // static unordered_map<int,int> cache;
 
-public:
-    int fib(int n)
-    {
+    public:
+        int fib(int n) {
 
-        static unordered_map<int, int> cache;
+            static unordered_map<int, int> cache;
 
-        if (cache.count(n))
-            return cache.at(n);
+            if (cache.count(n))
+                return cache.at(n);
 
-        auto res = n == 0 ? 0 : n == 1 || n == 2 ? 1
-                                                 : fib(n - 1) + fib(n - 2);
+            auto res = n == 0 ? 0 : n == 1 || n == 2 ? 1
+                                                     : fib(n - 1) + fib(n - 2);
 
-        cache[n] = res;
+            cache[n] = res;
 
-        return res;
-    }
-};
+            return res;
+        }
+    };
 }
