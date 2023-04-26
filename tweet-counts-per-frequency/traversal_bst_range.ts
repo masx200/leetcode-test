5997b0ccc5481005187296b9089f745604305caf
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-26 20:22:12 +0800

    Update traversal_bst_range.ts

--- tweet-counts-per-frequency/traversal_bst_range.ts
+++ tweet-counts-per-frequency/traversal_bst_range.ts
@@ -9,12 +9,15 @@ export function traversal_bst_range<K extends string | number = number>(
     if (!node) return;
 
     const value = node.getValue();
+    //@ts-ignore
     if (value <= high && value >= low) {
         callback(value);
     }
+     //@ts-ignore
     if (value > low) {
         traversal_bst_range(node.getLeft(), low, high, callback);
     }
+     //@ts-ignore
     if (value < high) {
         traversal_bst_range(node.getRight(), low, high, callback);
     }
