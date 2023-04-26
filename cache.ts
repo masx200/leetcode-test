2f12bef21f24ddeabb7dc52358acbca118e7b85f
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-26 16:30:35 +0800

    - run: deno task cache

---
+++ cache.ts
@@ -0,0 +1,82 @@
+import {
+    AsyncCurrentLimiter,
+    AsyncLimiterClass,
+    parse,
+    walk,
+    WalkEntry,
+} from "./deps.ts";
+
+import { split_by_count } from "./utils/split_by_count.ts";
+
+function searchFilesNames({
+    skip,
+}: {
+    skip?: RegExp | RegExp[];
+}) {
+    const entry_iter = walk(".", {
+        includeFiles: true,
+        includeDirs: false,
+        exts: ["ts"],
+        skip: [/node_modules/, skip].flat().filter(Boolean) as RegExp[],
+    });
+    return entry_iter;
+}
+if (import.meta.main) {
+    await start();
+}
+
+async function parallel_cache(
+    entry_iter: AsyncIterableIterator<WalkEntry>,
+    limiter: AsyncCurrentLimiter,
+) {
+    const files: string[] = [];
+
+    for await (const entry of entry_iter) {
+        files.push(entry.path);
+    }
+
+    const entries = split_by_count(files, 50);
+
+    await Promise.all(
+        entries.map((stack) =>
+            limiter.run(function (stack: string[]) {
+                return runDenocache(stack);
+            }.bind(null, stack))
+        ),
+    );
+}
+async function start() {
+    const limiter = new AsyncLimiterClass(1);
+    const args = parse(Deno.args);
+    console.log(args);
+    const skip = typeof args.skip === "string"
+        ? new RegExp(String(args.skip))
+        : Array.isArray(args.skip)
+        ? args.skip.map((s: string | RegExp) => new RegExp(s))
+        : undefined;
+    const entry_iter = searchFilesNames({ skip });
+    await parallel_cache(entry_iter, limiter);
+}
+
+async function runDenocache(stack: string[]) {
+    const cmd = ["deno", "cache", "--unstable", ...stack];
+    console.log(cmd);
+    const process = Deno.run({
+        cmd: cmd,
+        stderr: "piped",
+        stdout: "piped",
+    });
+    const [status, stdout, stderr] = await Promise.all([
+        process.status(),
+        process.output(),
+        process.stderrOutput(),
+    ]);
+    const decoder = new TextDecoder();
+    const out = decoder.decode(stdout);
+    const err = decoder.decode(stderr);
+    console.log(status, out, err);
+    process.close();
+    if (!status.success) {
+        throw new Error("type cache failed:" + out + err);
+    }
+}
