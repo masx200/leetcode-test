ba64d83d9748653e73d3a86e1429a27bafe20632
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-27 23:35:53 +0800

    Update Solution.kt

--- reverse-nodes-in-even-length-groups/src/main/kotlin/Solution.kt
+++ reverse-nodes-in-even-length-groups/src/main/kotlin/Solution.kt
@@ -3,38 +3,52 @@ package com.github.masx200.leetcode_test.reverse_nodes_in_even_length_groups
 class Solution {
     fun reverseEvenLengthGroups(head: ListNode?): ListNode? {
         if (null == head) return null
-        var cur = head
-        val nodes = mutableListOf<ListNode>()
-
-        var group = 1
-        while (null != cur) {
-            if (nodes.size == group) {
-// println("group=$group")
-//  println(nodes.map{it.`val`})
-                if ((group and 1) == 0) {
-
-                    nodes.map { it.`val` }.reversed().forEachIndexed { i, v -> nodes[i].`val` = v }
-                }
-
-                nodes.clear()
-                group++
-
-                nodes.add(cur)
-            } else
-                if (nodes.size < group) {
-
-                    nodes.add(cur)
-                }
-            cur = cur.next
-
-
+        for (nodes in generateEvenGroups(head)) {
+            reverseListNodes(nodes)
         }
-        if (nodes.size > 0) {
-            // println("group=$group")
-            //  println(nodes.map{it.`val`})
-            if ((nodes.size and 1) == 0) nodes.map { it.`val` }.reversed().forEachIndexed { i, v -> nodes[i].`val` = v }
-        }
-
         return head
+    }
+
+    private fun reverseListNodes(nodes: MutableList<ListNode>) {
+        nodes.map { it.`val` }.reversed().forEachIndexed { i, v -> nodes[i].`val` = v }
+    }
+
+    private fun generateEvenGroups(head: ListNode?): Sequence<MutableList<ListNode>> {
+
+        return sequence {
+            if (null == head) return@sequence
+            var cur = head
+            val nodes = mutableListOf<ListNode>()
+
+            var group = 1
+            while (null != cur) {
+                if (nodes.size == group) {
+
+                    if ((group and 1) == 0) {
+
+                        yield(nodes)
+                    }
+
+                    nodes.clear()
+                    group++
+
+
+                }
+                nodes.add(cur)
+
+                cur = cur.next
+            }
+
+            if (nodes.size > 0) {
+
+                if ((nodes.size and 1) == 0) {
+
+                    yield(nodes)
+                }
+            }
+
+            return@sequence
+        }
+
     }
 }
