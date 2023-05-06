d3159d6efaabd80a39df3652d8d0ed3fb750cb32
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-06 22:10:00 +0800

    package construct_quad_tree  func construct(grid [][]int) *Node {

---
+++ construct-quad-tree/Node.go
@@ -0,0 +1,10 @@
+package construct_quad_tree
+
+type Node struct {
+	Val         bool
+	IsLeaf      bool
+	TopLeft     *Node
+	TopRight    *Node
+	BottomLeft  *Node
+	BottomRight *Node
+}
---
+++ construct-quad-tree/export.go
@@ -0,0 +1,5 @@
+package construct_quad_tree
+
+func Construct(grid [][]int) *Node {
+	return construct(grid)
+}
