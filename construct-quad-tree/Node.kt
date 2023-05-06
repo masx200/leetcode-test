465360afef096df33c30f6c7c6702f8c7c64ca2c
GitHub <noreply@github.com>
2023-05-06 15:55:57 +0000

    Update dependencies

--- construct-quad-tree/Node.kt
+++ construct-quad-tree/Node.kt
@@ -1,9 +1,10 @@
 package com.github.masx200.leetcode_test.construct_quad_tree
 
-
 class Node(
-    var `val`: Boolean, var isLeaf: Boolean, var topLeft: Node? = null,
+    var `val`: Boolean,
+    var isLeaf: Boolean,
+    var topLeft: Node? = null,
     var topRight: Node? = null,
     var bottomLeft: Node? = null,
-    var bottomRight: Node? = null
+    var bottomRight: Node? = null,
 )
