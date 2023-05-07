42c50387adf08dccbe9c7eb4ce74b964a728527d
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-07 16:35:48 +0800

    Update xmake.ts

--- xmake.ts
+++ xmake.ts
@@ -5,8 +5,7 @@ import { join, resolve } from "https://deno.land/std@0.186.0/path/mod.ts";
 import { assertEquals } from "asserts";
 import { dirname } from "https://deno.land/x/dirname_es@v1.0.1/mod.ts";
 import parse from "npm:@masx200/mini-cli-args-parser@1.0.5";
-
-// import { writeAll } from "https://deno.land/std@0.186.0/streams/mod.ts";
+import { retry } from "https://deno.land/std@0.186.0/async/retry.ts";
 
 async function* findFilesRecursive(
     path: string,
@@ -32,11 +31,17 @@ async function main() {
     console.log(JSON.stringify(args));
     const { sdk, toolchain } = args;
     for await (const file of findFilesRecursive(__dirname, "test.cpp")) {
-        await xmake(file, toolchain, sdk);
+        await RunXmake(file, toolchain, sdk);
     }
 }
 
-async function xmake(file: string, toolchain: string, sdk: string) {
+async function RunXmake(file: string, toolchain: string, sdk: string) {
+    await RunXmakeConfig(file, toolchain, sdk);
+    await retry(RunXmakeBuild.bind(null, file));
+    await RunXmakeTest(file);
+}
+
+async function RunXmakeConfig(file: string, toolchain: string, sdk: string) {
     const os = Deno.build.os;
     console.log({ file });
     // console.log({ os });
@@ -46,9 +51,67 @@ async function xmake(file: string, toolchain: string, sdk: string) {
         `  xmake f ${toolchain ? "--toolchain=" + toolchain : ""} ${
             sdk ? "--sdk=" + sdk : ""
         } -y -v `,
+    ];
+    const args = os === "windows"
+        ? [
+            "-command",
+            others.join(" \n "),
+        ]
+        : [
+            "-c",
+            others.join(" && "),
+        ];
+    const cwd = path.dirname(file);
+    console.log(JSON.stringify({ cmd, cwd, args }));
+    const command = new Deno.Command(cmd, { cwd: cwd, args });
+
+    const { success, stderr, stdout, code } = await command.output();
+    console.log(new TextDecoder().decode(stdout));
+    console.error(new TextDecoder().decode(stderr));
+    console.log({ success, code });
+    // await writeAll(Deno.stdout, stdout);
+    //await writeAll(Deno.stderr, stderr);
+    assertEquals(success, true);
+    assertEquals(code, 0);
+}
+
+async function RunXmakeBuild(file: string) {
+    const os = Deno.build.os;
+    console.log({ file });
+    // console.log({ os });
+    const cmd = os === "windows" ? "powershell.exe" : "bash";
+    const others = [
         ` xmake build -v -y  -w test`,
-        ` xmake build -v -y  -w`,
-        ` xmake build -v -y  -w`,
+    ];
+    const args = os === "windows"
+        ? [
+            "-command",
+            others.join(" \n "),
+        ]
+        : [
+            "-c",
+            others.join(" && "),
+        ];
+    const cwd = path.dirname(file);
+    console.log(JSON.stringify({ cmd, cwd, args }));
+    const command = new Deno.Command(cmd, { cwd: cwd, args });
+
+    const { success, stderr, stdout, code } = await command.output();
+    console.log(new TextDecoder().decode(stdout));
+    console.error(new TextDecoder().decode(stderr));
+    console.log({ success, code });
+    // await writeAll(Deno.stdout, stdout);
+    //await writeAll(Deno.stderr, stderr);
+    assertEquals(success, true);
+    assertEquals(code, 0);
+}
+
+async function RunXmakeTest(file: string) {
+    const os = Deno.build.os;
+    console.log({ file });
+    // console.log({ os });
+    const cmd = os === "windows" ? "powershell.exe" : "bash";
+    const others = [
         `  xmake run -v test`,
     ];
     const args = os === "windows"
@@ -70,7 +133,6 @@ async function xmake(file: string, toolchain: string, sdk: string) {
     console.log({ success, code });
     // await writeAll(Deno.stdout, stdout);
     //await writeAll(Deno.stderr, stderr);
-
     assertEquals(success, true);
     assertEquals(code, 0);
 }
