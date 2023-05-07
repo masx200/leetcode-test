04773254a973530384839f633306b1c03c0d3133
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-07 13:52:24 +0800

    powershell

--- xmake.ts
+++ xmake.ts
@@ -5,7 +5,8 @@ import { join, resolve } from "https://deno.land/std@0.186.0/path/mod.ts";
 import { assertEquals } from "asserts";
 import { dirname } from "https://deno.land/x/dirname_es@v1.0.1/mod.ts";
 import parse from "npm:@masx200/mini-cli-args-parser@1.0.5";
-import { writeAll } from "https://deno.land/std@0.186.0/streams/mod.ts";
+
+// import { writeAll } from "https://deno.land/std@0.186.0/streams/mod.ts";
 
 async function* findFilesRecursive(
     path: string,
@@ -39,31 +40,37 @@ async function xmake(file: string, toolchain: string, sdk: string) {
     const os = Deno.build.os;
     console.log({ file });
     // console.log({ os });
-    const cmd = os === "windows" ? "cmd.exe" : "bash";
-    const others = `xmake clean &&  xmake f ${
-        toolchain ? "--toolchain=" + toolchain : ""
-    } ${
-        sdk
-            ? "--sdk=" + sdk
-            : ""
-    } -y -v && xmake build -v -y &&  xmake run -v test`;
+    const cmd = os === "windows" ? "powershell.exe" : "bash";
+    const others = [
+        `xmake clean `,
+        `  xmake f ${toolchain ? "--toolchain=" + toolchain : ""} ${
+            sdk ? "--sdk=" + sdk : ""
+        } -y -v `,
+        ` xmake build -v -y  -w`,
+        ` xmake build -v -y  -w`,
+        ` xmake build -v -y  -w`,
+        `  xmake run -v test`,
+    ];
     const args = os === "windows"
         ? [
-            "/c",
-            others,
+            "-command",
+            others.join(" \n "),
         ]
         : [
             "-c",
-            others,
+            others.join(" && "),
         ];
     const cwd = path.dirname(file);
-    console.log({ cmd, cwd, args });
+    console.log(JSON.stringify({ cmd, cwd, args }));
     const command = new Deno.Command(cmd, { cwd: cwd, args });
 
     const { success, stderr, stdout, code } = await command.output();
+    console.log(new TextDecoder().decode(stdout));
+    console.error(new TextDecoder().decode(stderr));
     console.log({ success, code });
-    await writeAll(Deno.stdout, stdout);
-    await writeAll(Deno.stderr, stderr);
+    // await writeAll(Deno.stdout, stdout);
+    //await writeAll(Deno.stderr, stderr);
+
     assertEquals(success, true);
     assertEquals(code, 0);
 }
