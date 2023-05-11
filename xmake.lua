d7e4cb65e4bd29655e90e4e7a1453743dd47541e
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-11 22:53:51 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ set_languages("c17", "cxx20");
 add_repositories("masx200/xmake-repo https://gitee.com/masx200/xmake-repo.git");
 add_repositories(
     "masx200/xmake-repo https://ghproxy.com/https://github.com/masx200/xmake-repo.git");
-add_requires("leetcode-treenode-cpp 1.1.6");
+add_requires("leetcode-treenode-cpp 1.1.7");
 target('leetcode-test')
 set_languages("c++20");
 set_kind('static')
