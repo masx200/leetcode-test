392deff281d55655d13d89a14e4a505507119594
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-14 20:52:40 +0800

    Update xmake.lua

--- insert-into-a-binary-search-tree/xmake.lua
+++ insert-into-a-binary-search-tree/xmake.lua
@@ -1,3 +1,4 @@
+if is_mode("test") then add_requires("vcpkg::eventpp") end
 set_policy("platform.longpaths", true)
 add_repositories("masx200/xmake-repo https://gitee.com/masx200/xmake-repo.git");
 add_repositories(
@@ -7,6 +8,13 @@ add_requires("leetcode-treenode-cpp 1.1.7");
 add_requires("vcpkg::cppunit");
 set_languages("c17", "cxx20");
 target("insert-into-a-binary-search-tree-test");
+-- 如果当前编译模式是test
+if is_mode("test") then
+
+    -- 添加test编译宏
+    add_defines("__TEST__")
+    add_packages("vcpkg::eventpp")
+end
 set_group("test")
 set_default(false)
 set_kind("binary");
