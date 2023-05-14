ba572bfd9e35c16d33e6fca13d8e70cd01047eae
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-14 19:07:05 +0800

    std::ranges::views::transform

--- split-a-circular-linked-list/test.cpp
+++ split-a-circular-linked-list/test.cpp
@@ -1,9 +1,4 @@
-import leetcode_test.split_a_circular_linked_list.ListNode;
-#include <ranges>
-import leetcode_test.split_a_circular_linked_list.Solution;
-import leetcode_test.split_a_circular_linked_list.ArrayToCircularLinkedList;
-import leetcode_test.split_a_circular_linked_list.CircularLinkedListToArray;
-import leetcode_test.split_a_circular_linked_list.TraversalCircularListNode;
+
 #include <gtest/gtest.h>
 #include <iterator>
 #include <unordered_set>
@@ -11,35 +6,39 @@ import leetcode_test.split_a_circular_linked_list.TraversalCircularListNode;
 #ifdef __TEST__
 #include <eventpp/callbacklist.h>
 #endif
+#include <ranges>
+import leetcode_test.split_a_circular_linked_list.ListNode;
+
+import leetcode_test.split_a_circular_linked_list.Solution;
+import leetcode_test.split_a_circular_linked_list.ArrayToCircularLinkedList;
+import leetcode_test.split_a_circular_linked_list.CircularLinkedListToArray;
+import leetcode_test.split_a_circular_linked_list.TraversalCircularListNode;
 using namespace leetcode_test::split_a_circular_linked_list;
 using namespace std;
 using std::vector;
 template <class T>
-concept sizable = requires(T& t)
-{
+concept sizable = requires(T& t) {
     {
         t.size()
-        } -> std::same_as<size_t>;
+    } -> std::same_as<size_t>;
 };
 template <class T>
-concept iterable = requires(T& t)
-{
+concept iterable = requires(T& t) {
     ++t.begin();
     {
         t.begin() != t.end()
 
-        } -> std::same_as<bool>;
+    } -> std::same_as<bool>;
 };
 
 template <class T, typename Y>
-concept equalable = requires(T& t, Y& y, size_t i)
-{
+concept equalable = requires(T& t, Y& y, size_t i) {
     {
         *t.begin() == *y.begin()
-        } -> std::same_as<bool>;
+    } -> std::same_as<bool>;
 };
 template <typename T, typename Y>
-requires sizable<T> and sizable<Y> and equalable<T, Y> and iterable<T> and iterable<Y>
+    requires sizable<T> and sizable<Y> and equalable<T, Y> and iterable<T> and iterable<Y>
 auto assertContentEquals(T& left, Y& right)
 {
 
@@ -84,14 +83,14 @@ TEST(split_a_circular_linked_list, test1)
 #ifdef __TEST__
     ListNodeInspector inspector;
 #endif
-    using std::ranges::views::transform;
+    // using std::ranges::views::transform;
     auto input = vector<int> { 1, 5, 7 };
     auto output = vector<vector<int>> { { 1, 5 }, { 7 } };
     auto* list = ArrayToCircularLinkedList(input);
 
     auto nodes = unordered_set<ListNode*> {};
     TraversalCircularListNode(list, [&](auto* node) { nodes.insert(node); });
-    auto result = Solution().splitCircularLinkedList(list) | transform(CircularLinkedListToArray);
+    auto result = Solution().splitCircularLinkedList(list) | std::ranges::views::transform(CircularLinkedListToArray);
 
     assertContentEquals(result, output);
 
@@ -107,14 +106,14 @@ TEST(split_a_circular_linked_list, test2)
 #ifdef __TEST__
     ListNodeInspector inspector;
 #endif
-    using std::ranges::views::transform;
+    // using std::ranges::views::transform;
     auto input = vector<int> { 2, 6, 1, 5 };
     auto output = vector<vector<int>> { { 2, 6 }, { 1, 5 } };
     auto* list = ArrayToCircularLinkedList(input);
 
     auto nodes = unordered_set<ListNode*> {};
     TraversalCircularListNode(list, [&](auto* node) { nodes.insert(node); });
-    auto result = Solution().splitCircularLinkedList(list) | transform(CircularLinkedListToArray);
+    auto result = Solution().splitCircularLinkedList(list) | std::ranges::views::transform(CircularLinkedListToArray);
     assertContentEquals(result, output);
     for (auto* node : nodes) {
         delete node;
