cec2419af01ce98aa2b04e42450d6a030c20ffff
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-15 23:40:08 +0800

    删除注释

--- insufficient-nodes-in-root-to-leaf-paths/xmake.lua
+++ insufficient-nodes-in-root-to-leaf-paths/xmake.lua
@@ -1,10 +1,8 @@
 if is_mode("test") then add_requires("vcpkg::eventpp") end
 set_policy("platform.longpaths", true)
--- add_repositories("masx200/xmake-repo https://gitee.com/masx200/xmake-repo.git");
--- add_repositories(
---    "masx200/xmake-repo https://ghproxy.com/https://github.com/masx200/xmake-repo.git");
+
 add_rules("mode.debug", "mode.release");
--- add_requires("leetcode-treenode-cpp 1.1.7");
+
 set_languages("c17", "cxx20");
 add_requires("vcpkg::gtest");
 target("insufficient-nodes-in-root-to-leaf-paths-test");
@@ -18,12 +16,12 @@ end
 set_group("test")
 set_default(false)
 set_kind("binary");
--- add_files("debugTreeNode.ixx");
+
 add_files("printTreeNode.ixx", "*.ixx");
 set_languages("c++20");
 set_policy("build.c++.modules", true);
 add_files("test.cpp");
 add_packages("vcpkg::gtest");
--- add_packages("leetcode-treenode-cpp");
+
 add_files("../leetcode-treenode-cpp/*.ixx");
 target_end();
