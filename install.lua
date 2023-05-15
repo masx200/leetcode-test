f60ee39c552a936221c554e524102c3c897eafba
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-15 17:26:10 +0800

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
 import("net.http.download")
 import("utils.archive.extract")
 
-local function movefilesinfolder(src, dest)
+local function movefilesinfolder(src, dest, callback)
     print('create folder :' .. dest)
     os.mkdir(dest)
     print('move folder :' .. src .. " to folder:" .. dest)
@@ -10,13 +10,15 @@ local function movefilesinfolder(src, dest)
 
         if os.isdir(luafile) then
             -- print('folder:' .. luafile)
-            movefilesinfolder(luafile, path.join(dest, path.filename(luafile)))
+            movefilesinfolder(luafile, path.join(dest, path.filename(luafile)),
+                              callback)
 
         else
             -- print('file:' .. luafile)
             print('move file :' .. luafile .. " to folder:" .. dest)
 
             os.mv(luafile, dest)
+            callback(path.join(dest, path.filename(luafile)))
 
         end
 
@@ -28,26 +30,33 @@ function main()
     local name = "leetcode-treenode-cpp"
     local version = "1.1.8"
     local folder = name .. "-" .. version
-    local file = path.join('downloads', folder .. '.tar.gz')
-    if not os.exists(file) then
+    local archivefile = path.join('downloads', folder .. '.tar.gz')
+    if not os.exists(archivefile) then
         local url = 'https://ghproxy.com/https://github.com/masx200/' .. name ..
                         '/archive/' .. version .. '.tar.gz'
-        print('download url:' .. url .. " to file:" .. file)
-        download(url, file)
+        print('download url:' .. url .. " to file:" .. archivefile)
+        download(url, archivefile)
     end
     local outputdir = path.join('downloads', folder)
     if os.exists(outputdir) then
         print("remove folder:" .. outputdir)
         os.rmdir(outputdir)
     end
-    print('extract file:' .. file .. " to folder:" .. outputdir)
-    extract(file, outputdir)
+    print('extract file:' .. archivefile .. " to folder:" .. outputdir)
+    extract(archivefile, outputdir)
     local src = path.join('downloads', folder, folder)
     if os.exists(name) then
         print("remove folder:" .. name)
         os.rmdir(name)
     end
-
-    movefilesinfolder(src, name)
-
+    local installedlistfile = io.open(archivefile .. '.list', "w")
+    if nil == installedlistfile then
+        print("open file: " .. archivefile .. '.list' .. " fail")
+        return
+    end
+    movefilesinfolder(src, name, function(installedfile)
+        installedlistfile:write(installedfile)
+        installedlistfile:write("\n")
+    end)
+    installedlistfile:close()
 end
