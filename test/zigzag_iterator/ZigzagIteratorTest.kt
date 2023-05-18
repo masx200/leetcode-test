ca30bdebcca4dafa24b97ca6fc0827073a690d72
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-18 16:05:38 +0800

    测试

--- test/zigzag_iterator/ZigzagIteratorTest.kt
+++ test/zigzag_iterator/ZigzagIteratorTest.kt
@@ -3,7 +3,6 @@ package com.github.masx200.leetcode_test.zigzag_iterator
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertContentEquals
-import kotlin.test.junit5.JUnit5Asserter.assertEquals
 
 class ZigzagIteratorTest {
 
@@ -11,22 +10,52 @@ class ZigzagIteratorTest {
     @Test
     fun nextTest() {
 
-        val v1 = listOf<Int>(1, 2);
-        val v2 = listOf<Int>(3, 4, 5, 6);
-        val OUTPUT = listOf<Int>(1, 3, 2, 4, 5, 6);
+        val v1 = listOf<Int>(1, 2)
+        val v2 = listOf<Int>(3, 4, 5, 6)
+        val OUTPUT = listOf<Int>(1, 3, 2, 4, 5, 6)
 
 
-        val zzit =  ZigzagIterator(v1, v2);
+        val zzit = ZigzagIterator(v1, v2)
 
-        val res: MutableList<Int> = mutableListOf<Int>();
+        val res: MutableList<Int> = mutableListOf<Int>()
 
         while (zzit.hasNext()) {
-            res.add(zzit.next());
+            res.add(zzit.next())
         }
-        assertContentEquals(res, OUTPUT);
+        assertContentEquals(res, OUTPUT)
     }
 
     @Test
     fun hasNextTest() {
+        val v1 = listOf<Int>(1)
+        val v2 = listOf<Int>()
+        val OUTPUT = listOf<Int>(1)
+
+
+        val zzit = ZigzagIterator(v1, v2)
+
+        val res: MutableList<Int> = mutableListOf<Int>()
+
+        while (zzit.hasNext()) {
+            res.add(zzit.next())
+        }
+        assertContentEquals(res, OUTPUT)
+    }
+
+    @Test
+    fun hasNextTest2() {
+        val v1 = listOf<Int>()
+        val v2 = listOf<Int>(1)
+        val OUTPUT = listOf<Int>(1)
+
+
+        val zzit = ZigzagIterator(v1, v2)
+
+        val res: MutableList<Int> = mutableListOf<Int>()
+
+        while (zzit.hasNext()) {
+            res.add(zzit.next())
+        }
+        assertContentEquals(res, OUTPUT)
     }
 }
