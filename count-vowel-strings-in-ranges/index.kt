dc065954d6016199347477eed8713197de4dd048
masx200 <34191203+masx200@users.noreply.github.com>
2023-06-17 18:15:39 +0800

    vowelStrings

--- count-vowel-strings-in-ranges/index.kt
+++ count-vowel-strings-in-ranges/index.kt
@@ -3,5 +3,30 @@ package com.github.masx200.leetcode_test.count_vowel_strings_in_ranges
 class Solution {
     fun vowelStrings(words: Array<String>, queries: Array<IntArray>): IntArray {
 
+
+        val prefixs = IntArray(words.size) { 0 }
+
+        words.forEachIndexed { i, s ->
+            if (i == 0) {
+
+                prefixs[i] = booleanToInt(isVowelString(s))
+            } else {
+                prefixs[i] = prefixs[i - 1] + booleanToInt(isVowelString(s))
+
+            }
+
+        }
+        return IntArray(queries.size) {
+            val li = queries[it][0]
+            val ri = queries[it][1]
+            if (li == ri) booleanToInt(isVowelString(words[li])) else prefixs[ri] - if (li == 0) 0 else prefixs[li - 1]
+        }
     }
+
+    private fun isVowelString(s: String): Boolean {
+        val vowels = hashSetOf('a', 'e', 'i', 'o', 'u')
+        return s.isNotEmpty() && vowels.contains(s[0]) && vowels.contains(s.last())
+    }
+
+    fun booleanToInt(b: Boolean) = if (b) 1 else 0
 }
