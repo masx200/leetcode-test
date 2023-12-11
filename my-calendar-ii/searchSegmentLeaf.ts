8c195201d756ac3faf00718fbbb0af6bbad217c4
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-11 15:12:26 +0800

    //如果有三个子节点,则把更靠近的中间的节点保留,剩下的节点放另一边下面的子节点中

--- my-calendar-ii/searchSegmentLeaf.ts
+++ my-calendar-ii/searchSegmentLeaf.ts
@@ -34,6 +34,37 @@ export function searchSegmentLeaf(
             .filter(([a, b]) => node.start <= a && node.end >= b);
 
         node.children = segments.map(([a, b]) => SegmentTree(a, b, node.value));
+
+        if (node.children.length === 3) {
+            //如果有三个子节点,则把更靠近的中间的节点保留,剩下的节点放另一边下面的子节点中
+            const mid = Math.round((node.start + node.end) / 2);
+
+            // console.log(node);
+
+            if (
+                Math.abs(mid - node.children[0].end) <
+                    Math.abs(mid - node.children[2].start)
+            ) {
+                const left = node.children[0];
+                const right = SegmentTree(
+                    node.children[1].start,
+                    node.children[2].end,
+                    node.value,
+                    [node.children[1], node.children[2]],
+                );
+                node.children = [left, right];
+            } else {
+                const left = SegmentTree(
+                    node.children[0].start,
+                    node.children[1].end,
+                    node.value,
+                    [node.children[0], node.children[1]],
+                );
+                const right = node.children[2];
+                node.children = [left, right];
+            }
+            // console.log(node);
+        }
     }
     if (node.children.length) {
         each?.(node);
