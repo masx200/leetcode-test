94fc05fface9b499aafb6650c58697c8a4c16510
GitHub <noreply@github.com>
2024-12-12 05:49:16 +0000

    format

--- animal-shelter-lcci/xmake.lua
+++ animal-shelter-lcci/xmake.lua
@@ -4,5 +4,5 @@ target("animal-shelter-lcci")
 set_kind("static")
 add_files("index.ixx")
 
-add_files("*.ixx", {install = true,public = true})
+add_files("*.ixx", {install = true, public = true})
 target_end()
--- valid-perfect-square/xmake.lua
+++ valid-perfect-square/xmake.lua
@@ -4,5 +4,5 @@ target("valid-perfect-square")
 set_kind("static")
 add_files("index.ixx")
 
-add_files("*.ixx", {install = true,public = true})
+add_files("*.ixx", {install = true, public = true})
 target_end()
